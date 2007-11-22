70878aae3e3809938c158a99d4572a74e47b0538
Igor Czechowski <iczechowski@gmail.com>
2007-11-22 18:21:35 +0000

    verify execution on derived iterface mock but run in on a base interface
    --HG--
    extra : convert_revision : svn%3Aaa2aecf3-ea3e-0410-9d70-716747e7c967/trunk%4048

--- test/org/mockito/usage/binding/IncorectBindingPuzzleFixedTest.java
+++ test/org/mockito/usage/binding/IncorectBindingPuzzleFixedTest.java
@@ -5,15 +5,15 @@ import org.mockito.Mockito;
 
 public class IncorectBindingPuzzleFixedTest {
 
-    private DerivedInterface mock;
-    
-    @Before public void setup() {
-        mock = Mockito.mock(DerivedInterface.class);
+    private BaseInteface mock;
+
+    private void setMock(DerivedInterface mock) {
+        this.mock = mock;
     }
 
     private class BaseMessage {}
     private class Message extends BaseMessage {}
-    
+
     private interface BaseInteface {
         public void print(BaseMessage message);
     }
@@ -21,22 +21,28 @@ public class IncorectBindingPuzzleFixedTest {
     private interface DerivedInterface extends BaseInteface {
         public void print(Message message);
     }
-    
+
     private void print(BaseMessage message) {
         mock.print(message);
     }
-    
+
+    @Ignore
     @Test
     public void overriddenInterfaceMethodNotWorking() throws Exception {
+        DerivedInterface derivedMock = Mockito.mock(DerivedInterface.class);
+        setMock(derivedMock);
         Message message = new Message();
         print(message);
-        Mockito.verify(mock).print(message);
+        Mockito.verify(derivedMock).print(message);
     }
-    
+
     @Test
     public void overriddenInterfaceMethodWorking() throws Exception {
-        Message message = new Message();
+        DerivedInterface derivedMock = Mockito.mock(DerivedInterface.class);
+        setMock(derivedMock);
+        BaseMessage message = new Message();
         print(message);
-        Mockito.verify(mock).print((BaseMessage)message);
+        Mockito.verify(derivedMock).print(message);
     }
+
 }
