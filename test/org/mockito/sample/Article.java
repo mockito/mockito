d8aeefbc4a30f324c0a6fcec5c2609c85d7a6a81
Szczepan Faber <szczepiq@gmail.com>
2007-11-24 23:35:25 +0000

    changed the way mocks work. Now the default verification is once() instead of anyTimes(). BTW anyTimes is not yet implemented :>
    --HG--
    extra : convert_revision : svn%3Aaa2aecf3-ea3e-0410-9d70-716747e7c967/trunk%4063

---
+++ test/org/mockito/sample/Article.java
@@ -0,0 +1,14 @@
+package org.mockito.sample;
+
+public class Article {
+
+    private int numberOfRelatedArticles;
+
+    public int getNumberOfRelatedArticles() {
+        return numberOfRelatedArticles;
+    }
+
+    public void setNumberOfRelatedArticles(int numberOfRelatedArticles) {
+        this.numberOfRelatedArticles = numberOfRelatedArticles;
+    }
+}
