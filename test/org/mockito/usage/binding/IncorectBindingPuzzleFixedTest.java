0c0eead86a293ac77d1adf5292e6069d2e33c0a7
Szczepan Faber <szczepiq@gmail.com>
2007-11-30 17:03:47 +0000

    simplified the test
    --HG--
    extra : convert_revision : svn%3Aaa2aecf3-ea3e-0410-9d70-716747e7c967/trunk%40107

--- test/org/mockito/usage/binding/IncorectBindingPuzzleFixedTest.java
+++ test/org/mockito/usage/binding/IncorectBindingPuzzleFixedTest.java
@@ -20,24 +20,24 @@ public class IncorectBindingPuzzleFixedTest {
     }
 
     private interface Super {
-        public void print(Object message);
+        public void say(Object message);
     }
 
     private interface Sub extends Super {
-        public void print(String message);
+        public void say(String message);
     }
 
-    private void print(Object message) {
-        mock.print(message);
+    private void say(Object message) {
+        mock.say(message);
     }
 
     @Test
     public void shouldUseArgumentTypeWhenOverloadingPuzzleDetected() throws Exception {
         Sub sub = mock(Sub.class);
         setMockWithDowncast(sub);
-        print("Hello");
+        say("Hello");
         try {
-            verify(sub).print("Hello");
+            verify(sub).say("Hello");
             fail();
         } catch (VerificationError error) {
             String expected = 
@@ -56,10 +56,10 @@ public class IncorectBindingPuzzleFixedTest {
     public void shouldUseArgumentTypeWhenOverloadingPuzzleDetectedByStrictly() throws Exception {
         Sub sub = mock(Sub.class);
         setMockWithDowncast(sub);
-        print("Hello");
+        say("Hello");
         Strictly strictly = createStrictOrderVerifier(mock);
         try {
-            strictly.verify(sub).print("Hello");
+            strictly.verify(sub).say("Hello");
             fail();
         } catch (VerificationError error) {
             String expected = 
