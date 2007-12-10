55e400380f63a2cdb5b4f536db23c647a496e5ca
Szczepan Faber <szczepiq@gmail.com>
2007-12-10 23:58:55 +0000

    more and more refactorings
    --HG--
    extra : convert_revision : svn%3Aaa2aecf3-ea3e-0410-9d70-716747e7c967/trunk%40159

--- src/org/mockito/internal/MockitoState.java
+++ src/org/mockito/internal/MockitoState.java
@@ -1,84 +1,23 @@
-/*
- * Copyright (c) 2007 Mockito contributors 
- * This program is made available under the terms of the MIT License.
- */
 package org.mockito.internal;
 
-import org.mockito.exceptions.Exceptions;
-
 @SuppressWarnings("unchecked")
-public class MockitoState {
+public interface MockitoState {
+
+    void reportControlForStubbing(MockControl mockControl);
+
+    MockitoExpectation pullControlToBeStubbed();
+
+    void verifyingStarted(VerifyingMode verify);
+
+    VerifyingMode pullVerifyingMode();
+
+    void stubbingStarted();
+
+    void stubbingCompleted();
     
-    private static ThreadLocal<MockitoState> INSTANCE = new ThreadLocal<MockitoState>();
-        
-    private MockControl lastControl;
-    private VerifyingMode verifyingMode;
-    private int invocationSequenceNumber = 1;
-    private boolean stubbingInProgress = false;
+    int nextSequenceNumber();
 
-    public static MockitoState instance() {
-        if (INSTANCE.get() == null) {
-            INSTANCE.set(new MockitoState());
-        }
-        return INSTANCE.get();
-    }
-    
-    public void reportControlForStubbing(MockControl mockControl) {
-        lastControl = mockControl;
-    }
+    void validateState();
 
-    public MockitoExpectation pullControlToBeStubbed() {
-        MockControl temp = lastControl;
-        lastControl = null;
-        return temp;
-    }
-    
-    public void verifyingStarted(VerifyingMode verify) {
-        validateState();
-        verifyingMode = verify;
-    }
-
-    public VerifyingMode pullVerifyingMode() {
-        VerifyingMode temp = verifyingMode;
-        verifyingMode = null;
-        return temp;
-    }
-
-    public int nextSequenceNumber() {
-        return invocationSequenceNumber++;
-    }
-
-    public void stubbingStarted() {
-        validateState();
-        stubbingInProgress = true;
-    }
-
-    public void validateState() {
-        if (verifyingMode != null) {
-            verifyingMode = null;
-            Exceptions.unfinishedVerificationException();
-        }
-        
-        if (stubbingInProgress) {
-            stubbingInProgress = false;
-            Exceptions.unfinishedStubbing();
-        }
-    }
-
-    public void stubbingCompleted() {
-        stubbingInProgress = false;
-    }
-    
-    public String toString() {
-        return  "lastControl: " + lastControl + 
-        ", verifyingMode: " + verifyingMode +
-        ", invocationSequenceNumber: " + invocationSequenceNumber +
-        ", stubbingInProgress: " + stubbingInProgress;
-    }
-
-    void reset() {
-        stubbingInProgress = false;
-        verifyingMode = null;
-        invocationSequenceNumber = 1;
-    }
+    void reset();
 }
