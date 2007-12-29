15df84fc50ff93daff6e76c4272e532dd2ed82b6
Szczepan Faber <szczepiq@gmail.com>
2007-12-29 22:06:18 +0000

    added headers with copyright ;)
    --HG--
    extra : convert_revision : svn%3Aaa2aecf3-ea3e-0410-9d70-716747e7c967/trunk%40249

--- test/org/mockito/internal/verification/InvocationsFinderStub.java
+++ test/org/mockito/internal/verification/InvocationsFinderStub.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2007, Szczepan Faber. 
+ * Copyright (c) 2007 Mockito contributors
  * This program is made available under the terms of the MIT License.
  */
 package org.mockito.internal.verification;
