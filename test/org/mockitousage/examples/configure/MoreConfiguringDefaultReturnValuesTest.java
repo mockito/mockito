f3f0aeeaad311c510b22a31a786e47b36a681d9f
Szczepan Faber <szczepiq@gmail.com>
2008-03-24 01:26:22 +0000

    added another test that shows how to configure return values
    --HG--
    extra : convert_revision : svn%3Aaa2aecf3-ea3e-0410-9d70-716747e7c967/trunk%40499

---
+++ test/org/mockitousage/examples/configure/MoreConfiguringDefaultReturnValuesTest.java
@@ -0,0 +1,94 @@
+/*
+ * Copyright (c) 2007 Mockito contributors
+ * This program is made available under the terms of the MIT License.
+ */
+package org.mockitousage.examples.configure;
+import static org.mockito.Mockito.*;
+
+import java.util.LinkedList;
+import java.util.List;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.MockitoConfiguration;
+import org.mockito.ReturnValues;
+import org.mockito.TestBase;
+import org.mockito.internal.invocation.Invocation;
+import org.mockito.internal.stubbing.DefaultReturnValues;
+
+public class MoreConfiguringDefaultReturnValuesTest extends TestBase {
+    
+    private MyDefaultReturnValues myDefaultReturnValues;
+
+    @Test
+    public void shouldAllowConfiguringReturnValuesForSelectedMocks() throws Exception {
+        MyObject smartStub = mock(MyObject.class);
+        beSmartStub(smartStub);
+        MyObject mock = mock(MyObject.class);
+        
+        assertNotNull(smartStub.returnMyObject());
+        assertEquals("", smartStub.returnString());
+        assertTrue(smartStub.returnBoolean());
+        
+        assertNull(mock.returnMyObject());
+        assertNull(mock.returnString());
+        assertFalse(mock.returnBoolean());
+    }
+
+    interface MyObject {
+        MyObject returnMyObject();
+        String returnString();
+        boolean returnBoolean();
+    }
+    
+    private void beSmartStub(Object mock) {
+        myDefaultReturnValues.addSmartStub(mock);
+    }
+    
+    @Before
+    public void configureDefaultReturnValues() {
+        myDefaultReturnValues = new MyDefaultReturnValues();
+        MockitoConfiguration.setCustomReturnValues(myDefaultReturnValues);
+    }
+    
+    @After
+    public void resetDefaultReturnValues() {
+        MockitoConfiguration.resetCustomReturnValues();
+        myDefaultReturnValues.clearSmartStubs();
+    }
+    
+    private final class MyDefaultReturnValues implements ReturnValues {
+        private List<Object> smartStubs = new LinkedList<Object>();
+
+        public Object valueFor(Invocation invocation) {
+            Object value = new DefaultReturnValues().valueFor(invocation);
+            Class<?> returnType = invocation.getMethod().getReturnType();
+            if (value != null || returnType == Void.TYPE) {
+                return value;
+            } else if (smartStubs.contains(invocation.getMock())) {
+                return returnValueForSmartStub(returnType);
+            } else {
+                return null;
+            }
+        }
+
+        private Object returnValueForSmartStub(Class<?> returnType) {
+            if (returnType == String.class) {
+                return "";
+            } else if (returnType == Boolean.TYPE) {
+                return true;
+            } else {
+                return mock(returnType);
+            }
+        }
+
+        public void clearSmartStubs() {
+            smartStubs.clear();
+        }
+
+        public void addSmartStub(Object mock) {
+            smartStubs.add(mock);
+        }
+    }
+}
