658e86cdf010b36d61fecab66fe4df3401f5b22b
Szczepan Faber <szczepiq@gmail.com>
2008-04-21 19:09:08 +0000

    added a test that captures an argument (it's not very useful but there was an issue added about that...)
    --HG--
    extra : convert_revision : svn%3Aaa2aecf3-ea3e-0410-9d70-716747e7c967/trunk%40593

--- test/org/mockitousage/matchers/CapturingMatchersTest.java
+++ test/org/mockitousage/matchers/CapturingMatchersTest.java
@@ -4,7 +4,11 @@
  */
 package org.mockitousage.matchers;
 
+import static org.mockito.Matchers.*;
+import static org.mockito.Mockito.*;
+
 import org.junit.Test;
+import org.mockito.ArgumentMatcher;
 import org.mockito.TestBase;
 import org.mockito.MockitoAnnotations.Mock;
 import org.mockitousage.IMethods;
@@ -12,10 +16,34 @@ import org.mockitousage.IMethods;
 @SuppressWarnings("unchecked")  
 public class CapturingMatchersTest extends TestBase {
     
+    private final class ArgumentCaptor<T> extends ArgumentMatcher<T> {
+        private T argument;
+
+        @Override
+        public boolean matches(Object argument) {
+            this.argument = (T) argument;
+            return true;
+        }
+        
+        public T getArgument() {
+            return argument;
+        }
+    }
+
     @Mock private IMethods mock;
 
     @Test
     public void shouldAllowCapturingArguments() throws Exception {
+        Object object = new Object();
+        mock.simpleMethod(object);
         
+        ArgumentCaptor captor = new ArgumentCaptor();
+        verify(mock).simpleMethod(capturedArg(captor));
+        
+        assertSame(object, captor.getArgument());
+    }
+
+    private Object capturedArg(ArgumentCaptor captor) {
+        return argThat(captor);
     }
 }
