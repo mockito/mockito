5a45b97880236c9c7b30399a784c7668af56e229
Szczepan Faber <szczepiq@gmail.com>
2008-06-25 14:47:48 +0000

    added test case helpful when discussing generic aspect of matchers
    --HG--
    extra : convert_revision : svn%3Aaa2aecf3-ea3e-0410-9d70-716747e7c967/trunk%40657

---
+++ test/org/mockitousage/matchers/GenericMatchersTest.java
@@ -0,0 +1,35 @@
+/*
+ * Copyright (c) 2007 Mockito contributors
+ * This program is made available under the terms of the MIT License.
+ */
+package org.mockitousage.matchers;
+
+import static org.mockito.Matchers.*;
+import static org.mockito.Mockito.*;
+
+import java.util.Date;
+import java.util.List;
+
+import org.junit.Test;
+import org.mockito.TestBase;
+import org.mockito.MockitoAnnotations.Mock;
+
+public class GenericMatchersTest extends TestBase {
+    
+    private interface Foo {
+        List<String> sort(List<String> otherList);
+        String convertDate(Date date);
+    }
+    
+    @Mock Foo sorter;
+
+    @SuppressWarnings("unchecked")
+    @Test
+    public void shouldCompile() {
+        stub(sorter.convertDate(new Date())).toReturn("one");
+        stub(sorter.convertDate((Date) anyObject())).toReturn("two");
+
+        //following requires warning suppression but allows setting anyList()
+        stub(sorter.sort(anyList())).toReturn(null);
+    }
+}
