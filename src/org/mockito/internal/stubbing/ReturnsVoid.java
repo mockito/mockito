132915bfe8651342cf7f6154edb847dafd55a51f
Szczepan Faber <szczepiq@gmail.com>
2008-07-17 07:53:14 +0000

    removed redundant annotation
    --HG--
    extra : convert_revision : svn%3Aaa2aecf3-ea3e-0410-9d70-716747e7c967/trunk%40688

--- src/org/mockito/internal/stubbing/ReturnsVoid.java
+++ src/org/mockito/internal/stubbing/ReturnsVoid.java
@@ -5,7 +5,6 @@ import org.mockito.stubbing.Answer;
 
 public class ReturnsVoid implements Answer<Object> {
     
-    @Override
     public Object answer(InvocationOnMock invocation) throws Throwable {
         return null;
     }
