ed68639a4bde85b4c0ed6e8a9773a12c5414309b
Szczepan Faber <szczepiq@gmail.com>
2008-07-25 23:46:17 +0000

    removed some dead class
    --HG--
    extra : convert_revision : svn%3Aaa2aecf3-ea3e-0410-9d70-716747e7c967/trunk%40728

--- src/org/mockito/internal/util/Logger.java
+++
@@ -1,8 +0,0 @@
-package org.mockito.internal.util;
-
-public class Logger {
-
-    public static void log(String message) {
-        System.out.println("T:"+Thread.currentThread().getId() + " " + message);
-    }
-}
