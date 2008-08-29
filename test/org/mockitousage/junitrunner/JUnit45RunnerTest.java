58510540a9c3d871e4b865be7e84062549e6b3c0
Szczepan Faber <szczepiq@gmail.com>
2008-08-29 22:23:07 +0000

    fixing the build
    --HG--
    extra : convert_revision : svn%3Aaa2aecf3-ea3e-0410-9d70-716747e7c967/trunk%40861

--- test/org/mockitousage/junitrunner/JUnit45RunnerTest.java
+++ test/org/mockitousage/junitrunner/JUnit45RunnerTest.java
@@ -1,8 +1,24 @@
 package org.mockitousage.junitrunner;
 
+import static org.mockito.Mockito.*;
+
+import java.util.List;
+
+import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.Mock;
 import org.mockito.runners.MockitoJUnitRunner;
+import org.mockitoutil.TestBase;
 
 @RunWith(MockitoJUnitRunner.class)
-public class JUnit45RunnerTest extends JUnitRunnerTestCase {
+@SuppressWarnings("unchecked")
+public class JUnit45RunnerTest extends TestBase {
+    
+    @Mock private List list;
+    
+    @Test
+    public void shouldInitMocksUsingRunner() {
+        list.add("test");
+        verify(list).add("test");
+    }
 }
--- test/org/mockitousage/junitrunner/JUnit4RunnerTest.java
+++ test/org/mockitousage/junitrunner/JUnit4RunnerTest.java
@@ -1,8 +1,24 @@
 package org.mockitousage.junitrunner;
 
+import static org.mockito.Mockito.*;
+
+import java.util.List;
+
+import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.Mock;
 import org.mockito.runners.MockitoJUnit44Runner;
+import org.mockitoutil.TestBase;
 
 @RunWith(MockitoJUnit44Runner.class)
-public class JUnit4RunnerTest extends JUnitRunnerTestCase {
+@SuppressWarnings("unchecked")
+public class JUnit4RunnerTest extends TestBase {
+    
+    @Mock private List list;
+    
+    @Test
+    public void shouldInitMocksUsingRunner() {
+        list.add("test");
+        verify(list).add("test");
+    }
 }
