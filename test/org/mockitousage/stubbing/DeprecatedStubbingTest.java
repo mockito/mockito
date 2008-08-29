b987701b29c1a729d24ec82ee4eeaafd234842e7
Szczepan Faber <szczepiq@gmail.com>
2008-08-29 19:53:01 +0000

    kept single test that exercises deprecated API
    --HG--
    extra : convert_revision : svn%3Aaa2aecf3-ea3e-0410-9d70-716747e7c967/trunk%40847

---
+++ test/org/mockitousage/stubbing/DeprecatedStubbingTest.java
@@ -0,0 +1,82 @@
+/*
+ * Copyright (c) 2007 Mockito contributors
+ * This program is made available under the terms of the MIT License.
+ */
+package org.mockitousage.stubbing;
+
+import static org.mockito.Matchers.*;
+import static org.mockito.Mockito.*;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.exceptions.verification.ArgumentsAreDifferent;
+import org.mockito.exceptions.verification.NoInteractionsWanted;
+import org.mockitousage.IMethods;
+import org.mockitoutil.TestBase;
+
+@SuppressWarnings({"unchecked", "deprecation"})
+public class DeprecatedStubbingTest extends TestBase {
+
+    @Mock private IMethods mock;
+
+    @Before
+    public void setup() {
+        mock = mock(IMethods.class);
+    }
+    
+    @Test
+    public void shouldEvaluateLatestStubbingFirst() throws Exception {
+        stub(mock.objectReturningMethod(isA(Integer.class))).toReturn(100);
+        stub(mock.objectReturningMethod(200)).toReturn(200);
+        
+        assertEquals(200, mock.objectReturningMethod(200));
+        assertEquals(100, mock.objectReturningMethod(666));
+        assertEquals("default behavior should return null", null, mock.objectReturningMethod("blah"));
+    }
+    
+    @Test
+    public void shouldStubbingBeTreatedAsInteraction() throws Exception {
+        stub(mock.booleanReturningMethod()).toReturn(true);
+        
+        mock.booleanReturningMethod();
+        
+        try {
+            verifyNoMoreInteractions(mock);
+            fail();
+        } catch (NoInteractionsWanted e) {}
+    }
+    
+    @Test
+    public void shouldAllowStubbingToString() throws Exception {
+        IMethods mockTwo = mock(IMethods.class);
+        stub(mockTwo.toString()).toReturn("test");
+        
+        assertThat(mock.toString(), contains("Mock for IMethods"));
+        assertEquals("test", mockTwo.toString());
+    }
+    
+    @Test
+    public void shouldStubbingNotBeTreatedAsInteraction() {
+        stub(mock.simpleMethod("one")).toThrow(new RuntimeException());
+        stubVoid(mock).toThrow(new RuntimeException()).on().simpleMethod("two");
+        
+        verifyZeroInteractions(mock);
+    }
+    
+    @Test
+    public void shouldStubbingWithThrowableFailVerification() {
+        stub(mock.simpleMethod("one")).toThrow(new RuntimeException());
+        stubVoid(mock).toThrow(new RuntimeException()).on().simpleMethod("two");
+        
+        try {
+            verify(mock).simpleMethod("one");
+            fail();
+        } catch (ArgumentsAreDifferent e) {}
+        
+        try {
+            verify(mock).simpleMethod("two");
+            fail();
+        } catch (ArgumentsAreDifferent e) {}
+    }
+}
