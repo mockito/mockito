5d0b4c1637f6964f8cc9f6326c4af4f7488ca498
Szczepan Faber <szczepiq@gmail.com>
2008-10-10 17:04:02 +0000

    more and more refactorings around verification
    --HG--
    extra : convert_revision : svn%3Aaa2aecf3-ea3e-0410-9d70-716747e7c967/trunk%40939

---
+++ src/org/mockito/internal/verification/VerificationModeFactory.java
@@ -0,0 +1,31 @@
+/*
+ * Copyright (c) 2007 Mockito contributors
+ * This program is made available under the terms of the MIT License.
+ */
+package org.mockito.internal.verification;
+
+import org.mockito.internal.verification.MockitoVerificationMode.Verification;
+
+/**
+ * Holds additional information regarding verification.
+ * <p> 
+ * Implements marking interface which hides details from Mockito users. 
+ */
+public class VerificationModeFactory {
+    
+    public static MockitoVerificationMode atLeastOnce() {
+        return atLeast(1);
+    }
+
+    public static MockitoVerificationMode atLeast(int minNumberOfInvocations) {
+        return new MockitoVerificationMode(minNumberOfInvocations, Verification.AT_LEAST);
+    }
+
+    public static MockitoVerificationMode times(int wantedNumberOfInvocations) {
+        return new MockitoVerificationMode(wantedNumberOfInvocations, Verification.EXPLICIT);
+    }
+
+    public static NoMoreInteractionsMode noMoreInteractions() {
+        return new NoMoreInteractionsMode();
+    }
+}
