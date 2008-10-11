e068a6238fb05ad4eb72553435d86287ec984309
Szczepan Faber <szczepiq@gmail.com>
2008-10-11 17:17:49 +0000

    polished some unfinished parts
    --HG--
    extra : convert_revision : svn%3Aaa2aecf3-ea3e-0410-9d70-716747e7c967/trunk%40944

--- src/org/mockito/InOrderVerifier.java
+++ src/org/mockito/InOrderVerifier.java
@@ -33,12 +33,13 @@ class InOrderVerifier implements InOrder {
         if (!mocksToBeVerifiedInOrder.contains(mock)) {
             reporter.inOrderRequiresFamiliarMock();
         }
-//        if (!(mode instanceof InOrderVerificationMode) {
-//            //TODO fail nicely here
-//        }
         if (mode instanceof MockitoVerificationMode) {
             return Mockito.verify(mock, new MockitoInOrderVerificationMode((MockitoVerificationMode) mode, mocksToBeVerifiedInOrder));
         }
-        return Mockito.verify(mock, mode);
+        throw new RuntimeException("nothing else is yet implemented");
+//        if (!(mode instanceof InOrderVerificationMode) {
+//            //TODO fail nicely here
+//        }
+//        return Mockito.verify(mock, mode);
     }
 }
