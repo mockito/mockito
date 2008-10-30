643757569747daf52655ff77520bdcd61ce00de4
Szczepan Faber <szczepiq@gmail.com>
2008-10-30 19:23:09 +0000

    added one TODO test
    --HG--
    extra : convert_revision : svn%3Aaa2aecf3-ea3e-0410-9d70-716747e7c967/trunk%401063

---
+++ test/org/mockitousage/misuse/RestrictedObjectMethodsTest.java
@@ -0,0 +1,46 @@
+/*
+ * Copyright (c) 2007 Mockito contributors
+ * This program is made available under the terms of the MIT License.
+ */
+package org.mockitousage.misuse;
+
+import static org.mockito.Mockito.*;
+
+import org.junit.Ignore;
+import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.exceptions.base.MockitoException;
+import org.mockitousage.IMethods;
+import org.mockitoutil.TestBase;
+
+public class RestrictedObjectMethodsTest extends TestBase {
+
+    @Mock IMethods mock;
+
+    @Ignore
+    @Test
+    public void shouldNotAllowStubbingRestrictedMethods() {
+        try {
+            when(mock.hashCode()).thenReturn(1);
+            fail();
+        } catch(MockitoException e) {
+            assertEquals("Cannot stub hashCode() method", e.getMessage());
+        }
+        
+        try {
+            when(mock.equals(null)).thenReturn(false);
+            fail();
+        } catch(MockitoException e) {
+            assertEquals("Cannot stub equals() method", e.getMessage());
+        }
+    }
+    
+    @Ignore
+    @Test
+    public void shouldNotAllowVerifyingRestrictedMethods() {
+        //TODO exception message should mention those methods are not verifiable
+        verify(mock).toString();
+        verify(mock).hashCode();
+        verify(mock).equals(null);
+    }
+}
