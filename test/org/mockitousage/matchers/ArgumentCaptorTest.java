df409c31b55097c761eb0d844ea1e217cd1e5402
Szczepan Faber <szczepiq@gmail.com>
2008-12-28 16:01:46 +0000

    Added new test for new functionality
    --HG--
    extra : convert_revision : svn%3Aaa2aecf3-ea3e-0410-9d70-716747e7c967/trunk%401106

---
+++ test/org/mockitousage/matchers/ArgumentCaptorTest.java
@@ -0,0 +1,97 @@
+/*
+ * Copyright (c) 2007 Mockito contributors
+ * This program is made available under the terms of the MIT License.
+ */
+package org.mockitousage.matchers;
+
+import static org.mockito.Mockito.*;
+
+import org.junit.Ignore;
+import org.junit.Test;
+import org.mockito.ArgumentMatcher;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockitousage.IMethods;
+import org.mockitoutil.TestBase;
+
+@SuppressWarnings("unchecked")
+public class ArgumentCaptorTest extends TestBase {
+
+    public class Capture<T> extends ArgumentMatcher<T> {
+        private Object argument;
+
+        public boolean matches(Object argument) {
+            this.argument = argument;
+            return true;
+        }
+
+        public T getValue() {
+            //TODO nice instanceof check ?
+            return (T) argument;
+        }
+    }
+
+    class Person {
+
+        private final int age;
+
+        public Person(int age) {
+            this.age = age;
+        }
+
+        public int getAge() {
+            return age;
+        }
+    }
+    
+    class Foo {
+     
+        private IMethods methods;
+        
+        public Foo(IMethods methods) {
+            this.methods = methods;
+        }
+
+        public void bar() {
+            Person person = new Person(12);
+            methods.simpleMethod(person);
+        }
+    }
+
+    private Object capture(Capture<Person> capturedArgument) {
+        Mockito.argThat(capturedArgument);
+        return null;
+    }
+
+    @Mock IMethods mock;
+
+    //TODO decide on getting capture matcher into Mockito
+    @Test
+    public void shouldUseArgumentCaptorInEasyMockStyle() {
+        Foo foo = new Foo(mock);
+        foo.bar();
+        Capture<Person> capturedArgument = new Capture<Person>();
+        verify(mock).simpleMethod(capture(capturedArgument));
+        assertEquals(12, capturedArgument.getValue().getAge());
+    }
+    
+    @Ignore("assertion idea is not for current release I guess")
+    @Test
+    public void shouldUseAssertor() {
+        Foo foo = new Foo(mock);
+        foo.bar();
+        verify(mock).simpleMethod(argThat(new Assertion<Person>() {
+            public void asserts(Person person) {
+                assertEquals(12, person.getAge());
+            }
+        }));
+    }
+    
+    public static <T> T argThat(Assertion<T> assertion) {
+        return null;
+    }
+    
+    public interface Assertion<T> {
+        void asserts(T object);
+    }
+}
