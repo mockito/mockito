6f20f5aec6507f65f6ab9812adfc4f6640d6fcb8
Szczepan Faber <szczepiq@gmail.com>
2009-01-10 18:24:13 +0000

    removed TODO
    --HG--
    extra : convert_revision : svn%3Aaa2aecf3-ea3e-0410-9d70-716747e7c967/trunk%401164

--- src/org/mockito/DefaultMockitoConfiguration.java
+++ src/org/mockito/DefaultMockitoConfiguration.java
@@ -3,7 +3,6 @@ package org.mockito;
 import org.mockito.internal.returnvalues.EmptyReturnValues;
 
 //TODO javadocs
-//TODO test
 public class DefaultMockitoConfiguration implements IMockitoConfiguration {
 
     public ReturnValues getReturnValues() {
