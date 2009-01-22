7f8304b30a933b6ed91d6d55f3dafc9e02a26275
Szczepan Faber <szczepiq@gmail.com>
2009-01-22 23:22:41 +0000

    more work on release script
    --HG--
    extra : convert_revision : svn%3Aaa2aecf3-ea3e-0410-9d70-716747e7c967/trunk%401238

--- releasing/release.py
+++ releasing/release.py
@@ -1,22 +1,35 @@
 import os
 
 def run(cmd):
-  os.system(cmd)
+  print "\nRunning command: " + cmd
+  assert os.system(cmd) == 0, "\nCommand failed: " + cmd
 
-run('svn ruby replace_headers')
+run('ruby replace_headers.rb')
 
-ok = input("Make sure NOW if all you need is checked in. Proceed? (Y/N):")
-if ok != 'Y': 
+ok = raw_input("Make sure NOW if all you need is checked in. Proceed? (Y/N):")
+print ok
+if ok != "Y": 
   print "Exited on user request"
   exit(1)
-
-ver = input("Specify the version, e.g. 1.7:")
+  
+ver = raw_input("Specify the version, e.g. 1.7:")
 
 branch = 'https://mockito.googlecode.com/svn/branches/' + ver
-run('svn copy -m "branched before release" https://mockito.googlecode.com/svn/trunk ' + branch)
-run('svn co ' + branch + ' ../../mockito-' + ver) 
+#run('svn copy -m "branched before release" https://mockito.googlecode.com/svn/trunk ' + branch)
+#run('svn co ' + branch + ' ../../mockito-' + ver)
 
-run('svn st')
+os.chdir('../../mockito-' + ver)
 
-run('svn ps -R svn:mime-type text/html ../javadoc/*')
-run('svn ps -R svn:mime-type text/css ../javadoc/stylesheet.css')
+f = open('version.properties', 'w')
+f.write('version=' + ver)
+f.close()
+
+#run('ant test.release release.javadoc')
+
+run('svn add javadoc/*')
+run('svn ps -R svn:mime-type text/html javadoc/*')
+run('svn ps -R svn:mime-type text/css javadoc/stylesheet.css')
+run('svn ci -m "released javadoc, updated version"')
+
+os.chdir('releasing')
+import upload_jars
