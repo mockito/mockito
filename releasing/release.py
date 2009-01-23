9df2b5d8c7f7fd6ca44c90bf0cadbbf1b0e00bcc
Szczepan Faber <szczepiq@gmail.com>
2009-01-23 23:05:53 +0000

    more work on releasing script added ignore
    --HG--
    extra : convert_revision : svn%3Aaa2aecf3-ea3e-0410-9d70-716747e7c967/trunk%401242

--- releasing/release.py
+++ releasing/release.py
@@ -1,13 +1,12 @@
 import os
 
 def run(cmd):
-  print "\nRunning command: " + cmd
+  print("\nRunning command: " + cmd)
   assert os.system(cmd) == 0, "\nCommand failed: " + cmd
 
 run('ruby replace_headers.rb')
 
 ok = raw_input("Make sure NOW if all you need is checked in. Proceed? (Y/N):")
-print ok
 if ok != "Y": 
   print "Exited on user request"
   exit(1)
@@ -18,11 +17,11 @@ branch = 'https://mockito.googlecode.com/svn/branches/' + ver
 #run('svn copy -m "branched before release" https://mockito.googlecode.com/svn/trunk ' + branch)
 #run('svn co ' + branch + ' ../../mockito-' + ver)
 
-print "Switching to ../../mockito-" + ver + " folder"
+print("Switching to ../../mockito-" + ver + " folder")
 
 os.chdir('../../mockito-' + ver)
 
-print "Updating version.properties to " + ver
+print("Updating version.properties to " + ver)
 
 f = open('version.properties', 'w')
 f.write('version=' + ver)
@@ -35,5 +34,24 @@ run('svn ps -R svn:mime-type text/html javadoc/*')
 run('svn ps -R svn:mime-type text/css javadoc/stylesheet.css')
 run('svn ci -m "released javadoc, updated version"')
 
+print("Uploading binaries to the googlecode")
+
 os.chdir('releasing')
-import upload_jars
+
+import google_upload
+import sys
+
+sys.argv.append('--project=mockito')
+sys.argv.append('--user=szczepiq')
+base_args = sys.argv[:]
+
+sys.argv.append('--summary=All jars, source and javadocs')
+sys.argv.append('../target/mockito-' + ver + '.zip')
+
+google_upload.main()
+
+sys.argv = base_args
+sys.argv.append("--summary=Single jar, includes source")
+sys.argv.append('../target/mockito-all-' + ver + '.jar')
+
+google_upload.main()
