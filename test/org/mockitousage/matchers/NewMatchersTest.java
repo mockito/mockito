2e0f918ff2f693c5704ba3832a94ffccddf97840
Szczepan Faber <szczepiq@gmail.com>
2009-04-03 10:30:04 +0000

    Added more tests around new matchers
    --HG--
    extra : convert_revision : svn%3Aaa2aecf3-ea3e-0410-9d70-716747e7c967/trunk%401285

---
+++ test/org/mockitousage/matchers/NewMatchersTest.java
@@ -0,0 +1,69 @@
+/*
+ * Copyright (c) 2007 Mockito contributors
+ * This program is made available under the terms of the MIT License.
+ */
+package org.mockitousage.matchers;
+
+import static org.mockito.Matchers.*;
+import static org.mockito.Mockito.*;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.HashSet;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mockito;
+import org.mockitousage.IMethods;
+import org.mockitoutil.TestBase;
+
+@SuppressWarnings("unchecked")
+public class NewMatchersTest extends TestBase {
+    
+    private IMethods mock;
+
+    @Before
+    public void setUp() {
+        mock = Mockito.mock(IMethods.class);
+    }
+
+    @Test
+    public void shouldAllowAnyList() {
+        when(mock.forList(anyList())).thenReturn("x");
+        
+        assertEquals("x", mock.forList(null));
+        assertEquals("x", mock.forList(Arrays.asList("x", "y")));
+        
+        verify(mock, times(2)).forList(anyList());
+    }
+    
+    @Test
+    public void shouldAllowAnyCollection() {
+        when(mock.forCollection(anyCollection())).thenReturn("x");
+        
+        assertEquals("x", mock.forCollection(null));
+        assertEquals("x", mock.forCollection(Arrays.asList("x", "y")));
+        
+        verify(mock, times(2)).forCollection(anyCollection());
+    }
+    
+    @Test
+    public void shouldAllowAnyMap() {
+        when(mock.forMap(anyMap())).thenReturn("x");
+        
+        assertEquals("x", mock.forMap(null));
+        assertEquals("x", mock.forMap(new HashMap<String, String>()));
+        
+        verify(mock, times(2)).forMap(anyMap());
+    }
+    
+    @Test
+    public void shouldAllowAnySet() {
+        when(mock.forSet(anySet())).thenReturn("x");
+        
+        assertEquals("x", mock.forSet(null));
+        assertEquals("x", mock.forSet(new HashSet<String>()));
+        
+        verify(mock, times(2)).forSet(anySet());
+    }
+}
