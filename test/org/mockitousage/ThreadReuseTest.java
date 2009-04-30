f6188a60e3ead959d02416c83756524db5842b48
Szczepan Faber <szczepiq@gmail.com>
2009-04-30 17:45:22 +0000

    cosmetic changes, added some initial code to work on one weird bug
    --HG--
    extra : convert_revision : svn%3Aaa2aecf3-ea3e-0410-9d70-716747e7c967/trunk%401365

---
+++ test/org/mockitousage/ThreadReuseTest.java
@@ -0,0 +1,53 @@
+package org.mockitousage;
+
+import static org.mockito.Mockito.*;
+
+import org.junit.Ignore;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockitoutil.TestBase;
+
+@RunWith(org.mockito.runners.MockitoJUnit44Runner.class)
+@SuppressWarnings("unchecked")
+@Ignore("this issue is a little bit unclear to me. Let's wait until we have some more data")
+public class ThreadReuseTest extends TestBase {
+    
+    private static ThreadLocal<Class> l = new ThreadLocal<Class>();
+    @Mock IMethods mock;
+
+    @Test(timeout = 100)
+    public void test1() throws Exception {
+        when(mock.simpleMethod()).thenReturn("foo");
+        assertNull(l.get());
+        l.set(getClass());
+    }
+
+    @Test
+    public void test2() throws Exception {
+        System.out.println(mock.simpleMethod());
+        assertNull(l.get());
+        l.set(getClass());
+    }
+
+    @Test(timeout = 100)
+    public void test3() throws Exception {
+        doThrow(new RuntimeException()).when(mock).voidMethod();
+        assertNull(l.get());
+        l.set(getClass());
+    }
+
+    @Test(timeout = 100)
+    public void test4() throws Exception {
+        mock.voidMethod();
+        assertNull(l.get());
+        l.set(getClass());
+    }
+
+//    @Test
+//    public void test5() throws Exception {
+//        when(mock.simpleMethod()).thenReturn("bar");
+//        assertNull(l.get());
+//        l.set(getClass());
+//    }
+}
