e878da23e3a555c644438302cdb6e497252268e6
Szczepan Faber <szczepiq@gmail.com>
2009-04-30 17:49:11 +0000

    Initial check-in for argument capturing code
    --HG--
    extra : convert_revision : svn%3Aaa2aecf3-ea3e-0410-9d70-716747e7c967/trunk%401366

--- test/org/mockitousage/matchers/ArgumentCaptorTest.java
+++ test/org/mockitousage/matchers/ArgumentCaptorTest.java
@@ -6,37 +6,54 @@ package org.mockitousage.matchers;
 
 import static org.mockito.Mockito.*;
 
-import org.junit.Ignore;
+import java.util.LinkedList;
+import java.util.List;
+
 import org.junit.Test;
 import org.mockito.ArgumentMatcher;
-import org.mockito.Mock;
 import org.mockito.Mockito;
-import org.mockitousage.IMethods;
 import org.mockitoutil.TestBase;
 
 @SuppressWarnings("unchecked")
 public class ArgumentCaptorTest extends TestBase {
 
-    public class Capture<T> extends ArgumentMatcher<T> {
-        private Object argument;
+    public class Argument<T> extends ArgumentMatcher<T> {
+        private LinkedList<Object> arguments = new LinkedList<Object>();
 
         public boolean matches(Object argument) {
-            this.argument = argument;
+            this.arguments.add(argument);
             return true;
         }
+        
+        public T capture() {
+            Mockito.argThat(this);
+            return null;
+        }
 
-        public T getValue() {
-            //TODO: after 1.7 nice instanceof check here?
-            //TODO: capture according to what one guy wrote to the mailing list?
-            return (T) argument;
+        public T value() {
+            if (arguments.isEmpty()) {
+                assert false;
+            } else {
+                //TODO: after 1.7 nice instanceof check here?
+                return (T) arguments.getLast();
+            }
+            return (T) arguments;
+        }
+        
+        public T getLastValue() {
+            return value();
+        }
+
+        public List<T> allValues() {
+            return (List) arguments;
         }
     }
 
     class Person {
 
-        private final int age;
+        private final Integer age;
 
-        public Person(int age) {
+        public Person(Integer age) {
             this.age = age;
         }
 
@@ -45,54 +62,75 @@ public class ArgumentCaptorTest extends TestBase {
         }
     }
     
-    class Foo {
+    class Emailer {
      
-        private IMethods methods;
+        private EmailService service;
         
-        public Foo(IMethods methods) {
-            this.methods = methods;
+        public Emailer(EmailService service) {
+            this.service = service;
         }
 
-        public void bar() {
-            Person person = new Person(12);
-            methods.simpleMethod(person);
-        }
-    }
-
-    private Object capture(Capture<Person> capturedArgument) {
-        Mockito.argThat(capturedArgument);
-        return null;
-    }
-
-    @Mock IMethods mock;
-
-    //TODO: after 1.7 decide on getting capture matcher into Mockito
-    @Test
-    public void shouldUseArgumentCaptorInEasyMockStyle() {
-        Foo foo = new Foo(mock);
-        foo.bar();
-        Capture<Person> capturedArgument = new Capture<Person>();
-        verify(mock).simpleMethod(capture(capturedArgument));
-        assertEquals(12, capturedArgument.getValue().getAge());
-    }
-    
-    @Ignore("assertion idea is not for current release I guess")
-    @Test
-    public void shouldUseAssertor() {
-        Foo foo = new Foo(mock);
-        foo.bar();
-        verify(mock).simpleMethod(argThat(new Assertion<Person>() {
-            public void asserts(Person person) {
-                assertEquals(12, person.getAge());
+        public void email(Integer ... personId) {
+            for (Integer i : personId) {
+                Person person = new Person(i);
+                service.sendEmailTo(person);
             }
-        }));
+        }
     }
     
-    public static <T> T argThat(Assertion<T> assertion) {
-        return null;
+    interface EmailService {
+        void sendEmailTo(Person person);
+    }
+
+    EmailService emailService = mock(EmailService.class);
+    Emailer emailer = new Emailer(emailService);
+
+    @Test
+    public void shouldAllowAssertionsOnCapturedArgument() {
+        //when
+        emailer.email(12);
+        
+        //then
+        Argument<Person> argument = new Argument<Person>();
+        verify(emailService).sendEmailTo(argument.capture());
+        
+        assertEquals(12, argument.value().getAge());
     }
     
-    public interface Assertion<T> {
-        void asserts(T object);
+    @Test
+    public void shouldAllowAssertionsOnAllCapturedArguments() {
+        //when
+        emailer.email(11, 12);
+        
+        //then
+        Argument<Person> argument = new Argument<Person>();
+        verify(emailService, atLeastOnce()).sendEmailTo(argument.capture());
+        List<Person> allValues = argument.allValues();
+        
+        assertEquals(11, allValues.get(0).getAge());
+        assertEquals(12, allValues.get(1).getAge());
+    }
+    
+    @Test
+    public void shouldAllowAssertionsOnLastArgument() {
+        //when
+        emailer.email(11, 12, 13);
+        
+        //then
+        Argument<Person> argument = new Argument<Person>();
+        verify(emailService, atLeastOnce()).sendEmailTo(argument.capture());
+        
+        assertEquals(13, argument.value().getAge());
+    }
+    
+    @Test
+    public void shouldAllowAssertionsOnCapturedNull() {
+        //when
+        emailService.sendEmailTo(null);
+        
+        //then
+        Argument<Person> argument = new Argument<Person>();
+        verify(emailService).sendEmailTo(argument.capture());
+        assertEquals(null, argument.value());
     }
 }
