25d1049f92730831dd6790b6b0ef31781b617407
Szczepan Faber <szczepiq@gmail.com>
2009-05-01 20:20:14 +0000

    Added logo to the javadocs
    --HG--
    extra : convert_revision : svn%3Aaa2aecf3-ea3e-0410-9d70-716747e7c967/trunk%401385

--- src/org/mockito/Mockito.java
+++ src/org/mockito/Mockito.java
@@ -30,9 +30,11 @@ import org.mockito.stubbing.Answer;
  * Mockito library enables mocks creation, verification and stubbing.
  * <p>
  * This javadoc content is also available on the <a href="http://mockito.org">http://mockito.org</a> web page. 
- * We try to keep all documentation in javadocs because it guarantees consistency between what's on the web and what's in the source code.
+ * All documentation is kept in javadocs because it guarantees consistency between what's on the web and what's in the source code.
  * Also, it makes possible to access documentation straight from the IDE even if you work offline.   
  * 
+ * <p align="left"><img src="logo.jpg"/></p>
+ *
  * <h1>Contents</h1>
  * 
  * <b> 
