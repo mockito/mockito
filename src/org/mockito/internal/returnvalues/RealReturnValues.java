b35e2bbb889a8b9f9148883a01ac5607455ac735
Bartosz Bankowski <bbankowski@gmail.com>
2009-05-14 10:49:12 +0000

    Removing unwanted javadocs.
    --HG--
    extra : convert_revision : svn%3Aaa2aecf3-ea3e-0410-9d70-716747e7c967/trunk%401429

--- src/org/mockito/internal/returnvalues/RealReturnValues.java
+++ src/org/mockito/internal/returnvalues/RealReturnValues.java
@@ -7,18 +7,6 @@ package org.mockito.internal.returnvalues;
 import org.mockito.ReturnValues;
 import org.mockito.invocation.InvocationOnMock;
 
-/**
- * Description: TODO: Enter a paragraph that summarizes what the class does and
- * why someone might want to utilize it
- * 
- * <p>
- * Copyright © 2000-2007, NetSuite, Inc.
- * </p>
- * 
- * @author amurkes
- * @version 2007.0
- * @since Apr 15, 2009
- */
 public class RealReturnValues implements ReturnValues {
     public Object valueFor(InvocationOnMock invocation) throws Throwable {
         return invocation.invokeSuper();
