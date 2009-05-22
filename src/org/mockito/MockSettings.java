43f3f3ad4e5171d19d9641e8012126bacf03c917
Szczepan Faber <szczepiq@gmail.com>
2009-05-22 22:11:30 +0000

    Checked & removed TODOs
    --HG--
    extra : convert_revision : svn%3Aaa2aecf3-ea3e-0410-9d70-716747e7c967/trunk%401450

--- src/org/mockito/MockSettings.java
+++ src/org/mockito/MockSettings.java
@@ -8,10 +8,8 @@ public interface MockSettings {
 
     MockSettings name(String name);
 
-    //TODO: hide spiedInstance?
     MockSettings spiedInstance(Object object);
 
-    //TODO: check out types when using this method - can we suppress deprecation warnings?
     MockSettings defaultAnswer(Answer defaultAnswer);
     
 }
