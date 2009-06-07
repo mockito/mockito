f62ef2b7a821ebd1f6f02326d6b83f86456cecfd
Szczepan Faber <szczepiq@gmail.com>
2009-06-07 21:07:39 +0000

    finished missing tests
    --HG--
    extra : convert_revision : svn%3Aaa2aecf3-ea3e-0410-9d70-716747e7c967/trunk%401483

--- test/org/mockito/internal/matchers/LocalizedMatcherTest.java
+++ test/org/mockito/internal/matchers/LocalizedMatcherTest.java
@@ -1,6 +1,7 @@
 package org.mockito.internal.matchers;
 
 import org.hamcrest.Matcher;
+import org.hamcrest.StringDescription;
 import org.junit.Test;
 import org.mockitoutil.TestBase;
 
@@ -28,13 +29,22 @@ public class LocalizedMatcherTest extends TestBase {
     }
     
     @Test
-    public void shouldGetSelfDescribingVerboselyTypesWhenActualMatcherHasCorrectType() throws Exception {
+    public void shouldDescribeWithTypeInfoWhenActualMatcherHasCorrectType() throws Exception {
         //when
         ContainsExtraTypeInformation equals10 = new Equals(10);
         LocalizedMatcher m = new LocalizedMatcher((Matcher) equals10);
         
         //then
-        assertTrue(m.typeMatches(10));
-        assertFalse(m.typeMatches(10L));
+        //TODO: find usages of StringDescription and use static method
+        assertEquals("(Integer) 10", StringDescription.toString(m.withExtraTypeInfo()));
+    }
+    
+    @Test
+    public void shouldNotDescribeWithTypeInfoWhenActualMatcherDoesNotHaveCorrectType() throws Exception {
+        //when
+        LocalizedMatcher m = new LocalizedMatcher(Any.ANY);
+        
+        //then
+        assertSame(m, m.withExtraTypeInfo());
     }
 }
