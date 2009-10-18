454cd22e6bc98033d7e9045382118dee6820e5e3
Szczepan Faber <szczepiq@gmail.com>
2009-10-18 13:52:51 +0000

    Added missing declaration. This test is still not implemented
    --HG--
    extra : convert_revision : svn%3Aaa2aecf3-ea3e-0410-9d70-716747e7c967/trunk%401616

--- test/org/mockitousage/basicapi/MocksSerializationTest.java
+++ test/org/mockitousage/basicapi/MocksSerializationTest.java
@@ -4,22 +4,24 @@
  */
 package org.mockitousage.basicapi;
 
-import org.junit.Ignore;
 import org.junit.Test;
+import org.junit.Ignore;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.withSettings;
 import org.mockitousage.IMethods;
 import org.mockitoutil.TestBase;
 
 import java.io.ByteArrayOutputStream;
 import java.io.ObjectOutputStream;
+import java.io.Serializable;
 
-@Ignore
 public class MocksSerializationTest extends TestBase {
 
     @Test
+    @Ignore
     public void shouldAllowMockToBeSerializable() throws Exception {
         //given
-        IMethods mock = mock(IMethods.class);
+        IMethods mock = mock(IMethods.class, withSettings().extraInterfaces(Serializable.class));
         ByteArrayOutputStream serialized = new ByteArrayOutputStream();
 
         //when
