c4e61e5418c4bde70f1e0eb5e99817e1ac4775b9
Szczepan Faber <szczepiq@gmail.com>
2009-10-20 18:41:17 +0000

    Fixed issue 123 There was an error related to calling real behavior when method had varargs
    --HG--
    extra : convert_revision : svn%3Aaa2aecf3-ea3e-0410-9d70-716747e7c967/trunk%401622

---
+++ test/org/mockitousage/bugs/VarargsErrorWhenCallingRealMethodTest.java
@@ -0,0 +1,27 @@
+/*
+ * Copyright (c) 2007 Mockito contributors
+ * This program is made available under the terms of the MIT License.
+ */
+package org.mockitousage.bugs;
+
+import org.junit.Test;
+import static org.mockito.Mockito.*;
+import org.mockitoutil.TestBase;
+
+public class VarargsErrorWhenCallingRealMethodTest extends TestBase {
+
+    class Foo {
+        int blah(String a, String b, Object ... c) {
+            return 1;
+        }
+    }
+
+    @Test
+    public void shouldNotThrowAnyException() throws Exception {
+        Foo foo = mock(Foo.class);
+
+        when(foo.blah(anyString(), anyString())).thenCallRealMethod();
+
+        assertEquals(1, foo.blah("foo", "bar"));
+    }
+}
