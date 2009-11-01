f895c2420266ee9c4ba5c7b8428c3acdf73e94bf
wesandevie <wesandevie@gmail.com>
2009-11-01 06:09:16 +0000

    In orderIn order implement issue 110 - moved the SerializableNoOp class to the same package as ClassImposterizer since it is the only class that uses it.
    --HG--
    rename : src/org/mockito/cglib/proxy/SerializableNoOp.java => src/org/mockito/internal/creation/jmock/SerializableNoOp.java
    extra : convert_revision : svn%3Aaa2aecf3-ea3e-0410-9d70-716747e7c967/trunk%401644

--- src/org/mockito/internal/creation/jmock/SerializableNoOp.java
+++ src/org/mockito/internal/creation/jmock/SerializableNoOp.java
@@ -1,6 +1,9 @@
-package org.mockito.cglib.proxy;
+package org.mockito.internal.creation.jmock;
 
 import java.io.Serializable;
+
+import org.mockito.cglib.proxy.Callback;
+import org.mockito.cglib.proxy.NoOp;
 
 public class SerializableNoOp implements NoOp, Serializable {
 
