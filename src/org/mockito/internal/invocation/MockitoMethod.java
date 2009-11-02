7daaf900cd7f3e9d4612e7612c101316964dd53d
Szczepan Faber <szczepiq@gmail.com>
2009-11-02 11:04:07 +0000

    In order to have consistent formatting made tabs = 4 spaces
    --HG--
    extra : convert_revision : svn%3Aaa2aecf3-ea3e-0410-9d70-716747e7c967/trunk%401645

--- src/org/mockito/internal/invocation/MockitoMethod.java
+++ src/org/mockito/internal/invocation/MockitoMethod.java
@@ -8,80 +8,80 @@ import org.mockito.exceptions.base.MockitoException;
 
 public class MockitoMethod implements Serializable {
 
-  private static final long serialVersionUID = 6005610965006048445L;
-  private Class<?> declaringClass;
-  private String methodName;
-  private Class<?>[] parameterTypes;
-  private Class<?> returnType;
+    private static final long serialVersionUID = 6005610965006048445L;
+    private Class<?> declaringClass;
+    private String methodName;
+    private Class<?>[] parameterTypes;
+    private Class<?> returnType;
 
-  public MockitoMethod(Method method) {
-    declaringClass = method.getDeclaringClass();
-    methodName = method.getName();
-    parameterTypes = method.getParameterTypes();
-    returnType = method.getReturnType();
-  }
-
-  @Override
-  public int hashCode() {
-    final int prime = 31;
-    int result = 1;
-    result = prime * result + ((declaringClass == null) ? 0 : declaringClass.hashCode());
-    result = prime * result + ((methodName == null) ? 0 : methodName.hashCode());
-    result = prime * result + Arrays.hashCode(parameterTypes);
-    result = prime * result + ((returnType == null) ? 0 : returnType.hashCode());
-    return result;
-  }
-
-  @Override
-  public boolean equals(Object obj) {
-    if (this == obj)
-      return true;
-    if (obj == null)
-      return false;
-    if (getClass() != obj.getClass())
-      return false;
-    MockitoMethod other = (MockitoMethod) obj;
-    if (declaringClass == null) {
-      if (other.declaringClass != null)
-        return false;
-    } else if (!declaringClass.equals(other.declaringClass))
-      return false;
-    if (methodName == null) {
-      if (other.methodName != null)
-        return false;
-    } else if (!methodName.equals(other.methodName))
-      return false;
-    if (!Arrays.equals(parameterTypes, other.parameterTypes))
-      return false;
-    if (returnType == null) {
-      if (other.returnType != null)
-        return false;
-    } else if (!returnType.equals(other.returnType))
-      return false;
-    return true;
-  }
-
-  public Method getMethod() {
-    try {
-      return declaringClass.getDeclaredMethod(methodName, parameterTypes);
-    } catch (SecurityException e) {
-      String message = String.format(
-          "The method %1$s.%2$s is probably private or protected and cannot be mocked.", declaringClass, methodName);
-      throw new MockitoException(message, e);
-    } catch (NoSuchMethodException e) {
-      String message = String.format( 
-        "The method %1$s.%2$s does not exists and you should not get to this point.\n" +
-        "Please report this as a defect with an example of how to reproduce it.", declaringClass, methodName);
-      throw new MockitoException(message, e);
+    public MockitoMethod(Method method) {
+        declaringClass = method.getDeclaringClass();
+        methodName = method.getName();
+        parameterTypes = method.getParameterTypes();
+        returnType = method.getReturnType();
     }
-  }
 
-  public String getName() {
-    return methodName;
-  }
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((declaringClass == null) ? 0 : declaringClass.hashCode());
+        result = prime * result + ((methodName == null) ? 0 : methodName.hashCode());
+        result = prime * result + Arrays.hashCode(parameterTypes);
+        result = prime * result + ((returnType == null) ? 0 : returnType.hashCode());
+        return result;
+    }
 
-  public Class<?> getReturnType() {
-    return returnType;
-  }
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        MockitoMethod other = (MockitoMethod) obj;
+        if (declaringClass == null) {
+            if (other.declaringClass != null)
+                return false;
+        } else if (!declaringClass.equals(other.declaringClass))
+            return false;
+        if (methodName == null) {
+            if (other.methodName != null)
+                return false;
+        } else if (!methodName.equals(other.methodName))
+            return false;
+        if (!Arrays.equals(parameterTypes, other.parameterTypes))
+            return false;
+        if (returnType == null) {
+            if (other.returnType != null)
+                return false;
+        } else if (!returnType.equals(other.returnType))
+            return false;
+        return true;
+    }
+
+    public Method getMethod() {
+        try {
+            return declaringClass.getDeclaredMethod(methodName, parameterTypes);
+        } catch (SecurityException e) {
+            String message = String.format(
+                    "The method %1$s.%2$s is probably private or protected and cannot be mocked.", declaringClass, methodName);
+            throw new MockitoException(message, e);
+        } catch (NoSuchMethodException e) {
+            String message = String.format(
+                    "The method %1$s.%2$s does not exists and you should not get to this point.\n" +
+                            "Please report this as a defect with an example of how to reproduce it.", declaringClass, methodName);
+            throw new MockitoException(message, e);
+        }
+    }
+
+    public String getName() {
+        return methodName;
+    }
+
+    public Class<?> getReturnType() {
+        return returnType;
+    }
 
 }
--- test/org/mockito/internal/invocation/MockitoMethodTest.java
+++ test/org/mockito/internal/invocation/MockitoMethodTest.java
@@ -10,19 +10,19 @@ import org.mockitoutil.TestBase;
 
 public class MockitoMethodTest extends TestBase {
 
-  @Test
-  public void shouldBeSerializable() throws Exception {
-    Class<?>[] args = new Class<?>[0];
-    MockitoMethod method = new MockitoMethod(this.getClass().getMethod("toString", args));
-    ByteArrayOutputStream serialized = new ByteArrayOutputStream();
-    new ObjectOutputStream(serialized).writeObject(method);
-  }
-  
-  @Test
-  public void shouldBeAbleToRetrieveTheMethodInitializedWith() throws Exception {
-    Class<?>[] args = new Class<?>[0];
-    Method method = this.getClass().getMethod("toString", args);
-    MockitoMethod mockMethod = new MockitoMethod(method);
-    assertEquals(method, mockMethod.getMethod());
-  }
+    @Test
+    public void shouldBeSerializable() throws Exception {
+        Class<?>[] args = new Class<?>[0];
+        MockitoMethod method = new MockitoMethod(this.getClass().getMethod("toString", args));
+        ByteArrayOutputStream serialized = new ByteArrayOutputStream();
+        new ObjectOutputStream(serialized).writeObject(method);
+    }
+
+    @Test
+    public void shouldBeAbleToRetrieveTheMethodInitializedWith() throws Exception {
+        Class<?>[] args = new Class<?>[0];
+        Method method = this.getClass().getMethod("toString", args);
+        MockitoMethod mockMethod = new MockitoMethod(method);
+        assertEquals(method, mockMethod.getMethod());
+    }
 }
