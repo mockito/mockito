d95c869fd7e4a8e858d5f20d7ef8d9306693b90c
Szczepan Faber <szczepiq@gmail.com>
2009-11-15 20:21:50 +0000

    Added little javadocs
    --HG--
    extra : convert_revision : svn%3Aaa2aecf3-ea3e-0410-9d70-716747e7c967/trunk%401737

--- src/org/mockito/MockitoDebugger.java
+++ src/org/mockito/MockitoDebugger.java
@@ -1,5 +1,9 @@
 package org.mockito;
 
 public interface MockitoDebugger {
+
+    //Prints all interactions with mock. Also prints stubbing information.
+    //You can put it in your 'tearDown' method
     String printInvocations(Object ... mocks);
+
 }
