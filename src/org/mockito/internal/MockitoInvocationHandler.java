70dee2bf95b9587a38abd421d7ca1c18b92b96be
Szczepan Faber <szczepiq@gmail.com>
2009-11-15 20:18:48 +0000

    renames
    --HG--
    rename : src/org/mockito/internal/IMockHandler.java => src/org/mockito/internal/MockitoInvocationHandler.java
    extra : convert_revision : svn%3Aaa2aecf3-ea3e-0410-9d70-716747e7c967/trunk%401725

--- src/org/mockito/internal/MockitoInvocationHandler.java
+++ src/org/mockito/internal/MockitoInvocationHandler.java
@@ -8,7 +8,7 @@ import java.io.Serializable;
 
 import org.mockito.internal.invocation.Invocation;
 
-public interface IMockHandler extends Serializable {
+public interface MockitoInvocationHandler extends Serializable {
 
     Object handle(Invocation invocation) throws Throwable;
 
