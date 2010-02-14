99d92aa0e7b4bcab5ef0fd46c558844d9926a29d
Szczepan Faber <szczepiq@gmail.com>
2010-02-14 20:20:29 +0000

    Got rid of TODO
    --HG--
    extra : convert_revision : svn%3Aaa2aecf3-ea3e-0410-9d70-716747e7c967/trunk%401834

--- src/org/mockito/internal/listeners/MockingStartedListener.java
+++ src/org/mockito/internal/listeners/MockingStartedListener.java
@@ -2,7 +2,6 @@ package org.mockito.internal.listeners;
 
 import org.mockito.MockSettings;
 
-//TODO: shitty name
 public interface MockingStartedListener extends MockingProgressListener {
     void mockingStarted(Object mock, Class classToMock, MockSettings mockSettings);
 }
