ca877b7c95faef8e1f68109573b87f8abe646311
Szczepan Faber <szczepiq@gmail.com>
2010-06-14 21:02:14 +0000

    Fixed issue 197 only() mode did not respect capturing matchers
    --HG--
    extra : convert_revision : svn%3Aaa2aecf3-ea3e-0410-9d70-716747e7c967/trunk%402016

--- src/org/mockito/internal/verification/Only.java
+++ src/org/mockito/internal/verification/Only.java
@@ -25,13 +25,12 @@ public class Only implements VerificationMode {
 		InvocationMatcher wantedMatcher = data.getWanted();
 		List<Invocation> invocations = data.getAllInvocations();
 		List<Invocation> chunk = finder.findInvocations(invocations,wantedMatcher);
-		if (invocations.size() != 1 && chunk.size() > 0) {
-			marker.markVerified(chunk.get(0), wantedMatcher);
+		if (invocations.size() != 1 && chunk.size() > 0) {			
 			Invocation unverified = finder.findFirstUnverified(invocations);
 			reporter.noMoreInteractionsWanted(unverified, (List) invocations);
 		} else if (invocations.size() != 1 || chunk.size() == 0) {
 			reporter.wantedButNotInvoked(wantedMatcher);
 		}
+		marker.markVerified(chunk.get(0), wantedMatcher);
 	}
-
 }
---
+++ test/org/mockitousage/bugs/ShouldOnlyModeAllowCapturingArgumentsTest.java
@@ -0,0 +1,32 @@
+/*
+ * Copyright (c) 2007 Mockito contributors
+ * This program is made available under the terms of the MIT License.
+ */
+package org.mockitousage.bugs;
+
+import static org.mockito.Mockito.*;
+
+import org.junit.Test;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Mock;
+import org.mockitousage.IMethods;
+import org.mockitoutil.TestBase;
+
+//bug 197
+public class ShouldOnlyModeAllowCapturingArgumentsTest extends TestBase {
+    
+    @Mock IMethods mock;
+    
+    @Test
+    public void shouldAllowCapturingArguments() {
+        //given
+        mock.simpleMethod("o");
+        ArgumentCaptor<String> arg = ArgumentCaptor.forClass(String.class);
+        
+        //when
+        verify(mock, only()).simpleMethod(arg.capture());
+
+        //then
+        assertEquals("o", arg.getValue());
+    }
+}
