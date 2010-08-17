27fbfdca1caf8fbb892c9356b103a96af97c1002
Szczepan Faber <szczepiq@gmail.com>
2010-08-17 20:52:05 +0000

    Added a test for the bug that is not yet fixed
    --HG--
    extra : convert_revision : svn%3Aaa2aecf3-ea3e-0410-9d70-716747e7c967/trunk%402061

---
+++ test/org/mockitousage/bugs/ParentClassNotPublicVeryWeirdBugTest.java
@@ -0,0 +1,28 @@
+package org.mockitousage.bugs;
+
+import static org.mockito.Mockito.*;
+
+import org.junit.Ignore;
+import org.junit.Test;
+import org.mockito.Mockito;
+
+//see bug 212
+@Ignore //needs fixing
+public class ParentClassNotPublicVeryWeirdBugTest {
+    
+    class SuperClass {
+        public boolean isValid() {
+            System.out.println(" cool: " + System.currentTimeMillis());
+          return false;
+        }
+      }
+    
+    public class ClassForMocking extends SuperClass {
+    }
+    
+    @Test
+    public void isValidMocked() {
+        ClassForMocking clazzMock = mock(ClassForMocking.class);
+        Mockito.when(clazzMock.isValid()).thenReturn(true);
+    }
+}
