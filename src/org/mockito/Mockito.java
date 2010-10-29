ecc666ee48ee0727afee65346fbb7d5cc25381b7
Szczepan Faber <szczepiq@gmail.com>
2010-10-29 22:04:21 +0200

    Fixed issue 228 Documentation update

--- src/org/mockito/Mockito.java
+++ src/org/mockito/Mockito.java
@@ -236,6 +236,21 @@ import org.mockito.verification.VerificationMode;
  * <h3 id="6">6. Verification in order</h3>
  * 
  * <pre>
+ * // A. Single mock whose methods must be invoked in a particular order
+ * List singleMock = mock(List.class);
+ *
+ * //using a single mock
+ * singleMock.add("was added first");
+ * singleMock.add("was added second");
+ *
+ * //create an inOrder verifier for a single mock
+ * InOrder inOrder = inOrder(singleMock);
+ *
+ * //following will make sure that add is first called with "was added first, then with "was added second"
+ * inOrder.verify(singleMock).add("was added first");
+ * inOrder.verify(singleMock).add("was added second");
+ *
+ * // B. Multiple mocks that must be used in a particular order
  * List firstMock = mock(List.class);
  * List secondMock = mock(List.class);
  * 
@@ -249,6 +264,8 @@ import org.mockito.verification.VerificationMode;
  * //following will make sure that firstMock was called before secondMock
  * inOrder.verify(firstMock).add("was called first");
  * inOrder.verify(secondMock).add("was called second");
+ *
+ * // Oh, and A + B can be mixed together at will
  * </pre>
  * 
  * Verification in order is flexible - <b>you don't have to verify all
