9f3ecef3d1b3e09ff6f6675edbce1de151514575
murat.knecht@gmail.com <murat.knecht@gmail.com>
2011-02-23 19:14:54 +0100

    Completed documentation of the extended listener methods.
    --HG--
    branch : issue148
    extra : transplant_source : %27%E7%A0%1C%8E%A3%D0V%FF%20%DBd%10%8B%D11%CCs%88%9A

--- src/org/mockito/invocation/InvocationListener.java
+++ src/org/mockito/invocation/InvocationListener.java
@@ -15,17 +15,32 @@ import org.mockito.exceptions.PrintableInvocation;
 public interface InvocationListener {
 	
 	/**
-	 * Called when a method on the listener's mock is invoked.
+	 * Called when a method on the listener's mock was invoked and returned normally.
 	 * 
 	 * Exceptions during this callback are treated as fatal errors. 
 	 * 
-	 * TODO update documentation to parameters
-	 * 
-	 * @param invocation information on the happening method call, never {@code null}.
+	 * @param invocation information on the happening method call, never {@code null}
+	 * @param returnValue whatever it was that the method returned, may be {@code null}
+	 * @param locationOfStubbing Indicates where the method was stubbed, and {@code null} if it was not
 	 * @throws RuntimeException on fatal errors
 	 */
 	void invokingWithReturnValue(PrintableInvocation invocation, Object returnValue, String locationOfStubbing);
 	
+	/**
+	 * Called when a method on the listener's mock was invoked and threw an exception.
+	 * 
+	 * Errors are not reported, as they usually indicate some severe VM error, that ought
+	 * to be propagated and is typically not thrown by your code to begin with.
+	 * 
+	 * Note that the exception is not necessarily caused by stubbing the method with it, but may also
+	 * be the result of incorrect usage of the mockito API or even a bug inside mockito.
+	 * 
+	 * Exceptions during this callback are treated as fatal errors. 
+	 * 
+	 * @param invocation information on the happening method call, never {@code null}
+	 * @param exception the exception that was thrown
+	 * @param locationOfStubbing Indicates where the method was stubbed, and {@code null} if it was not
+	 * @throws RuntimeException on fatal errors
+	 */
 	void invokingWithException(PrintableInvocation invocation, Exception exception, String locationOfStubbing);
-
 }
