4bf949d45744e87869fec9aa06b73cb2cceec31a
Szczepan Faber <szczepiq@gmail.com>
2011-07-03 21:16:08 +0200

    Updated the copyright, take two. Unfortunately, idea is quite eager to put a line break after the copyright and I don't know how to configure it otherwise. So there you go, the history will be polluted with this mean check-in that adds na extra line break :)

--- src/org/mockito/exceptions/misusing/MockitoConfigurationException.java
+++ src/org/mockito/exceptions/misusing/MockitoConfigurationException.java
@@ -2,6 +2,7 @@
  * Copyright (c) 2007 Mockito contributors
  * This program is made available under the terms of the MIT License.
  */
+
 package org.mockito.exceptions.misusing;
 
 import org.mockito.exceptions.base.MockitoException;
