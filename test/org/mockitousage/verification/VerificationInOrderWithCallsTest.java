497f09fe084922531951c2f11b43da54eca25fc5
David Wallace <dmwallace.nz@gmail.com>
2012-01-30 09:45:48 +1300

    Added given, when, then comments to tests
    --HG--
    branch : issue296

--- test/org/mockitousage/verification/VerificationInOrderWithCallsTest.java
+++ test/org/mockitousage/verification/VerificationInOrderWithCallsTest.java
@@ -26,8 +26,8 @@ public class VerificationInOrderWithCallsTest extends TestBase {
 
     @Test
     public void shouldFailWhenMethodNotCalled(){
+        // Given
         mockOne.oneArg( 1 );
-
         InOrder verifier = inOrder( mockOne );
         verifier.verify( mockOne, calls(1)).oneArg( 1 );
 
@@ -35,11 +35,16 @@ public class VerificationInOrderWithCallsTest extends TestBase {
         exceptionRule.expectMessage( "Verification in order failure" );
         exceptionRule.expectMessage( "Wanted but not invoked" );
         exceptionRule.expectMessage( "mockOne.oneArg(2)" );
+
+        // When
         verifier.verify( mockOne, calls(1)).oneArg( 2 );
+
+        // Then - expected exception thrown
     }
 
     @Test
     public void shouldFailWhenMethodCalledTooFewTimes(){
+        // Given
         mockOne.oneArg( 1 );
         mockOne.oneArg( 2 );
 
@@ -51,11 +56,16 @@ public class VerificationInOrderWithCallsTest extends TestBase {
         exceptionRule.expectMessage( "mockOne.oneArg(2)" );
         exceptionRule.expectMessage( "Wanted 2 times" );
         exceptionRule.expectMessage( "But was 1 time" );
+
+        // When
         verifier.verify( mockOne, calls(2)).oneArg( 2 );
+
+        // Then - expected exception thrown
     }
 
     @Test
     public void shouldFailWhenSingleMethodCallsAreOutOfSequence(){
+        // Given
         mockOne.oneArg( 1 );
         mockOne.oneArg( 2 );
 
@@ -66,11 +76,16 @@ public class VerificationInOrderWithCallsTest extends TestBase {
         exceptionRule.expectMessage( "Verification in order failure" );
         exceptionRule.expectMessage( "Wanted but not invoked" );
         exceptionRule.expectMessage( "mockOne.oneArg(1)" );
+
+        // When
         verifier.verify( mockOne, calls(1)).oneArg( 1 );
+
+        // Then - expected exception thrown
     }
 
     @Test
     public void shouldFailWhenDifferentMethodCallsAreOutOfSequence(){
+        // Given
         mockOne.oneArg( 1 );
         mockOne.voidMethod();
 
@@ -81,11 +96,16 @@ public class VerificationInOrderWithCallsTest extends TestBase {
         exceptionRule.expectMessage( "Verification in order failure" );
         exceptionRule.expectMessage( "Wanted but not invoked" );
         exceptionRule.expectMessage( "mockOne.oneArg(1)" );
+
+        // When
         verifier.verify( mockOne, calls(1)).oneArg( 1 );
+
+        // Then - expected exception thrown
     }
 
     @Test
     public void shouldFailWhenMethodCallsOnDifferentMocksAreOutOfSequence(){
+        // Given
         mockOne.voidMethod();
         mockTwo.voidMethod();
 
@@ -96,59 +116,79 @@ public class VerificationInOrderWithCallsTest extends TestBase {
         exceptionRule.expectMessage( "Verification in order failure" );
         exceptionRule.expectMessage( "Wanted but not invoked" );
         exceptionRule.expectMessage( "mockOne.voidMethod()" );
+
+        // When
         verifier.verify( mockOne, calls(1)).voidMethod();
+
+        // Then - expected exception thrown
     }
     
 
     @Test
     public void shouldAllowSequentialCallsToCallsForSingleMethod(){
+        // Given
         mockOne.oneArg( 1 );
         mockOne.oneArg( 2 );
         mockOne.oneArg( 2 );
         mockOne.oneArg( 1 );
 
         InOrder verifier = inOrder( mockOne );
+
+        // When
         verifier.verify( mockOne, calls(1)).oneArg( 1 );
         verifier.verify( mockOne, calls(2)).oneArg( 2 );
         verifier.verify( mockOne, calls(1)).oneArg( 1 );
         verifyNoMoreInteractions(mockOne);
         verifier.verifyNoMoreInteractions();
+
+        // Then - no exception thrown
     }
 
     @Test
     public void shouldAllowSequentialCallsToCallsForDifferentMethods(){
+        // Given
         mockOne.oneArg( 1 );
         mockOne.voidMethod();
         mockOne.voidMethod();
         mockOne.oneArg( 1 );
 
         InOrder verifier = inOrder( mockOne );
+
+        // When
         verifier.verify( mockOne, calls(1)).oneArg( 1 );
         verifier.verify( mockOne, calls(2)).voidMethod();
         verifier.verify( mockOne, calls(1)).oneArg(1);
         verifyNoMoreInteractions(mockOne);
         verifier.verifyNoMoreInteractions();
+
+        // Then - no exception thrown
     }
 
     @Test
     public void shouldAllowSequentialCallsToCallsForMethodsOnDifferentMocks(){
+        // Given
         mockOne.voidMethod();
         mockTwo.voidMethod();
         mockTwo.voidMethod();
         mockOne.voidMethod();
 
         InOrder verifier = inOrder( mockOne, mockTwo );
+
+        // When
         verifier.verify( mockOne, calls(1)).voidMethod();
         verifier.verify( mockTwo, calls(2)).voidMethod();
         verifier.verify( mockOne, calls(1)).voidMethod();
         verifyNoMoreInteractions(mockOne);
         verifyNoMoreInteractions(mockTwo);
         verifier.verifyNoMoreInteractions();
+
+        // Then - no exception thrown
     }
     
     
     @Test
     public void shouldAllowFewerCallsForSingleMethod(){
+        // Given
         mockOne.oneArg( 1 );
         mockOne.oneArg( 2 );
         mockOne.oneArg( 2 );
@@ -156,14 +196,19 @@ public class VerificationInOrderWithCallsTest extends TestBase {
         mockOne.oneArg( 2 );
 
         InOrder verifier = inOrder( mockOne );
+
+        // When
         verifier.verify( mockOne, calls(1)).oneArg( 1 );
         verifier.verify( mockOne, calls(1)).oneArg( 2 );
         verifier.verify( mockOne, calls(1)).oneArg( 1 );
         verifier.verify( mockOne, calls(1)).oneArg( 2 );
+
+        // Then - no exception thrown
     }
 
     @Test
     public void shouldNotVerifySkippedCallsWhenFewerCallsForSingleMethod(){
+        // Given
         mockOne.oneArg( 1 );
         mockOne.oneArg( 2 );
         mockOne.oneArg( 2 );
@@ -175,11 +220,16 @@ public class VerificationInOrderWithCallsTest extends TestBase {
         verifier.verify( mockOne, calls(1)).oneArg( 1 );
         
         exceptionRule.expect( NoInteractionsWanted.class );
+
+        // When
         verifyNoMoreInteractions( mockOne );
+
+        // Then - expected exception thrown
     }
 
     @Test
     public void shouldNotVerifySkippedCallsInInOrderWhenFewerCallsForSingleMethod(){
+        // Given
         mockOne.oneArg( 1 );
         mockOne.oneArg( 2 );
         mockOne.oneArg( 2 );
@@ -190,11 +240,16 @@ public class VerificationInOrderWithCallsTest extends TestBase {
 
         exceptionRule.expect( VerificationInOrderFailure.class );
         exceptionRule.expectMessage( "No interactions wanted here" );
+
+        // When
         verifier.verifyNoMoreInteractions();
+
+        // Then - expected exception thrown
     }
 
     @Test
     public void shouldAllowFewerCallsForDifferentMethods(){
+        // Given
         mockOne.oneArg( 1 );
         mockOne.voidMethod();
         mockOne.voidMethod();
@@ -202,14 +257,19 @@ public class VerificationInOrderWithCallsTest extends TestBase {
         mockOne.voidMethod();
 
         InOrder verifier = inOrder( mockOne );
+
+        // When
         verifier.verify( mockOne, calls(1)).oneArg( 1 );
         verifier.verify( mockOne, calls(1)).voidMethod();
         verifier.verify( mockOne, calls(1)).oneArg( 1 );
         verifier.verify( mockOne, calls(1)).voidMethod();
+
+        // Then - no exception thrown
     }
 
     @Test
     public void shouldNotVerifySkippedCallsWhenFewerCallsForDifferentMethods(){
+        // Given
         mockOne.oneArg( 1 );
         mockOne.voidMethod();
         mockOne.voidMethod();
@@ -221,11 +281,16 @@ public class VerificationInOrderWithCallsTest extends TestBase {
         verifier.verify( mockOne, calls(1)).oneArg( 1 );
 
         exceptionRule.expect( NoInteractionsWanted.class );
+
+        // When
         verifyNoMoreInteractions( mockOne );
+
+        // Then - no exception thrown
     }
 
     @Test
     public void shouldNotVerifySkippedCallsInInOrderWhenFewerCallsForDifferentMethods(){
+        // Given
         mockOne.oneArg( 1 );
         mockOne.voidMethod();
         mockOne.voidMethod();
@@ -236,11 +301,16 @@ public class VerificationInOrderWithCallsTest extends TestBase {
 
         exceptionRule.expect( VerificationInOrderFailure.class );
         exceptionRule.expectMessage( "No interactions wanted here" );
+
+        // When
         verifier.verifyNoMoreInteractions();
+
+        // Then - expected exception thrown
     }
 
     @Test
     public void shouldAllowFewerCallsForMethodsOnDifferentMocks(){
+        // Given
         mockOne.voidMethod();
         mockTwo.voidMethod();
         mockTwo.voidMethod();
@@ -248,14 +318,19 @@ public class VerificationInOrderWithCallsTest extends TestBase {
         mockTwo.voidMethod();
 
         InOrder verifier = inOrder( mockOne, mockTwo );
+
+        // When
         verifier.verify( mockOne, calls(1)).voidMethod();
         verifier.verify( mockTwo, calls(1)).voidMethod();
         verifier.verify( mockOne, calls(1)).voidMethod();
         verifier.verify( mockTwo, calls(1)).voidMethod();
+
+        // Then - no exception thrown
     }
 
     @Test
     public void shouldNotVerifySkippedCallsWhenFewerCallsForMethodsOnDifferentMocks(){
+        // Given
         mockOne.voidMethod();
         mockTwo.voidMethod();
         mockTwo.voidMethod();
@@ -267,11 +342,16 @@ public class VerificationInOrderWithCallsTest extends TestBase {
         verifier.verify( mockOne, calls(1)).voidMethod();
 
         exceptionRule.expect(NoInteractionsWanted.class);
+
+        // When
         verifyNoMoreInteractions( mockTwo );
+
+        // Then - expected exception thrown
     }
 
     @Test
     public void shouldNotVerifySkippedCallsInInOrderWhenFewerCallsForMethodsOnDifferentMocks(){
+        // Given
         mockOne.voidMethod();
         mockTwo.voidMethod();
         mockTwo.voidMethod();
@@ -282,93 +362,136 @@ public class VerificationInOrderWithCallsTest extends TestBase {
 
         exceptionRule.expect( VerificationInOrderFailure.class );
         exceptionRule.expectMessage( "No interactions wanted here" );
+
+        // When
         verifier.verifyNoMoreInteractions();
+
+        // Then - expected exception thrown
     }
 
     @Test
     public void shouldVerifyWithCallsAfterUseOfTimes(){
+        // Given
         mockOne.oneArg( 1 );
         mockOne.oneArg( 2 );
         mockOne.oneArg( 2 );
         mockOne.oneArg( 1 );
 
         InOrder verifier = inOrder( mockOne );
+
+        // When
         verifier.verify( mockOne, times(1)).oneArg( 1 );
         verifier.verify( mockOne, calls(2)).oneArg( 2 );
         verifier.verify( mockOne, calls(1)).oneArg( 1 );
+
+        // Then - no exception thrown
     }
 
     @Test
     public void shouldVerifyWithCallsAfterUseOfAtLeast(){
+        // Given
         mockOne.oneArg( 1 );
         mockOne.oneArg( 2 );
         mockOne.oneArg( 2 );
 
         InOrder verifier = inOrder( mockOne );
+
+        // When
         verifier.verify( mockOne, atLeast(1)).oneArg( 1 );
         verifier.verify( mockOne, calls(2)).oneArg( 2 );
+
+        // Then - no exception thrown
     }
 
     @Test
     public void shouldVerifyWithTimesAfterUseOfCalls(){
+        // Given
         mockOne.oneArg( 1 );
         mockOne.oneArg( 2 );
         mockOne.oneArg( 2 );
         mockOne.oneArg( 1 );
 
         InOrder verifier = inOrder( mockOne );
+
+        // When
         verifier.verify( mockOne, calls(1)).oneArg( 1 );
         verifier.verify( mockOne, times(2)).oneArg( 2 );
         verifier.verify( mockOne, times(1)).oneArg( 1 );
 
+        // Then - no exception thrown
     }
 
     @Test
     public void shouldVerifyWithAtLeastAfterUseOfCalls(){
+        // Given
         mockOne.oneArg( 1 );
         mockOne.oneArg( 2 );
         mockOne.oneArg( 2 );
         mockOne.oneArg( 1 );
 
         InOrder verifier = inOrder( mockOne );
+
+        // When
         verifier.verify( mockOne, calls(1)).oneArg( 1 );
         verifier.verify( mockOne, atLeast(1)).oneArg( 2 );
         verifier.verify( mockOne, atLeast(1)).oneArg( 1 );
+
+        // Then - no exception thrown
     }
 
     @Test
     public void shouldVerifyWithTimesAfterCallsInSameChunk(){
+        // Given
         mockOne.oneArg( 1 );
         mockOne.oneArg( 1 );
         mockOne.oneArg( 1 );
 
         InOrder verifier = inOrder( mockOne );
+
+        // When
         verifier.verify( mockOne, calls(1)).oneArg( 1 );
         verifier.verify( mockOne, times(2)).oneArg( 1 );
         verifier.verifyNoMoreInteractions();
+
+        // Then - no exception thrown
     }
 
     @Test
     public void shouldFailToCreateCallsWithZeroArgument(){
+        // Given
         InOrder verifier = inOrder( mockOne );
         exceptionRule.expect( MockitoException.class );
         exceptionRule.expectMessage( "Negative and zero values are not allowed here" );
+
+        // When
         verifier.verify( mockOne, calls(0)).voidMethod();
+
+        // Then - expected exception thrown
     }
 
     @Test
     public void shouldFailToCreateCallsWithNegativeArgument(){
+        // Given
         InOrder verifier = inOrder( mockOne );
         exceptionRule.expect( MockitoException.class );
         exceptionRule.expectMessage( "Negative and zero values are not allowed here" );
+
+        // When
         verifier.verify( mockOne, calls(-1)).voidMethod();
+
+        // Then - expected exception thrown
     }
 
     @Test
     public void shouldFailToCreateCallsForNonInOrderVerification(){
+        // Given
         mockOne.voidMethod();
         exceptionRule.expect( MockitoException.class );
         exceptionRule.expectMessage( "calls is only intended to work with InOrder" );
+
+        // When
         verify( mockOne, calls(1)).voidMethod();
+
+        // Then - expected exception thrown
     }
 }
