c478e6c8709034e4796cffc1d1bfeba8dc8d0ca2
Jesse Wilson <jessewilson@google.com>
2012-02-02 23:04:22 -0500

    New ClassPathLoader that doesn't depend on ServiceLoader.

--- test/org/mockito/internal/configuration/ReadingConfigurationFromClasspathTest.java
+++ test/org/mockito/internal/configuration/ReadingConfigurationFromClasspathTest.java
@@ -4,6 +4,10 @@
  */
 package org.mockito.internal.configuration;
 
+import java.io.IOException;
+import java.io.StringReader;
+import java.util.Arrays;
+import java.util.Collections;
 import static org.mockito.Mockito.*;
 
 import org.junit.Test;
@@ -13,7 +17,7 @@ import org.mockitousage.IMethods;
 import org.mockitoutil.TestBase;
 
 public class ReadingConfigurationFromClasspathTest extends TestBase {
-    
+
     @Test
     public void shouldReadConfigurationClassFromClassPath() {
         ConfigurationAccess.getConfig().overrideDefaultAnswer(new Answer<Object>() {
@@ -21,7 +25,54 @@ public class ReadingConfigurationFromClasspathTest extends TestBase {
                 return "foo";
             }});
 
-        IMethods mock = mock(IMethods.class); 
+        IMethods mock = mock(IMethods.class);
         assertEquals("foo", mock.simpleMethod());
+    }
+
+    @Test
+    public void readerToLinesEmptyString() throws IOException {
+        assertEquals(Collections.emptyList(), ClassPathLoader.readerToLines(new StringReader("")));
+    }
+
+    @Test
+    public void readerToLinesNoLineBreaks() throws IOException {
+        assertEquals(Arrays.asList("a"), ClassPathLoader.readerToLines(new StringReader("a")));
+    }
+
+    @Test
+    public void readerToLinesWithLineBreaks() throws IOException {
+        assertEquals(Arrays.asList("a", "b", "c"),
+                ClassPathLoader.readerToLines(new StringReader("a\nb\nc")));
+    }
+
+    @Test
+    public void readerToLinesWithEmptyLines() throws IOException {
+        assertEquals(Arrays.asList("a", "", "c"),
+                ClassPathLoader.readerToLines(new StringReader("a\n\nc")));
+    }
+
+    @Test
+    public void stripCommentsAndWhitespaceEmptyInput() throws IOException {
+        assertEquals("", ClassPathLoader.stripCommentAndWhitespace(""));
+    }
+
+    @Test
+    public void stripCommentsAndWhitespaceWhitespaceInput() throws IOException {
+        assertEquals("", ClassPathLoader.stripCommentAndWhitespace(" "));
+    }
+
+    @Test
+    public void stripCommentsAndWhitespaceCommentInInput() throws IOException {
+        assertEquals("a", ClassPathLoader.stripCommentAndWhitespace("a#b"));
+    }
+
+    @Test
+    public void stripCommentsAndWhitespaceMultipleHashes() throws IOException {
+        assertEquals("a", ClassPathLoader.stripCommentAndWhitespace("a#b#c"));
+    }
+
+    @Test
+    public void stripCommentsAndWhitespaceWithWhitespaceAndComments() throws IOException {
+        assertEquals("a", ClassPathLoader.stripCommentAndWhitespace(" a #b"));
     }
 }
