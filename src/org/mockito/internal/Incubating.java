b270b262b93ce524bf284ffea6a5a82129a50f34
Szczepan Faber <szczepiq@gmail.com>
2012-04-01 17:02:19 +0200

    Added more detailed info on the Incubating interface.

--- src/org/mockito/internal/Incubating.java
+++ src/org/mockito/internal/Incubating.java
@@ -9,7 +9,19 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 
 /**
- * Indicates the type or method is <strong>work in progress</strong> and might change.
+ * The annotation conveys following information:
+ * <ul>
+ * <li>The API is fairly new and we would appreciate your feedback. For example, what are you missing from the API
+ * to solve your use case (yes, please, real use cases).</li>
+ * <li>For types or methods that are already released this annotation means that that the API might change.
+ * The chance for that is small and we will always try to make any changes in a backwards compatible way.
+ * The only reason we would want to change it is to provide better support for using and extending Mockito.
+ * </li>
+ * <li>
+ * For types or methods that are not yet released it means the API is <strong>work in progress</strong>
+ * and can change before release.
+ * </li>
+ * </ul>
  */
 @Retention(RetentionPolicy.RUNTIME)
 public @interface Incubating {
