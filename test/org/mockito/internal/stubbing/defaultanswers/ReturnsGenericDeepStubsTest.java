d7c18327ef8a54cf3ed186bf7b11954f23f3f405
Brice Dutheil <brice.dutheil@gmail.com>
2012-10-25 01:33:34 +0200

    better test names

--- test/org/mockito/internal/stubbing/defaultanswers/ReturnsGenericDeepStubsTest.java
+++ test/org/mockito/internal/stubbing/defaultanswers/ReturnsGenericDeepStubsTest.java
@@ -30,7 +30,7 @@ public class ReturnsGenericDeepStubsTest {
     }
 
     @Test
-    public void returning_deep_stubs_1() throws Exception {
+    public void generic_deep_mock_frenzy__look_at_these_chained_calls() throws Exception {
         GenericsNest<?> mock = mock(GenericsNest.class, new ReturnsGenericDeepStubs());
 
         Set<? extends Map.Entry<? extends Cloneable, Set<Number>>> entries = mock.entrySet();
@@ -46,35 +46,50 @@ public class ReturnsGenericDeepStubsTest {
     }
 
     @Test
-    public void returning_deep_stubs_2() throws Exception {
+    public void can_create_mock_from_multiple_type_variable_bounds_when_return_type_of_parameterized_method_is_a_parameterizedtype_that_is_referencing_a_typevar_on_class() throws Exception {
         GenericsNest<?> mock = mock(GenericsNest.class, new ReturnsGenericDeepStubs());
 
-        Cloneable cloneable1 = mock.paramTypeWithTypeParams().get(0);
-        Comparable<?> comparable1 = mock.paramTypeWithTypeParams().get(0);
-        Cloneable cloneable2 = mock.returningK();
-        Comparable<?> comparable2 = mock.returningK();
-        Cloneable cloneable3 = (Cloneable) mock.typeVarWithTypeParams();
+        Cloneable cloneable_bound_that_is_declared_on_typevar_K_in_the_class_which_is_referenced_by_typevar_O_declared_on_the_method =
+                mock.paramTypeWithTypeParams().get(0);
+        Comparable<?> comparable_bound_that_is_declared_on_typevar_K_in_the_class_which_is_referenced_by_typevar_O_declared_on_the_method =
+                mock.paramTypeWithTypeParams().get(0);
     }
 
     @Test
-    public void returning_deep_stubs_3() throws Exception {
+    public void can_create_mock_from_multiple_type_variable_bounds_when_method_return_type_is_referencing_a_typevar_on_class() throws Exception {
+        GenericsNest<?> mock = mock(GenericsNest.class, new ReturnsGenericDeepStubs());
+
+        Cloneable cloneable_bound_of_typevar_K = mock.returningK();
+        Comparable<?> comparable_bound_of_typevar_K = mock.returningK();
+    }
+
+    @Test
+    public void can_create_mock_from_multiple_type_variable_bounds_when_return_type_of_parameterized_method_is_a_typevar_that_is_referencing_a_typevar_on_class() throws Exception {
+        GenericsNest<?> mock = mock(GenericsNest.class, new ReturnsGenericDeepStubs());
+
+        Cloneable cloneable_bound_of_typevar_K_referenced_by_typevar_O = (Cloneable) mock.typeVarWithTypeParams();
+        Comparable<?> comparable_bound_of_typevar_K_referenced_by_typevar_O = mock.typeVarWithTypeParams();
+    }
+
+    @Test
+    public void can_create_mock_from_return_types_declared_with_a_bounded_wildcard() throws Exception {
         GenericsNest<?> mock = mock(GenericsNest.class, new ReturnsGenericDeepStubs());
 
         List<? super Integer> objects = mock.returningWildcard();
-        Number n = (Number) mock.returningWildcard().get(45);
-        n.floatValue();
+        Number type_that_is_the_upper_bound_of_the_wildcard = (Number) mock.returningWildcard().get(45);
+        type_that_is_the_upper_bound_of_the_wildcard.floatValue();
     }
 
     @Test
-    public void returning_deep_stubs_4() throws Exception {
+    public void can_still_work_with_raw_type_in_the_return_type() throws Exception {
         GenericsNest<?> mock = mock(GenericsNest.class, new ReturnsGenericDeepStubs());
 
-        Number n = mock.returnsNormalType();
-        n.floatValue();
+        Number the_raw_type_that_should_be_returned = mock.returnsNormalType();
+        the_raw_type_that_should_be_returned.floatValue();
     }
 
     @Test
-    public void return_default_value_on_non_mockable_nested_generic() throws Exception {
+    public void will_return_default_value_on_non_mockable_nested_generic() throws Exception {
         GenericsNest<?> genericsNest = mock(GenericsNest.class, new ReturnsGenericDeepStubs());
         ListOfInteger listOfInteger = mock(ListOfInteger.class, new ReturnsGenericDeepStubs());
 
@@ -83,9 +98,11 @@ public class ReturnsGenericDeepStubsTest {
     }
 
     @Test(expected = ClassCastException.class)
-    public void returning_deep_stub_dont_work_because_StringBuilder_is_subject_to_erasure() throws Exception {
+    public void as_expected_fail_with_a_CCE_on_callsite_when_erasure_takes_place_for_example___StringBuilder_is_subject_to_erasure() throws Exception {
         GenericsNest<?> mock = mock(GenericsNest.class, new ReturnsGenericDeepStubs());
 
-        StringBuilder stringBuilder = mock.twoTypeParams(new StringBuilder()).append(2).append(3); // ClassCastException
+        // following assignment needed to create a ClassCastException on the call site (i.e. : here)
+        StringBuilder stringBuilder_assignment_that_should_throw_a_CCE =
+                mock.twoTypeParams(new StringBuilder()).append(2).append(3);
     }
 }
