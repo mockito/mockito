cf6e59d2d67d9956d9d5331c8dcdd268f2499bf2
Brice Dutheil <brice.dutheil@gmail.com>
2012-10-25 01:48:08 +0200

    now replacing completely ReturnsDeepStub

--- src/org/mockito/internal/stubbing/defaultanswers/ReturnsDeepStubs.java
+++ src/org/mockito/internal/stubbing/defaultanswers/ReturnsDeepStubs.java
@@ -4,12 +4,13 @@
  */
 package org.mockito.internal.stubbing.defaultanswers;
 
-import org.mockito.Mockito;
 import org.mockito.internal.InternalMockHandler;
+import org.mockito.internal.creation.settings.CreationSettings;
 import org.mockito.internal.stubbing.InvocationContainerImpl;
 import org.mockito.internal.stubbing.StubbedInvocationMatcher;
 import org.mockito.internal.util.MockCreationValidator;
 import org.mockito.internal.util.MockUtil;
+import org.mockito.internal.util.reflection.GenericMetadataSupport;
 import org.mockito.invocation.InvocationOnMock;
 import org.mockito.stubbing.Answer;
 
@@ -20,26 +21,38 @@ import java.io.Serializable;
  *
  * Will return previously created mock if the invocation matches.
  *
- * @see Mockito#RETURNS_DEEP_STUBS
+ * <p>Supports nested generic information, with this answer you can write code like this :
+ *
+ * <pre class="code"><code class="java">
+ *     interface GenericsNest&lt;K extends Comparable&lt;K&gt; & Cloneable&gt; extends Map&lt;K, Set&lt;Number&gt;&gt; {}
+ *
+ *     GenericsNest&lt;?&gt; mock = mock(GenericsNest.class, new ReturnsGenericDeepStubs());
+ *     Number number = mock.entrySet().iterator().next().getValue().iterator().next();
+ * </code></pre>
+ * </p>
+ *
+ * @see org.mockito.Mockito#RETURNS_DEEP_STUBS
  * @see org.mockito.Answers#RETURNS_DEEP_STUBS
  */
 public class ReturnsDeepStubs implements Answer<Object>, Serializable {
     
-    private static final long serialVersionUID = -6926328908792880098L;
-    
-    private Answer<Object> delegate = new ReturnsEmptyValues();
+    private static final long serialVersionUID = -7105341425736035847L;
+
+    private ReturnsEmptyValues delegate = new ReturnsEmptyValues();
 
     public Object answer(InvocationOnMock invocation) throws Throwable {
-        Class<?> clz = invocation.getMethod().getReturnType();
+        GenericMetadataSupport returnTypeGenericMetadata =
+                actualParameterizedType(invocation.getMock()).resolveGenericReturnType(invocation.getMethod());
 
-        if (!new MockCreationValidator().isTypeMockable(clz)) {
-            return delegate.answer(invocation);
+        Class<?> rawType = returnTypeGenericMetadata.rawType();
+        if (!new MockCreationValidator().isTypeMockable(rawType)) {
+            return delegate.returnValueFor(rawType);
         }
 
-        return getMock(invocation);
+        return getMock(invocation, returnTypeGenericMetadata);
     }
 
-    private Object getMock(InvocationOnMock invocation) throws Throwable {
+    private Object getMock(InvocationOnMock invocation, GenericMetadataSupport returnTypeGenericMetadata) throws Throwable {
     	InternalMockHandler<Object> handler = new MockUtil().getMockHandler(invocation.getMock());
     	InvocationContainerImpl container = (InvocationContainerImpl) handler.getInvocationContainer();
 
@@ -51,12 +64,10 @@ public class ReturnsDeepStubs implements Answer<Object>, Serializable {
 		}
 
         // deep stub
-        return recordDeepStubMock(invocation, container);
+        return recordDeepStubMock(returnTypeGenericMetadata.toMock(this), container);
     }
 
-    private Object recordDeepStubMock(InvocationOnMock invocation, InvocationContainerImpl container) {
-        Class<?> clz = invocation.getMethod().getReturnType();
-        final Object mock = Mockito.mock(clz, this);
+    private Object recordDeepStubMock(final Object mock, InvocationContainerImpl container) throws Throwable {
 
         container.addAnswer(new Answer<Object>() {
             public Object answer(InvocationOnMock invocation) throws Throwable {
@@ -65,5 +76,10 @@ public class ReturnsDeepStubs implements Answer<Object>, Serializable {
         }, false);
 
         return mock;
+    }
+
+    private GenericMetadataSupport actualParameterizedType(Object mock) {
+        CreationSettings mockSettings = (CreationSettings) new MockUtil().getMockHandler(mock).getMockSettings();
+        return mockSettings.getMockitoGenericMetadata();
     }
 }
--- test/org/mockito/internal/stubbing/defaultanswers/ReturnsGenericDeepStubsTest.java
+++ test/org/mockito/internal/stubbing/defaultanswers/ReturnsGenericDeepStubsTest.java
@@ -12,6 +12,7 @@ import java.util.Map;
 import java.util.Set;
 
 import static org.fest.assertions.Assertions.assertThat;
+import static org.mockito.Mockito.RETURNS_DEEP_STUBS;
 import static org.mockito.Mockito.mock;
 
 @SuppressWarnings("unused")
@@ -31,7 +32,7 @@ public class ReturnsGenericDeepStubsTest {
 
     @Test
     public void generic_deep_mock_frenzy__look_at_these_chained_calls() throws Exception {
-        GenericsNest<?> mock = mock(GenericsNest.class, new ReturnsGenericDeepStubs());
+        GenericsNest<?> mock = mock(GenericsNest.class, RETURNS_DEEP_STUBS);
 
         Set<? extends Map.Entry<? extends Cloneable, Set<Number>>> entries = mock.entrySet();
         Iterator<? extends Map.Entry<? extends Cloneable,Set<Number>>> entriesIterator = mock.entrySet().iterator();
@@ -47,7 +48,7 @@ public class ReturnsGenericDeepStubsTest {
 
     @Test
     public void can_create_mock_from_multiple_type_variable_bounds_when_return_type_of_parameterized_method_is_a_parameterizedtype_that_is_referencing_a_typevar_on_class() throws Exception {
-        GenericsNest<?> mock = mock(GenericsNest.class, new ReturnsGenericDeepStubs());
+        GenericsNest<?> mock = mock(GenericsNest.class, RETURNS_DEEP_STUBS);
 
         Cloneable cloneable_bound_that_is_declared_on_typevar_K_in_the_class_which_is_referenced_by_typevar_O_declared_on_the_method =
                 mock.paramTypeWithTypeParams().get(0);
@@ -57,7 +58,7 @@ public class ReturnsGenericDeepStubsTest {
 
     @Test
     public void can_create_mock_from_multiple_type_variable_bounds_when_method_return_type_is_referencing_a_typevar_on_class() throws Exception {
-        GenericsNest<?> mock = mock(GenericsNest.class, new ReturnsGenericDeepStubs());
+        GenericsNest<?> mock = mock(GenericsNest.class, RETURNS_DEEP_STUBS);
 
         Cloneable cloneable_bound_of_typevar_K = mock.returningK();
         Comparable<?> comparable_bound_of_typevar_K = mock.returningK();
@@ -65,15 +66,15 @@ public class ReturnsGenericDeepStubsTest {
 
     @Test
     public void can_create_mock_from_multiple_type_variable_bounds_when_return_type_of_parameterized_method_is_a_typevar_that_is_referencing_a_typevar_on_class() throws Exception {
-        GenericsNest<?> mock = mock(GenericsNest.class, new ReturnsGenericDeepStubs());
+        GenericsNest<?> mock = mock(GenericsNest.class, RETURNS_DEEP_STUBS);
 
         Cloneable cloneable_bound_of_typevar_K_referenced_by_typevar_O = (Cloneable) mock.typeVarWithTypeParams();
-        Comparable<?> comparable_bound_of_typevar_K_referenced_by_typevar_O = mock.typeVarWithTypeParams();
+        Comparable<?> comparable_bound_of_typevar_K_referenced_by_typevar_O = (Comparable) mock.typeVarWithTypeParams();
     }
 
     @Test
     public void can_create_mock_from_return_types_declared_with_a_bounded_wildcard() throws Exception {
-        GenericsNest<?> mock = mock(GenericsNest.class, new ReturnsGenericDeepStubs());
+        GenericsNest<?> mock = mock(GenericsNest.class, RETURNS_DEEP_STUBS);
 
         List<? super Integer> objects = mock.returningWildcard();
         Number type_that_is_the_upper_bound_of_the_wildcard = (Number) mock.returningWildcard().get(45);
@@ -82,7 +83,7 @@ public class ReturnsGenericDeepStubsTest {
 
     @Test
     public void can_still_work_with_raw_type_in_the_return_type() throws Exception {
-        GenericsNest<?> mock = mock(GenericsNest.class, new ReturnsGenericDeepStubs());
+        GenericsNest<?> mock = mock(GenericsNest.class, RETURNS_DEEP_STUBS);
 
         Number the_raw_type_that_should_be_returned = mock.returnsNormalType();
         the_raw_type_that_should_be_returned.floatValue();
@@ -90,8 +91,8 @@ public class ReturnsGenericDeepStubsTest {
 
     @Test
     public void will_return_default_value_on_non_mockable_nested_generic() throws Exception {
-        GenericsNest<?> genericsNest = mock(GenericsNest.class, new ReturnsGenericDeepStubs());
-        ListOfInteger listOfInteger = mock(ListOfInteger.class, new ReturnsGenericDeepStubs());
+        GenericsNest<?> genericsNest = mock(GenericsNest.class, RETURNS_DEEP_STUBS);
+        ListOfInteger listOfInteger = mock(ListOfInteger.class, RETURNS_DEEP_STUBS);
 
         assertThat(genericsNest.returningNonMockableNestedGeneric().keySet().iterator().next()).isNull();
         assertThat(listOfInteger.get(25)).isEqualTo(0);
@@ -99,7 +100,7 @@ public class ReturnsGenericDeepStubsTest {
 
     @Test(expected = ClassCastException.class)
     public void as_expected_fail_with_a_CCE_on_callsite_when_erasure_takes_place_for_example___StringBuilder_is_subject_to_erasure() throws Exception {
-        GenericsNest<?> mock = mock(GenericsNest.class, new ReturnsGenericDeepStubs());
+        GenericsNest<?> mock = mock(GenericsNest.class, RETURNS_DEEP_STUBS);
 
         // following assignment needed to create a ClassCastException on the call site (i.e. : here)
         StringBuilder stringBuilder_assignment_that_should_throw_a_CCE =
