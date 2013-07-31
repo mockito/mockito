46158895587c8386aa9ecde36fb1d49189aacc5d
Tim Perry <tim.perry@softwire.com>
2013-07-31 15:51:36 +0100

    Improved after() to return immediately if a test has definitely failed, rather than waiting the full time

--- src/org/mockito/internal/verification/VerificationAfterDelayImpl.java
+++ src/org/mockito/internal/verification/VerificationAfterDelayImpl.java
@@ -5,34 +5,63 @@
 
 package org.mockito.internal.verification;
 
+import org.mockito.exceptions.base.MockitoAssertionError;
 import org.mockito.internal.verification.api.VerificationData;
 import org.mockito.verification.VerificationMode;
 
 public class VerificationAfterDelayImpl implements VerificationMode {
 
+    private int pollingPeriod;
     private int delayMillis;
     private VerificationMode delegate;
 
-    public VerificationAfterDelayImpl(int delayMillis, VerificationMode delegate) {
+    public VerificationAfterDelayImpl(int pollingPeriod, int delayMillis, VerificationMode delegate) {
+        this.pollingPeriod = pollingPeriod;
         this.delayMillis = delayMillis;
         this.delegate = delegate;
     }
     
     public void verify(VerificationData data) {
+        MockitoAssertionError error = null;
+        
         long startTime = System.currentTimeMillis();
         while (System.currentTimeMillis() - startTime <= delayMillis) {
             try {
-                Thread.sleep(delayMillis);
-            } catch (InterruptedException ie) {
-                // Oops. Try again.
+                delegate.verify(data);
+                error = null;
+            } catch (MockitoAssertionError e) {
+                if (canRecoverFromFailure(delegate)) {
+                    error = e;
+                    sleep(pollingPeriod);
+                } else {
+                    throw e;
+                }
             }
         }
         
-        delegate.verify(data);
+        if (error != null) {
+            throw error;
+        }
+    }
+
+    private boolean canRecoverFromFailure(VerificationMode verificationMode) {
+        return !(verificationMode instanceof AtMost || verificationMode instanceof Only || verificationMode instanceof NoMoreInteractions);
+    }
+
+    private void sleep(int sleep) {
+        try {
+            Thread.sleep(sleep);
+        } catch (InterruptedException ie) {
+            // oups. not much luck.
+        }
     }
 
     public int getDelay() {
         return delayMillis;
+    }
+
+    public int getPollingPeriod() {
+        return pollingPeriod;
     }
 
 }
