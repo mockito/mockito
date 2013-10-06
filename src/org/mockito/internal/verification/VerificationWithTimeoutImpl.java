81601c5ab0c56bc3aa5efc35da44fe4f31bd91cd
pimterry <pimterry@gmail.com>
2013-10-06 16:05:24 +0100

    Added JavaDoc explaining the details of VerificationWithTimeout

--- src/org/mockito/internal/verification/VerificationWithTimeoutImpl.java
+++ src/org/mockito/internal/verification/VerificationWithTimeoutImpl.java
@@ -8,18 +8,39 @@ import org.mockito.exceptions.base.MockitoAssertionError;
 import org.mockito.internal.verification.api.VerificationData;
 import org.mockito.verification.VerificationMode;
 
+/**
+ * Verifies that another verification mode (the delegate) becomes true within a certain timeframe
+ * (before timeoutMillis has passed, measured from the call to verify()).
+ */
 public class VerificationWithTimeoutImpl {
     
     VerificationMode delegate;
     int timeoutMillis;
     int pollingPeriod;
 
+    /**
+     * Create this verification mode, to be used to verify invocation ongoing data later.
+     *
+     * @param pollingPeriod The frequency to poll delegate.verify(), to check whether the delegate has been satisfied
+     * @param timeoutMillis The time to wait (in millis) for the delegate verification mode to be satisfied
+     * @param delegate The verification mode to delegate overall success or failure to
+     */
     public VerificationWithTimeoutImpl(int pollingPeriod, int timeoutMillis, VerificationMode delegate) {
         this.pollingPeriod = pollingPeriod;
         this.timeoutMillis = timeoutMillis;
         this.delegate = delegate;
     }
 
+    /**
+     * Verify the given ongoing verification data, and confirm that it satisfies the delegate verification mode
+     * before the timeout has passed.
+     *
+     * In practice, this polls the delegate verification mode, and returns successfully as soon as
+     * the delegate is satisfied. If the delegate is not satisfied before the timeout has passed, the last
+     * error returned by the delegate verification mode will be thrown here in turn.
+     *
+     * @throws MockitoAssertionError if the delegate verification mode does not succeed before the timeout
+     */
     public void verify(VerificationData data) {
         MockitoAssertionError error = null;
         
