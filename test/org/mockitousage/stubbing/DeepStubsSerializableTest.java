c9ea446b45d26fe782ff9fa91d4cdca91dd162da
Marcin.Grzejszczak <marcin@grzejszczak.pl>
2014-01-10 16:31:34 +0100

    Issue 399 generics : Added test (ignored for the time being) that replicates the issue

--- test/org/mockitousage/stubbing/DeepStubsSerializableTest.java
+++ test/org/mockitousage/stubbing/DeepStubsSerializableTest.java
@@ -4,8 +4,11 @@ import static org.fest.assertions.Assertions.*;
 import static org.mockito.Mockito.*;
 
 import java.io.Serializable;
+import java.util.Map;
 
+import org.junit.Ignore;
 import org.junit.Test;
+import org.mockito.Matchers;
 import org.mockito.Mockito;
 import org.mockitoutil.SimpleSerializationUtil;
 
@@ -30,6 +33,22 @@ public class DeepStubsSerializableTest {
         assertThat(deserializedSample.getSample().isSth()).isEqualTo(STUBBED_BOOLEAN_VALUE);
         assertThat(deserializedSample.getSample().getNumber()).isEqualTo(STUBBED_INTEGER_VALUE);
     }
+
+	@Test
+	@Ignore
+	public void should_serialize_and_deserialize_map_mocked_by_deep_stubs() throws Exception {
+		// given
+		Map map = Mockito.mock(Map.class, withSettings().defaultAnswer(Mockito.RETURNS_DEEP_STUBS).serializable());
+		Mockito.when(map.entrySet().contains(Matchers.anyString())).thenReturn(STUBBED_BOOLEAN_VALUE);
+
+		// when
+		Object o = SimpleSerializationUtil.serializeAndBack(map);
+		
+		// then
+		assertThat(o).isInstanceOf(Map.class);
+		Map deserializedMap = (Map) o;
+		assertThat(deserializedMap.entrySet().contains("Something")).isEqualTo(STUBBED_BOOLEAN_VALUE);
+	}
 
 
     class SampleClass implements Serializable {
