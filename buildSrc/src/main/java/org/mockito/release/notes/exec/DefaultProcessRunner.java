ebe60b77e13863ee46ae3b1ade569e60cceeec4b
Szczepan Faber <szczepiq@gmail.com>
2014-11-21 10:10:51 +0100

    buildSrc tidy-up. Now it is all java code.
    Tests are still spock of course!!!

---
+++ buildSrc/src/main/java/org/mockito/release/notes/exec/DefaultProcessRunner.java
@@ -0,0 +1,42 @@
+package org.mockito.release.notes.exec;
+
+import org.mockito.release.notes.util.ReleaseNotesException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.*;
+import java.util.Arrays;
+
+class DefaultProcessRunner implements ProcessRunner {
+
+    private static Logger LOG = LoggerFactory.getLogger(DefaultProcessRunner.class);
+    private final File workDir;
+
+    DefaultProcessRunner(File workDir) {
+        this.workDir = workDir;
+    }
+
+    public String run(String... commandLine) {
+        LOG.info("Executing command: {}", (Object) commandLine);
+
+        try {
+            Process process = new ProcessBuilder(commandLine).directory(workDir).redirectErrorStream(true).start();
+            return readFully(new BufferedReader(new InputStreamReader(process.getInputStream())));
+        } catch (Exception e) {
+            throw new ReleaseNotesException("Problems executing command: " + Arrays.toString(commandLine), e);
+        }
+    }
+
+    private static String readFully(BufferedReader reader) throws IOException {
+        try {
+            StringBuilder sb = new StringBuilder();
+            String line;
+            while((line = reader.readLine()) != null) {
+                sb.append(line).append("\n");
+            }
+            return sb.toString();
+        } finally {
+            reader.close();
+        }
+    }
+}
