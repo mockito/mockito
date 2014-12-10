726ba2a9e2507dbbea367256f84e8829ebc51a30
Szczepan Faber <szczepiq@gmail.com>
2014-12-10 12:54:50 +0100

    Added documentation

--- buildSrc/src/main/groovy/org/mockito/release/notes/exec/Exec.java
+++ buildSrc/src/main/groovy/org/mockito/release/notes/exec/Exec.java
@@ -2,8 +2,14 @@ package org.mockito.release.notes.exec;
 
 import java.io.File;
 
+/**
+ * Process execution services
+ */
 public class Exec {
 
+    /**
+     * Provides process runner for given working dir
+     */
     public static ProcessRunner getProcessRunner(File workDir) {
         return new DefaultProcessRunner(workDir);
     }
