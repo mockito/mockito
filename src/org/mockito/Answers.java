621d945e04d933e8316d1f6ef121a315c474066b
Christian Schwarz <chriss.dev@github>
2015-01-02 13:00:11 +0100

    Add a javadoc note to indicate that this method will be removed in a future release.

--- src/org/mockito/Answers.java
+++ src/org/mockito/Answers.java
@@ -78,7 +78,7 @@ public enum Answers implements Answer<Object>{
     }
 
     /**
-     * @deprecated Use the enum-constant directly, instead of this getter.<br> 
+     * @deprecated Use the enum-constant directly, instead of this getter. This method will be removed in a future release<br> 
      * E.g. instead of <code>Answers.CALLS_REAL_METHODS.get()</code> use <code>Answers.CALLS_REAL_METHODS</code> .
      */
     @Deprecated
