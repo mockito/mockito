44fbd4985cb7448b727df60df398c1b7705b7602
Brice Dutheil <brice.dutheil@gmail.com>
2015-02-16 02:17:52 +0100

    Fixes and cleans test code according to the reported issue #169

--- test/org/mockitousage/annotation/WrongSetOfAnnotationsTest.java
+++ test/org/mockitousage/annotation/WrongSetOfAnnotationsTest.java
@@ -5,7 +5,7 @@
 package org.mockitousage.annotation;
 
 import java.util.List;
-
+import org.fest.assertions.Assertions;
 import org.junit.Test;
 import org.mockito.ArgumentCaptor;
 import org.mockito.Captor;
@@ -18,48 +18,65 @@ import org.mockitoutil.TestBase;
 
 @SuppressWarnings({"unchecked", "unused"})
 public class WrongSetOfAnnotationsTest extends TestBase {
-    
+
     @Test(expected=MockitoException.class)
-    public void shouldNotAllowMockAndSpy() throws Exception {
+    public void should_not_allow_Mock_and_Spy() throws Exception {
         MockitoAnnotations.initMocks(new Object() {
             @Mock @Spy List mock;
         });
     }
-    
-    @Test(expected=MockitoException.class)
-    public void shouldNotAllowSpyAndInjectMock() throws Exception {
-        MockitoAnnotations.initMocks(new Object() {
-            @InjectMocks @Spy List mock;
-        });
+
+    @Test
+    public void should_not_allow_Spy_and_or_InjectMocks_on_interfaces() throws Exception {
+        try {
+            MockitoAnnotations.initMocks(new Object() { @InjectMocks @Spy List mock; });
+            fail();
+        } catch (MockitoException me) {
+            Assertions.assertThat(me.getMessage()).contains("'List' is an interface");
+        }
+        try {
+            MockitoAnnotations.initMocks(new Object() { @Spy List mock; });
+            fail();
+        } catch (MockitoException me) {
+            Assertions.assertThat(me.getMessage()).contains("'List' is an interface");
+        }
     }
-    
+
+//    @Test
+//    public void should_allow_Spy_and_InjectMocks() throws Exception {
+//        MockitoAnnotations.initMocks(new Object() {
+//            @InjectMocks
+//            @Spy
+//            WithDependency mock;
+//        });
+//    }
+//    static class WithDependency { List list; }
+
     @Test(expected=MockitoException.class)
-    public void shouldNotAllowMockAndInjectMock() throws Exception {
+    public void should_not_allow_Mock_and_InjectMocks() throws Exception {
         MockitoAnnotations.initMocks(new Object() {
             @InjectMocks @Mock List mock;
         });
     }
-    
+
     @Test(expected=MockitoException.class)
-    public void shouldNotAllowCaptorAndMock() throws Exception {
+    public void should_not_allow_Captor_and_Mock() throws Exception {
         MockitoAnnotations.initMocks(new Object() {
             @Mock @Captor ArgumentCaptor captor;
         });
     }
-    
+
     @Test(expected=MockitoException.class)
-    public void shouldNotAllowCaptorAndSpy() throws Exception {
+    public void should_not_allow_Captor_and_Spy() throws Exception {
         MockitoAnnotations.initMocks(new Object() {
             @Spy @Captor ArgumentCaptor captor;
         });
     }
-    
+
     @Test(expected=MockitoException.class)
-    public void shouldNotAllowCaptorAndInjectMock() throws Exception {
+    public void should_not_allow_Captor_and_InjectMocks() throws Exception {
         MockitoAnnotations.initMocks(new Object() {
             @InjectMocks @Captor ArgumentCaptor captor;
         });
     }
-    
-    
 }
