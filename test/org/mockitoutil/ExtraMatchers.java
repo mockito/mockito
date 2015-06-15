e1c7c92506d527da1b37555fb65ab8bf9ac074ce
durron597 <martin.jared@gmail.com>
2015-06-15 10:13:08 -0500

    It's better to use isA, not is(InstanceOf

--- test/org/mockitoutil/ExtraMatchers.java
+++ test/org/mockitoutil/ExtraMatchers.java
@@ -134,7 +134,7 @@ public class ExtraMatchers {
     }
     
     public static org.hamcrest.Matcher<java.lang.Object> clazz(java.lang.Class<?> type) {
-        return CoreMatchers.is(CoreMatchers.instanceOf(type));
+        return CoreMatchers.isA(type);
     }
 
     public static Assertor hasMethodsInStackTrace(final String ... methods) {
