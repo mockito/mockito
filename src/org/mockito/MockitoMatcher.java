18baa5f85ada540980898e23a8bbdcfccd7ecb67
Szczepan Faber <szczepiq@gmail.com>
2015-06-22 22:05:29 -0700

    Continued to decouple Mockito from hamcrest
    - Updated the documentation for the MockitoMatcher interface.
    
    Issue #154

--- src/org/mockito/MockitoMatcher.java
+++ src/org/mockito/MockitoMatcher.java
@@ -4,15 +4,40 @@ package org.mockito;
  * Allows creating customized argument matchers.
  * This API was changed in Mockito 2.*
  * <p>
- * ArgumentMatcher is an hamcrest {@link org.hamcrest.Matcher} with predefined describeTo() method.
- * In case of failure, ArgumentMatcher generates description based on <b>decamelized class name</b> - to promote meaningful class names.
- * For example <b>StringWithStrongLanguage</b> matcher will generate 'String with strong language' description.
- * You can always override describeTo() method and provide detailed description.
+ * For non-trivial method arguments used in stubbing or verification, you have following options
+ * (in no particular order):
+ * <li>
+ *     <ul>refactor the code so that the interactions with collaborators are easier to test with mocks.
+ *     Perhaps it is possible to pass a different argument to the method so that mocking is easier?
+ *     If stuff is hard to test it usually indicates the design could be better, so refactor for testability.
+ *     </ul>
+ *     <ul>don't match the argument strictly, just use one of the lenient argument matchers like
+ *     {@link Mockito#notNull()}. Some times it is better to have a simple test that works than
+ *     a complicated test that seem to work.
+ *     </ul>
+ *     <ul>implement equals() method in the objects that are used as arguments to mocks.
+ *     Mockito naturally uses equals() for argument matching.
+ *     Many times, this is option is clean and simple.
+ *     </ul>
+ *     <ul>use {@link ArgumentCaptor} to capture the arguments and perform assertions on their state.
+ *     Useful when you need to verify the arguments. Captor is not useful if you need argument matching for stubbing.
+ *     Many times, this option leads to clean and readable tests with fine-grained validation of arguments.
+ *     </ul>
+ *     <ul>use customized argument matchers by implementing {@link MockitoMatcher} interface
+ *     and passing the implementation to the {@link Mockito#argThat} method.
+ *     This option is useful if custom matcher is needed for stubbing and can be reused a lot
+ *     </ul>
+ *     <ul>use an instance of hamcrest matcher and pass it to
+ *     TODO
+ *     Useful if you already have a hamcrest matcher. Reuse and win!
+ *     </ul>
+ * </li>
+ *
  * <p>
- * Use {@link Matchers#argThat} method and pass an instance of hamcrest {@link org.hamcrest.Matcher}, e.g:
+ * Implementations of this interface can be used with {@link Matchers#argThat} method:
  *
  * <pre class="code"><code class="java">
- * class ListOfTwoElements extends ArgumentMatcher&lt;List&gt; {
+ * class ListOfTwoElements implements MockitoMatcher&lt;List&gt; {
  *     public boolean matches(Object list) {
  *         return ((List) list).size() == 2;
  *     }
@@ -20,29 +45,21 @@ package org.mockito;
  *
  * List mock = mock(List.class);
  *
- * when(mock.addAll(argThat(new IsListOfTwoElements))).thenReturn(true);
+ * when(mock.addAll(argThat(new ListOfTwoElements))).thenReturn(true);
  *
  * mock.addAll(Arrays.asList(&quot;one&quot;, &quot;two&quot;));
  *
- * verify(mock).addAll(argThat(new IsLiListOfTwoElements;
+ * verify(mock).addAll(argThat(new ListOfTwoElements()));
  * </code></pre>
  *
- * To keep it readable you may want to extract method, e.g:
+ * To keep it readable you can extract method, e.g:
  *
  * <pre class="code"><code class="java">
- *   verify(mock).addAll(<b>argThat(new IsListOfTwoElements())</b>);
+ *   verify(mock).addAll(<b>argThat(new ListOfTwoElements())</b>);
  *   //becomes
  *   verify(mock).addAll(<b>listOfTwoElements()</b>);
  * </code></pre>
  *
- * <b>Warning:</b> Be reasonable with using complicated argument matching, especially custom argument matchers, as it can make the test less readable.
- * Sometimes it's better to implement equals() for arguments that are passed to mocks
- * (Mockito naturally uses equals() for argument matching).
- * This can make the test cleaner.
- * <p>
- * Also, <b>sometimes {@link ArgumentCaptor} may be a better fit</b> than custom matcher.
- * For example, if custom argument matcher is not likely to be reused
- * or you just need it to assert on argument values to complete verification of behavior.
  * <p>
  * Read more about other matchers in javadoc for {@link Matchers} class
  *
@@ -55,12 +72,14 @@ public interface MockitoMatcher<T> {
      * <p>
      * The method should <b>never</b> assert if the argument doesn't match. It
      * should only return false.
+     * <p>
+     * The argument is not using generic type in order to force explicit casting in the implementation.
+     * This way it is easier to debug when incompatible arguments are passed to the matchers.
+     * You have to trust us on this one.
      *
      * @param argument
      *            the argument
      * @return true if this matcher accepts the given argument.
      */
     public boolean matches(Object argument);
-
-    //TODO SF should it extend serializable?
 }
