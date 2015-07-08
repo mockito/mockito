854e641802988a689dc438abbd146049ebceb586
Szczepan Faber <szczepiq@gmail.com>
2015-07-08 07:23:36 -0700

    Removed all checked-in jars
    - Changed the whitebox test so that it does not use powermock. I don't think it is necessary to use powermock for this validation. One less dependency is always good (even for tests).
    - Declared fest dependencies instead of keeping them checked-in
    
    Fixes #250

--- test/org/mockito/internal/util/reflection/WhiteboxTest.java
+++ test/org/mockito/internal/util/reflection/WhiteboxTest.java
@@ -10,24 +10,13 @@ import org.mockitoutil.TestBase;
 public class WhiteboxTest extends TestBase {
 
     @Test
-    public void shouldSetInternalStateOnHierarchy() {
+    public void private_state() {
         //given
         DummyClassForTests dummy = new DummyClassForTests();
         //when
         Whitebox.setInternalState(dummy, "somePrivateField", "cool!");
         //then
-        Object internalState = org.powermock.reflect.Whitebox.getInternalState(dummy, "somePrivateField");
-        assertEquals("cool!", internalState);
-    }
-
-    @Test
-    public void shouldGetInternalStateFromHierarchy() {
-        //given
-        DummyClassForTests dummy = new DummyClassForTests();
-        org.powermock.reflect.Whitebox.setInternalState(dummy, "somePrivateField", "boo!");
-        //when
         Object internalState = Whitebox.getInternalState(dummy, "somePrivateField");
-        //then
-        assertEquals("boo!", internalState);
+        assertEquals("cool!", internalState);
     }
 }
