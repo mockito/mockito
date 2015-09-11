a777b93fdf5a5b87acc88c9e2bd6c778e77d7d3c
Emmeran Seehuber <rototor@rototor.de>
2015-09-11 13:46:33 +0200

    Allow the collection of stacktraces in LocationImpl to be disabled.

--- src/test/java/org/mockitousage/internal/debugging/LocationImplTest.java
+++ src/test/java/org/mockitousage/internal/debugging/LocationImplTest.java
@@ -12,25 +12,36 @@ import org.mockitoutil.TestBase;
 @SuppressWarnings("serial")
 public class LocationImplTest extends TestBase {
 
-    @Test
-    public void shouldLocationNotContainGetStackTraceMethod() {
-        assertContains("shouldLocationNotContainGetStackTraceMethod", new LocationImpl().toString());
-    }
+	@Test
+	public void shouldLocationNotContainGetStackTraceMethod() {
+		assertContains("shouldLocationNotContainGetStackTraceMethod", new LocationImpl().toString());
+	}
 
-    @Test
-    public void shouldBeSafeInCaseForSomeReasonFilteredStackTraceIsEmpty() {
-        //given
-        StackTraceFilter filterReturningEmptyArray = new StackTraceFilter() {
-            @Override
-            public StackTraceElement[] filter(StackTraceElement[] target, boolean keepTop) {
-                return new StackTraceElement[0];
-            }
-        };
+	@Test
+	public void shouldBeSafeInCaseForSomeReasonFilteredStackTraceIsEmpty() {
+		// given
+		StackTraceFilter filterReturningEmptyArray = new StackTraceFilter() {
+			@Override
+			public StackTraceElement[] filter(StackTraceElement[] target, boolean keepTop) {
+				return new StackTraceElement[0];
+			}
+		};
 
-        //when
-        String loc = new LocationImpl(filterReturningEmptyArray).toString();
+		// when
+		String loc = new LocationImpl(filterReturningEmptyArray).toString();
 
-        //then
-        assertEquals("-> at <<unknown line>>", loc);
-    }
+		// then
+		assertEquals("-> at <<unknown line>>", loc);
+	}
+
+	@Test
+	public void disableStackTraceTest() {
+		LocationImpl.ENABLE_STACKTRACE = false;
+		try {
+			assertEquals("-> at <<unknown line>>", new LocationImpl().toString());
+		} finally {
+			LocationImpl.ENABLE_STACKTRACE = true;
+		}
+		assertNotContains("-> at <<unknown line>>", new LocationImpl().toString());
+	}
 }
