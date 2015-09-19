99c9320a4208721833d0eab3b6caa889e168b239
fluentfuture <benyu@benyu-macbookpro2.roam.corp.google.com>
2015-09-19 14:25:41 -0500

    Remove validateSerializable

--- src/main/java/org/mockito/internal/util/MockCreationValidator.java
+++ src/main/java/org/mockito/internal/util/MockCreationValidator.java
@@ -54,19 +54,6 @@ public class MockCreationValidator {
         }
     }
 
-    public void validateSerializable(Class<?> classToMock, boolean serializable) {
-        // We can't catch all the errors with this piece of code
-        // Having a **superclass that do not implements Serializable** might fail as well when serialized
-        // Though it might prevent issues when mockito is mocking a class without superclass.
-        if(serializable
-                && !classToMock.isInterface()
-                && !(Serializable.class.isAssignableFrom(classToMock))
-                && Constructors.noArgConstructorOf(classToMock) == null
-                ) {
-            new Reporter().serializableWontWorkForObjectsThatDontImplementSerializable(classToMock);
-        }
-    }
-
     public void validateConstructorUse(boolean usingConstructor, SerializableMode mode) {
         if (usingConstructor && mode == SerializableMode.ACROSS_CLASSLOADERS) {
             new Reporter().usingConstructorWithFancySerializable(mode);
