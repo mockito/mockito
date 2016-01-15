74c871c18ce3ac069595417965caf0b14ea687e8
Rafael Winterhalter <rafael.wth@gmail.com>
2016-01-15 16:14:55 +0100

    Disabled explicit type validation since mocks are guaranteed to be valid. Speeds up code generation.

--- src/main/java/org/mockito/internal/creation/bytebuddy/MockBytecodeGenerator.java
+++ src/main/java/org/mockito/internal/creation/bytebuddy/MockBytecodeGenerator.java
@@ -40,16 +40,19 @@ class MockBytecodeGenerator {
                          .name(nameFor(features.mockedType))
                          .ignoreAlso(isGroovyMethod())
                          .annotateType(features.mockedType.getAnnotations())
-                         .serialVersionUid(42L)
                          .implement(new ArrayList<Type>(features.interfaces))
                          .method(any())
                            .intercept(MethodDelegation.to(DispatcherDefaultingToRealMethod.class))
                            .transform(MethodTransformer.Simple.withModifiers(SynchronizationState.PLAIN))
                            .attribute(MethodAttributeAppender.ForInstrumentedMethod.INSTANCE)
+                         .serialVersionUid(42L)
                          .defineField("mockitoInterceptor", MockMethodInterceptor.class, PRIVATE)
-                         .implement(MockAccess.class).intercept(FieldAccessor.ofBeanProperty())
-                         .method(isHashCode()).intercept(to(MockMethodInterceptor.ForHashCode.class))
-                         .method(isEquals()).intercept(to(MockMethodInterceptor.ForEquals.class));
+                         .implement(MockAccess.class)
+                           .intercept(FieldAccessor.ofBeanProperty())
+                         .method(isHashCode())
+                           .intercept(to(MockMethodInterceptor.ForHashCode.class))
+                         .method(isEquals())
+                           .intercept(to(MockMethodInterceptor.ForEquals.class));
         if (features.crossClassLoaderSerializable) {
             builder = builder.implement(CrossClassLoaderSerializableMock.class)
                              .intercept(to(MockMethodInterceptor.ForWriteReplace.class));
