bbd20383958b7773bed480c2d27cb583a4c42a4f
Rafael Winterhalter <rafael.wth@gmail.com>
2016-01-15 15:03:37 +0100

    Updated to Byte Buddy 1.0.0

--- src/main/java/org/mockito/internal/creation/bytebuddy/MockBytecodeGenerator.java
+++ src/main/java/org/mockito/internal/creation/bytebuddy/MockBytecodeGenerator.java
@@ -7,21 +7,18 @@ import net.bytebuddy.dynamic.DynamicType;
 import net.bytebuddy.dynamic.MethodTransformer;
 import net.bytebuddy.dynamic.loading.ClassLoadingStrategy;
 import net.bytebuddy.dynamic.loading.MultipleParentClassLoader;
-import net.bytebuddy.dynamic.scaffold.subclass.ConstructorStrategy;
 import net.bytebuddy.implementation.FieldAccessor;
 import net.bytebuddy.implementation.MethodDelegation;
-import net.bytebuddy.implementation.attribute.AnnotationAppender;
 import net.bytebuddy.implementation.attribute.MethodAttributeAppender;
-import net.bytebuddy.implementation.attribute.TypeAttributeAppender;
 import net.bytebuddy.matcher.ElementMatcher;
 import org.mockito.internal.creation.bytebuddy.ByteBuddyCrossClassLoaderSerializationSupport.CrossClassLoaderSerializableMock;
 import org.mockito.internal.creation.bytebuddy.MockMethodInterceptor.DispatcherDefaultingToRealMethod;
 import org.mockito.internal.creation.bytebuddy.MockMethodInterceptor.MockAccess;
 
+import java.lang.reflect.Type;
+import java.util.ArrayList;
 import java.util.Random;
 
-import static net.bytebuddy.description.modifier.FieldManifestation.FINAL;
-import static net.bytebuddy.description.modifier.Ownership.STATIC;
 import static net.bytebuddy.description.modifier.Visibility.PRIVATE;
 import static net.bytebuddy.implementation.MethodDelegation.to;
 import static net.bytebuddy.matcher.ElementMatchers.*;
@@ -32,25 +29,26 @@ class MockBytecodeGenerator {
     private final Random random;
 
     public MockBytecodeGenerator() {
-        byteBuddy = new ByteBuddy()
-                .withDefaultMethodAttributeAppender(new MethodAttributeAppender.ForInstrumentedMethod(AnnotationAppender.ValueFilter.AppendDefaults.INSTANCE))
-                .withAttribute(new TypeAttributeAppender.ForInstrumentedType(AnnotationAppender.ValueFilter.AppendDefaults.INSTANCE));
+        byteBuddy = new ByteBuddy();
         random = new Random();
     }
 
     public <T> Class<? extends T> generateMockClass(MockFeatures<T> features) {
         DynamicType.Builder<T> builder =
-                byteBuddy.subclass(features.mockedType, ConstructorStrategy.Default.IMITATE_SUPER_TYPE)
+                byteBuddy.subclass(features.mockedType)
                          .name(nameFor(features.mockedType))
-                         .implement(features.interfaces.toArray(new Class<?>[features.interfaces.size()]))
-                         .method(any()).intercept(MethodDelegation.to(DispatcherDefaultingToRealMethod.class),
-                                                  MethodTransformer.Simple.withModifiers(SynchronizationState.PLAIN))
+                         .ignoreAlso(isGroovyMethod())
+                         .annotateType(features.mockedType.getAnnotations())
+                         .serialVersionUid(42L)
+                         .implement(new ArrayList<Type>(features.interfaces))
+                         .method(any())
+                           .intercept(MethodDelegation.to(DispatcherDefaultingToRealMethod.class))
+                           .transform(MethodTransformer.Simple.withModifiers(SynchronizationState.PLAIN))
+                           .attribute(MethodAttributeAppender.ForInstrumentedMethod.INSTANCE)
                          .defineField("mockitoInterceptor", MockMethodInterceptor.class, PRIVATE)
                          .implement(MockAccess.class).intercept(FieldAccessor.ofBeanProperty())
                          .method(isHashCode()).intercept(to(MockMethodInterceptor.ForHashCode.class))
-                         .method(isEquals()).intercept(to(MockMethodInterceptor.ForEquals.class))
-                         .defineField("serialVersionUID", long.class, STATIC, PRIVATE, FINAL).value(42L)
-                         .ignoreMethods(isGroovyMethod());
+                         .method(isEquals()).intercept(to(MockMethodInterceptor.ForEquals.class));
         if (features.crossClassLoaderSerializable) {
             builder = builder.implement(CrossClassLoaderSerializableMock.class)
                              .intercept(to(MockMethodInterceptor.ForWriteReplace.class));
