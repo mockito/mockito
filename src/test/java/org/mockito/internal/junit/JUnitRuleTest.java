4a2417865b05cb20dc3de0540a0f38ec640cb634
Szczepan Faber <szczepiq@gmail.com>
2016-04-17 16:31:24 -0700

    Removed unnecessary test stub
    This case is covered in unit tests already.

--- src/test/java/org/mockito/internal/junit/JUnitRuleTest.java
+++ src/test/java/org/mockito/internal/junit/JUnitRuleTest.java
@@ -63,11 +63,6 @@ public class JUnitRuleTest {
     }
 
     @Test
-    public void arg_mismatch_warnings_are_at_the_top() {
-        //TODO
-    }
-
-    @Test
     public void shouldNotWarnAboutUnusedStubsWhenPassed() throws Throwable {
         jUnitRule.apply(new Statement() {
             public void evaluate() throws Throwable {
