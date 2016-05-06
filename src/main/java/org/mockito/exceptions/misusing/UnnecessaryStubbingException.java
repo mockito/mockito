54fb8cb9e189631c5ddad3435f3d36d50d1104bb
Szczepan Faber <sfaber@linkedin.com>
2016-05-06 15:36:24 -0700

    Improved exception message for unnecessary stubs
    - made the exception message much more descriptive
    - improved test coverage

--- src/main/java/org/mockito/exceptions/misusing/UnnecessaryStubbingException.java
+++ src/main/java/org/mockito/exceptions/misusing/UnnecessaryStubbingException.java
@@ -3,6 +3,8 @@ package org.mockito.exceptions.misusing;
 /**
  * Indicates presence of stubbings in the test class that were never used in the code during test execution.
  * Such stubbings should be removed from the test class to keep the test code clean.
+ *
+ * TODO this javadoc needs to better describe why it is important, what to do to work with it.
  */
 public class UnnecessaryStubbingException extends RuntimeException {
     public UnnecessaryStubbingException(String message) {
