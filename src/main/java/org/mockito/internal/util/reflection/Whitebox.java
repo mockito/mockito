42c81daa3072027ac85e033095ae21f39829830a
Andrei Cristian Petcu <andrei@ceata.org>
2016-05-31 21:22:14 +0300

    Fixes #422 : Allow setting private static final

--- src/main/java/org/mockito/internal/util/reflection/Whitebox.java
+++ src/main/java/org/mockito/internal/util/reflection/Whitebox.java
@@ -5,11 +5,18 @@
 package org.mockito.internal.util.reflection;
 
 import java.lang.reflect.Field;
+import java.lang.reflect.Modifier;
 
 public class Whitebox {
 
     public static Object getInternalState(Object target, String field) {
-        Class<?> c = target.getClass();
+        Class<?> c;
+        if(target instanceof Class){
+            c = (Class<?>) target;
+            target = null;
+        } else {
+            c = target.getClass();
+        }
         try {
             Field f = getFieldFromHierarchy(c, field);
             f.setAccessible(true);
@@ -20,13 +27,31 @@ public class Whitebox {
     }
 
     public static void setInternalState(Object target, String field, Object value) {
-        Class<?> c = target.getClass();
+        Class<?> c;
+        if(target instanceof Class){
+            c = (Class<?>) target;
+            target = null;
+        } else {
+            c = target.getClass();
+        }
         try {
             Field f = getFieldFromHierarchy(c, field);
             f.setAccessible(true);
+            removeFinalModifierIfPresent(f);
             f.set(target, value);
         } catch (Exception e) {
             throw new RuntimeException("Unable to set internal state on a private field. Please report to mockito mailing list.", e);
+        }
+    }
+
+    private static void removeFinalModifierIfPresent(Field fieldToRemoveFinalFrom) throws IllegalAccessException, NoSuchFieldException {
+        int fieldModifiersMask = fieldToRemoveFinalFrom.getModifiers();
+        boolean isFinalModifierPresent = (fieldModifiersMask & Modifier.FINAL) == Modifier.FINAL;
+        if (isFinalModifierPresent) {
+            Field modifiersField = Field.class.getDeclaredField("modifiers");
+            modifiersField.setAccessible(true);
+            int fieldModifiersMaskWithoutFinal = fieldModifiersMask & ~Modifier.FINAL;
+            modifiersField.setInt(fieldToRemoveFinalFrom, fieldModifiersMaskWithoutFinal);
         }
     }
 
--- src/test/java/org/mockito/internal/util/reflection/DummyParentClassForTests.java
+++ src/test/java/org/mockito/internal/util/reflection/DummyParentClassForTests.java
@@ -4,7 +4,12 @@
  */
 package org.mockito.internal.util.reflection;
 
+import java.util.logging.Logger;
+
 public class DummyParentClassForTests {
+
+    @SuppressWarnings("unused")//I know, I know. We're doing nasty reflection hacks here...
+    private static final Logger LOG = Logger.getLogger(DummyParentClassForTests.class.toString());
 
     @SuppressWarnings("unused")//I know, I know. We're doing nasty reflection hacks here...
     private String somePrivateField;
