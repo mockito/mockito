5428eea45560f63c3d0cb6810c5f2f15ec7bcdc7
Szczepan Faber <sfaber@linkedin.com>
2016-07-22 17:01:49 -0700

    Added a TODO

--- src/main/java/org/mockito/listeners/StubbingListener.java
+++ src/main/java/org/mockito/listeners/StubbingListener.java
@@ -1,12 +1,19 @@
 package org.mockito.listeners;
 
+import org.mockito.Incubating;
 import org.mockito.invocation.Invocation;
 
 /**
  * Created by sfaber on 5/2/16.
  */
+@Incubating
 public interface StubbingListener {
 
     void newStubbing(Invocation stubbing);
+
+    /**
+     * TODO make it thread safe so that users don't have to worry
+     * TODO add javadoc
+     */
     void usedStubbing(Invocation stubbing, Invocation actual);
 }
