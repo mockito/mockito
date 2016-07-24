49f53d0eb657a362229dcbdb663c644f1c67685c
Pascal Schumacher <pascalschumacher@gmx.net>
2016-07-24 17:13:26 +0200

    refactored ArgumentsComparator to a static utility class (#426)

--- src/main/java/org/mockito/internal/invocation/ArgumentsComparator.java
+++ src/main/java/org/mockito/internal/invocation/ArgumentsComparator.java
@@ -12,12 +12,15 @@ import org.mockito.invocation.Invocation;
 
 @SuppressWarnings("unchecked")
 public class ArgumentsComparator {
-    public boolean argumentsMatch(InvocationMatcher invocationMatcher, Invocation actual) {
+
+    private ArgumentsComparator() {}
+
+    public static boolean argumentsMatch(InvocationMatcher invocationMatcher, Invocation actual) {
         Object[] actualArgs = actual.getArguments();
         return argumentsMatch(invocationMatcher, actualArgs) || varArgsMatch(invocationMatcher, actual);
     }
 
-    public boolean argumentsMatch(InvocationMatcher invocationMatcher, Object[] actualArgs) {
+    public static boolean argumentsMatch(InvocationMatcher invocationMatcher, Object[] actualArgs) {
         if (actualArgs.length != invocationMatcher.getMatchers().size()) {
             return false;
         }
