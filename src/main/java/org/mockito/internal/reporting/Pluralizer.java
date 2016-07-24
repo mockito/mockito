7ead73e206fb3edbf27f3e3b11a81f6d45dff319
Szczepan Faber <szczepiq@gmail.com>
2016-07-24 10:16:08 -0700

    Improved exception message
    Fixes #506

--- src/main/java/org/mockito/internal/reporting/Pluralizer.java
+++ src/main/java/org/mockito/internal/reporting/Pluralizer.java
@@ -9,4 +9,12 @@ public class Pluralizer {
     public static String pluralize(int number) {
         return number == 1 ? "1 time" : number + " times";
     }
+
+    public static String were_exactly_x_interactions(int x) {
+        if (x == 1) {
+            return "was exactly 1 interaction";
+        } else {
+            return "were exactly " + x + " interactions";
+        }
+    }
 }
