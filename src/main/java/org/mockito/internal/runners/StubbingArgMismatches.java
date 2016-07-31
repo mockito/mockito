b104f617242571fb8cdd702dec07ef9f3ec3c73f
Szczepan Faber <szczepiq@gmail.com>
2016-07-31 12:53:17 -0700

    Improved the hint message, added coverage
    Issue #384

--- src/main/java/org/mockito/internal/runners/StubbingArgMismatches.java
+++ src/main/java/org/mockito/internal/runners/StubbingArgMismatches.java
@@ -3,22 +3,19 @@ package org.mockito.internal.runners;
 import org.mockito.internal.util.MockitoLogger;
 import org.mockito.invocation.Invocation;
 
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
 
 /**
  * Renders the stubbing hint on argument mismatches
  */
 class StubbingArgMismatches {
 
-    Map<Invocation, Set<Invocation>> mismatches = new HashMap<Invocation, Set<Invocation>>();
+    Map<Invocation, Set<Invocation>> mismatches = new LinkedHashMap<Invocation, Set<Invocation>>();
 
     public void add(Invocation invocation, Invocation stubbing) {
         Set<Invocation> matchingInvocations = mismatches.get(stubbing);
         if (matchingInvocations == null) {
-            matchingInvocations = new HashSet<Invocation>();
+            matchingInvocations = new LinkedHashSet<Invocation>();
             mismatches.put(stubbing, matchingInvocations);
         }
         matchingInvocations.add(invocation);
@@ -29,12 +26,14 @@ class StubbingArgMismatches {
             return;
         }
 
-        StringBuilder out = new StringBuilder("[MockitoHint] See javadoc for MockitoHint class.\n");
+        StringBuilder out = new StringBuilder("[MockitoHint] See javadoc for MockitoHint class.");
         int x = 1;
         for (Map.Entry<Invocation, Set<Invocation>> m : mismatches.entrySet()) {
-            out.append("[MockitoHint] ").append(x++).append(". unused stub  ")
-                    .append(m.getKey().getLocation()).append("\n");
-            out.append("[MockitoHint]    similar call ").append(m.getValue().iterator().next().getLocation());
+            out.append("\n[MockitoHint] ").append(x++).append(". unused stub  ")
+                    .append(m.getKey().getLocation());
+            for (Invocation invocation : m.getValue()) {
+                out.append("\n[MockitoHint]  - arg mismatch ").append(invocation.getLocation());
+            }
         }
 
         logger.log(out.toString());
---
+++ src/test/java/org/mockito/internal/runners/StubbingArgMismatchesTest.java
@@ -0,0 +1,106 @@
+package org.mockito.internal.runners;
+
+import org.junit.Test;
+import org.mockito.internal.invocation.InvocationBuilder;
+import org.mockito.internal.util.SimpleMockitoLogger;
+import org.mockito.invocation.Invocation;
+import org.mockitoutil.TestBase;
+
+import static org.junit.Assert.*;
+
+public class StubbingArgMismatchesTest extends TestBase {
+
+    SimpleMockitoLogger logger = new SimpleMockitoLogger();
+    StubbingArgMismatches mismatches = new StubbingArgMismatches();
+
+    @Test
+    public void no_op_when_no_mismatches() throws Exception {
+        //when
+        mismatches.log(logger);
+
+        //then
+        assertTrue(logger.isEmpty());
+    }
+
+    @Test
+    public void logs_mismatch() throws Exception {
+        //given
+        mismatches.add(
+                new InvocationBuilder().args("a").location("-> at A.java").toInvocation(),
+                new InvocationBuilder().args("b").location("-> at B.java").toInvocation());
+
+        //when
+        mismatches.log(logger);
+
+        //then
+        assertEquals(
+                "[MockitoHint] See javadoc for MockitoHint class.\n" +
+                "[MockitoHint] 1. unused stub  -> at B.java\n" +
+                "[MockitoHint]  - arg mismatch -> at A.java", logger.getLoggedInfo());
+    }
+
+    @Test
+    public void logs_multiple_mismatches() throws Exception {
+        //given
+        mismatches.add(
+                new InvocationBuilder().args("a").location("-> at A.java").toInvocation(),
+                new InvocationBuilder().args("b").location("-> at B.java").toInvocation());
+
+        mismatches.add(
+                new InvocationBuilder().method("differentMethod").args("x").location("-> at X.java").toInvocation(),
+                new InvocationBuilder().method("differentMethod").args("y").location("-> at Y.java").toInvocation());
+
+        //when
+        mismatches.log(logger);
+
+        //then
+        assertEquals(
+        "[MockitoHint] See javadoc for MockitoHint class.\n" +
+        "[MockitoHint] 1. unused stub  -> at B.java\n" +
+        "[MockitoHint]  - arg mismatch -> at A.java\n" +
+        "[MockitoHint] 2. unused stub  -> at Y.java\n" +
+        "[MockitoHint]  - arg mismatch -> at X.java", logger.getLoggedInfo());
+    }
+
+    @Test
+    public void multiple_matching_invocations_per_unused_stub() throws Exception {
+        //given
+        Invocation stubbing = new InvocationBuilder().args("a").location("-> at A.java").toInvocation();
+        mismatches.add(new InvocationBuilder().args("x").location("-> at X.java").toInvocation(), stubbing);
+        mismatches.add(new InvocationBuilder().args("y").location("-> at Y.java").toInvocation(), stubbing);
+
+        //when
+        mismatches.log(logger);
+
+        //then
+        assertEquals(
+        "[MockitoHint] See javadoc for MockitoHint class.\n" +
+        "[MockitoHint] 1. unused stub  -> at A.java\n" +
+        "[MockitoHint]  - arg mismatch -> at X.java\n" +
+        "[MockitoHint]  - arg mismatch -> at Y.java", logger.getLoggedInfo());
+    }
+
+    @Test
+    public void multiple_matching_invocations_per_stub_plus_some_other_invocation() throws Exception {
+        //given
+        Invocation stubbing = new InvocationBuilder().args("a").location("-> at A.java").toInvocation();
+        mismatches.add(new InvocationBuilder().args("x").location("-> at X.java").toInvocation(), stubbing);
+        mismatches.add(new InvocationBuilder().args("y").location("-> at Y.java").toInvocation(), stubbing);
+
+        mismatches.add(
+                new InvocationBuilder().method("differentMethod").args("n").location("-> at N.java").toInvocation(),
+                new InvocationBuilder().method("differentMethod").args("m").location("-> at M.java").toInvocation());
+
+        //when
+        mismatches.log(logger);
+
+        //then
+        assertEquals(
+        "[MockitoHint] See javadoc for MockitoHint class.\n" +
+        "[MockitoHint] 1. unused stub  -> at A.java\n" +
+        "[MockitoHint]  - arg mismatch -> at X.java\n" +
+        "[MockitoHint]  - arg mismatch -> at Y.java\n" +
+        "[MockitoHint] 2. unused stub  -> at M.java\n" +
+        "[MockitoHint]  - arg mismatch -> at N.java", logger.getLoggedInfo());
+    }
+}
