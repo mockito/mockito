727cf8d903ce92da903c6bd1f94da4a06ae14f67
Szczepan Faber <szczepiq@gmail.com>
2016-07-31 14:54:22 -0700

    Rename job
    Issue #384

--- src/test/java/org/mockito/internal/junit/RuleStubbingHintsReporterTest.java
+++ src/test/java/org/mockito/internal/junit/RuleStubbingHintsReporterTest.java
@@ -11,7 +11,7 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 import static org.mockito.Mockito.mock;
 
-public class RuleStubbingsReporterTest extends TestBase {
+public class RuleStubbingHintsReporterTest extends TestBase {
 
     RuleStubbingsReporter reporter = new RuleStubbingsReporter();
     SimpleMockitoLogger logger = new SimpleMockitoLogger();
