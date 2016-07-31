1ac8e819af61298f0f3606b13210d87ae8e2462c
Szczepan Faber <szczepiq@gmail.com>
2016-07-31 14:32:36 -0700

    Fixed the code
    Thank you, tests.
    
    Issue #384

--- src/main/java/org/mockito/internal/framework/DefaultMockitoFramework.java
+++ src/main/java/org/mockito/internal/framework/DefaultMockitoFramework.java
@@ -1,11 +1,17 @@
 package org.mockito.internal.framework;
 
 import org.mockito.MockitoFramework;
+import org.mockito.internal.progress.MockingProgress;
 import org.mockito.internal.progress.ThreadSafeMockingProgress;
 import org.mockito.listeners.StubbingListener;
 
 public class DefaultMockitoFramework implements MockitoFramework {
     public void setStubbingListener(StubbingListener listener) {
-        ThreadSafeMockingProgress.mockingProgress().setStubbingListener(new ThreadSafeStubbingListener(listener));
+        MockingProgress p = ThreadSafeMockingProgress.mockingProgress();
+        if (listener == null) {
+            p.setStubbingListener(null);
+        } else {
+            p.setStubbingListener(new ThreadSafeStubbingListener(listener));
+        }
     }
 }
