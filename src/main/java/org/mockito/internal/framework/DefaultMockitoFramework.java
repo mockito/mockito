3fc25ad563e77cb396c55acf78a972b47a76b4a9
Szczepan Faber <szczepiq@gmail.com>
2016-08-01 19:34:14 -0700

    Added a TODO
    Issue #384

--- src/main/java/org/mockito/internal/framework/DefaultMockitoFramework.java
+++ src/main/java/org/mockito/internal/framework/DefaultMockitoFramework.java
@@ -7,6 +7,7 @@ import org.mockito.listeners.StubbingListener;
 
 public class DefaultMockitoFramework implements MockitoFramework {
     public void setStubbingListener(StubbingListener listener) {
+        //TODO 384 - list of listeners, otherwise user can overwrite our own listeners
         MockingProgress p = ThreadSafeMockingProgress.mockingProgress();
         if (listener == null) {
             p.setStubbingListener(null);
