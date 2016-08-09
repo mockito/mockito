2735b0d4dc20d5cefeaf230732c6b9f43b86ff10
Szczepan Faber <sfaber@linkedin.com>
2016-08-08 21:31:36 -0700

    Added missing javadoc
    #384

--- src/main/java/org/mockito/MockitoFramework.java
+++ src/main/java/org/mockito/MockitoFramework.java
@@ -20,8 +20,34 @@ public interface MockitoFramework {
     @Incubating
     void setStubbingListener(StubbingListener listener);
 
-    //TODO 384 javadoc
+    /**
+     * Adds listener to Mockito.
+     * For a list of supported listeners, see the interfaces that extend {@link MockitoListener}.
+     * Depending on the type of listener, some listeners can be thread local.
+     * <p>
+     * Listeners can be useful for engs that extend Mockito framework.
+     * They are used in the implementation of unused stubbings warnings ({@link org.mockito.quality.MockitoHint}).
+     * <p>
+     * Make sure you remove the listener when the job is complete, see {@link #removeListener(MockitoListener)}
+     * <p>
+     * For usage examples, see Mockito codebase.
+     * If you have ideas and feature requests about Mockito listeners API
+     * we are very happy to hear about it via our issue tracker or mailing list.
+     *
+     * @param listener to add
+     */
+    @Incubating
     void addListener(MockitoListener listener);
 
+    /**
+     * When you add listener using {@link #addListener(MockitoListener)} make sure to remove it.
+     * <p>
+     * For usage examples, see Mockito codebase.
+     * If you have ideas and feature requests about Mockito listeners API
+     * we are very happy to hear about it via our issue tracker or mailing list.
+     *
+     * @param listener to remove
+     */
+    @Incubating
     void removeListener(MockitoListener listener);
 }
