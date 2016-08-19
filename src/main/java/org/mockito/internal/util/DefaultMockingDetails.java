4ea215f2e37eeedd11f3a3040883a87695c6ff12
Szczepan Faber <szczepiq@gmail.com>
2016-08-19 15:46:32 -0700

    Improved handling of incorrect input
    Issue 541

--- src/main/java/org/mockito/internal/util/DefaultMockingDetails.java
+++ src/main/java/org/mockito/internal/util/DefaultMockingDetails.java
@@ -5,12 +5,13 @@
 package org.mockito.internal.util;
 
 import org.mockito.MockingDetails;
+import org.mockito.exceptions.misusing.NotAMockException;
 import org.mockito.invocation.Invocation;
-
-import static org.mockito.internal.util.MockUtil.getMockHandler;
 
 import java.util.Collection;
 import java.util.Set;
+
+import static org.mockito.internal.util.MockUtil.getMockHandler;
 
 /**
  * Class to inspect any object, and identify whether a particular object is either a mock or a spy.  This is
@@ -36,7 +37,16 @@ public class DefaultMockingDetails implements MockingDetails {
 
     @Override
     public Collection<Invocation> getInvocations() {
+        assertIsMock(toInspect);
         return getMockHandler(toInspect).getInvocationContainer().getInvocations();
+    }
+
+    private void assertIsMock(Object toInspect) {
+        if (toInspect == null) {
+            throw new NotAMockException("Argument passed to Mockito.mockingDetails() should be a mock, but is null!");
+        } else if (!isMock()) {
+            throw new NotAMockException("Argument passed to Mockito.mockingDetails() should be a mock, but is an instance of " + toInspect.getClass() + "!");
+        }
     }
 
     @Override
--- src/test/java/org/mockito/MockingDetailsTest.java
+++ src/test/java/org/mockito/MockingDetailsTest.java
@@ -1,6 +1,7 @@
 package org.mockito;
 
 import org.junit.Test;
+import org.mockito.exceptions.misusing.NotAMockException;
 import org.mockito.internal.MockitoCore;
 import org.mockito.invocation.Invocation;
 import org.mockitoutil.TestBase;
@@ -11,6 +12,7 @@ import java.util.List;
 
 import static junit.framework.TestCase.*;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.mockingDetails;
 
 public class MockingDetailsTest extends TestBase {
     
@@ -44,8 +46,24 @@ public class MockingDetailsTest extends TestBase {
     }
 
     @Test
-    public void should_handle_null_input() {
-        //TODO 541, decide how to handle it and ensure the there is a top level integ test for the mockingDetails().getInvocations()
-        //assertTrue(new MockitoCore().mockingDetails(null).getInvocations().isEmpty());
+    public void getting_interactions_when_input_mock_is_null() {
+        try {
+            mockingDetails(null)
+                    .getInvocations();
+            fail();
+        } catch (NotAMockException e) {
+            assertEquals("Argument passed to Mockito.mockingDetails() should be a mock, but is null!", e.getMessage());
+        }
+    }
+
+    @Test
+    public void getting_interactions_when_input_mock_is_not_mock() {
+        try {
+            mockingDetails(new Object())
+                    .getInvocations();
+            fail();
+        } catch (NotAMockException e) {
+            assertEquals("Argument passed to Mockito.mockingDetails() should be a mock, but is an instance of class java.lang.Object!", e.getMessage());
+        }
     }
 }
