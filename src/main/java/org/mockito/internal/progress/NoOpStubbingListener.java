3924f4ca63955205d39a2f02864770c66f0b872d
Szczepan Faber <szczepiq@gmail.com>
2016-08-19 14:48:31 -0700

    Removed my login id from Javadoc
    It was automatically created by IDEA :) before I updated my IDEA settings

--- src/main/java/org/mockito/internal/progress/NoOpStubbingListener.java
+++ src/main/java/org/mockito/internal/progress/NoOpStubbingListener.java
@@ -3,9 +3,6 @@ package org.mockito.internal.progress;
 import org.mockito.invocation.Invocation;
 import org.mockito.listeners.StubbingListener;
 
-/**
- * Created by sfaber on 7/22/16.
- */
 class NoOpStubbingListener implements StubbingListener {
 
     static StubbingListener INSTANCE = new NoOpStubbingListener();
