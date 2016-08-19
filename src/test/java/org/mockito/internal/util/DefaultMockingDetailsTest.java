ff7ab2e96019bd7994a16a6a9d693f7eb73c9d5d
Szczepan Faber <szczepiq@gmail.com>
2016-08-19 15:51:46 -0700

    Moved tests to a better spot

--- src/test/java/org/mockito/internal/util/DefaultMockingDetailsTest.java
+++ src/test/java/org/mockito/internal/util/DefaultMockingDetailsTest.java
@@ -1,14 +1,17 @@
 package org.mockito.internal.util;
 
+import junit.framework.TestCase;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
 import org.mockito.Spy;
 import org.mockito.exceptions.misusing.NotAMockException;
 import org.mockito.runners.MockitoJUnitRunner;
+import org.mockitousage.IMethods;
 
 import java.util.*;
 
+import static junit.framework.TestCase.fail;
 import static org.junit.Assert.assertEquals;
 import static org.mockito.Mockito.*;
 
@@ -18,6 +21,7 @@ public class DefaultMockingDetailsTest {
 
     @Mock private Foo foo;
     @Mock private Bar bar;
+    @Mock private IMethods mock;
     @Spy private Gork gork;
 
     @Test
@@ -60,6 +64,41 @@ public class DefaultMockingDetailsTest {
     @Test(expected = NotAMockException.class)
     public void should_report_when_not_a_mockito_mock_on_getExtraInterfaces() throws Exception {
         mockingDetails("not a mock").getExtraInterfaces();
+    }
+
+    @Test
+    public void provides_invocations() {
+        //when
+        mock.simpleMethod(10);
+        mock.otherMethod();
+
+        //then
+        assertEquals(0, mockingDetails(foo).getInvocations().size());
+        assertEquals("[mock.simpleMethod(10);, mock.otherMethod();]", mockingDetails(mock).getInvocations().toString());
+    }
+
+    @Test
+    public void fails_when_getting_invocations_when_null() {
+        try {
+            //when
+            mockingDetails(null).getInvocations();
+            //then
+            fail();
+        } catch (NotAMockException e) {
+            TestCase.assertEquals("Argument passed to Mockito.mockingDetails() should be a mock, but is null!", e.getMessage());
+        }
+    }
+
+    @Test
+    public void fails_when_getting_invocations_when_not_mock() {
+        try {
+            //when
+            mockingDetails(new Object()).getInvocations();
+            //then
+            fail();
+        } catch (NotAMockException e) {
+            TestCase.assertEquals("Argument passed to Mockito.mockingDetails() should be a mock, but is an instance of class java.lang.Object!", e.getMessage());
+        }
     }
 
     private <T> Set<T> setOf(T... items) {
