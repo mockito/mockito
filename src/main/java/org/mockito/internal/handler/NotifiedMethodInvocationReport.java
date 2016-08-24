e16798aa06b6575ab525d6babe7cf939129af8b3
Christian Schwarz <chriss.dev@gmail.com>
2016-08-24 11:23:36 +0200

    removed package org.mockito.internal.listeners (#590)
    - deleted dead interface MockingProgressListener
     - moved NotifiedMethodInvocationReport to org.mockito.internal.handler

--- src/main/java/org/mockito/internal/handler/NotifiedMethodInvocationReport.java
+++ src/main/java/org/mockito/internal/handler/NotifiedMethodInvocationReport.java
@@ -2,7 +2,7 @@
  * Copyright (c) 2007 Mockito contributors
  * This program is made available under the terms of the MIT License.
  */
-package org.mockito.internal.listeners;
+package org.mockito.internal.handler;
 
 import org.mockito.invocation.DescribedInvocation;
 import org.mockito.invocation.Invocation;
