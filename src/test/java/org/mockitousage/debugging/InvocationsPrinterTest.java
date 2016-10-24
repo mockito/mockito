5e93bc6035cc26280f83303b432885c82df07d38
Szczepan Faber <szczepiq@gmail.com>
2016-10-23 21:57:21 -0700

    Updated tests to use AssertJ properly
    Nice catch, Brice!

--- src/test/java/org/mockitousage/debugging/InvocationsPrinterTest.java
+++ src/test/java/org/mockitousage/debugging/InvocationsPrinterTest.java
@@ -1,6 +1,5 @@
 package org.mockitousage.debugging;
 
-import org.assertj.core.api.Assertions;
 import org.junit.Assert;
 import org.junit.Test;
 import org.mockito.Mock;
@@ -9,7 +8,6 @@ import org.mockitousage.IMethods;
 import org.mockitoutil.TestBase;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.Assert.assertEquals;
 import static org.mockito.Mockito.when;
 
 public class InvocationsPrinterTest extends TestBase {
@@ -17,28 +15,28 @@ public class InvocationsPrinterTest extends TestBase {
     @Mock IMethods mock;
 
     @Test public void no_invocations() {
-        Assert.assertEquals("No interactions and stubbings found for mock: mock", new InvocationsPrinter().printInvocations(mock));
+        assertThat(new InvocationsPrinter().printInvocations(mock)).isEqualTo("No interactions and stubbings found for mock: mock");
     }
 
     @Test public void prints_invocations() {
         mock.simpleMethod(100);
         triggerInteraction();
 
-        assertThat(filterLineNo("[Mockito] Interactions of: mock\n" +
+        assertThat(filterLineNo(new InvocationsPrinter().printInvocations(mock)))
+                .isEqualTo(filterLineNo("[Mockito] Interactions of: mock\n" +
                         " 1. mock.simpleMethod(100);\n" +
                         "  -> at org.mockitousage.debugging.InvocationsPrinterTest.prints_invocations(InvocationsPrinterTest.java:0)\n" +
                         " 2. mock.otherMethod();\n" +
-                        "  -> at org.mockitousage.debugging.InvocationsPrinterTest.triggerInteraction(InvocationsPrinterTest.java:0)\n"))
-                .isEqualTo(filterLineNo(new InvocationsPrinter().printInvocations(mock)));
+                        "  -> at org.mockitousage.debugging.InvocationsPrinterTest.triggerInteraction(InvocationsPrinterTest.java:0)\n"));
     }
 
     @Test public void prints_stubbings() {
         triggerStubbing();
 
-        assertThat(filterLineNo("[Mockito] Unused stubbings of: mock\n" +
-                " 1. mock.simpleMethod(\"a\");\n" +
-                "  - stubbed -> at org.mockitousage.debugging.InvocationsPrinterTest.triggerStubbing(InvocationsPrinterTest.java:70)\n"))
-                .isEqualTo(filterLineNo(new InvocationsPrinter().printInvocations(mock)));
+        assertThat(filterLineNo(new InvocationsPrinter().printInvocations(mock)))
+                .isEqualTo(filterLineNo("[Mockito] Unused stubbings of: mock\n" +
+                        " 1. mock.simpleMethod(\"a\");\n" +
+                        "  - stubbed -> at org.mockitousage.debugging.InvocationsPrinterTest.triggerStubbing(InvocationsPrinterTest.java:70)\n"));
     }
 
     @Test public void prints_invocations_and_stubbings() {
@@ -47,13 +45,13 @@ public class InvocationsPrinterTest extends TestBase {
         mock.simpleMethod("a");
         triggerInteraction();
 
-        assertThat(filterLineNo("[Mockito] Interactions of: mock\n" +
+        assertThat(filterLineNo(new InvocationsPrinter().printInvocations(mock)))
+                .isEqualTo(filterLineNo("[Mockito] Interactions of: mock\n" +
                         " 1. mock.simpleMethod(\"a\");\n" +
                         "  -> at org.mockitousage.debugging.InvocationsPrinterTest.prints_invocations_and_stubbings(InvocationsPrinterTest.java:49)\n" +
                         "   - stubbed -> at org.mockitousage.debugging.InvocationsPrinterTest.triggerStubbing(InvocationsPrinterTest.java:73)\n" +
                         " 2. mock.otherMethod();\n" +
-                        "  -> at org.mockitousage.debugging.InvocationsPrinterTest.triggerInteraction(InvocationsPrinterTest.java:34)\n"))
-                .isEqualTo(filterLineNo(new InvocationsPrinter().printInvocations(mock)));
+                        "  -> at org.mockitousage.debugging.InvocationsPrinterTest.triggerInteraction(InvocationsPrinterTest.java:34)\n"));
     }
 
     @Test public void prints_invocations_and_unused_stubbings() {
@@ -62,15 +60,15 @@ public class InvocationsPrinterTest extends TestBase {
         mock.simpleMethod("b");
         triggerInteraction();
 
-        assertThat(filterLineNo("[Mockito] Interactions of: mock\n" +
-                " 1. mock.simpleMethod(\"b\");\n" +
-                "  -> at org.mockitousage.debugging.InvocationsPrinterTest.prints_invocations_and_unused_stubbings(InvocationsPrinterTest.java:55)\n" +
-                " 2. mock.otherMethod();\n" +
-                "  -> at org.mockitousage.debugging.InvocationsPrinterTest.triggerInteraction(InvocationsPrinterTest.java:34)\n" +
-                "[Mockito] Unused stubbings of: mock\n" +
-                " 1. mock.simpleMethod(\"a\");\n" +
-                "  - stubbed -> at org.mockitousage.debugging.InvocationsPrinterTest.triggerStubbing(InvocationsPrinterTest.java:62)\n"))
-                .isEqualTo(filterLineNo(new InvocationsPrinter().printInvocations(mock)));
+        assertThat(filterLineNo(new InvocationsPrinter().printInvocations(mock)))
+                .isEqualTo(filterLineNo("[Mockito] Interactions of: mock\n" +
+                        " 1. mock.simpleMethod(\"b\");\n" +
+                        "  -> at org.mockitousage.debugging.InvocationsPrinterTest.prints_invocations_and_unused_stubbings(InvocationsPrinterTest.java:55)\n" +
+                        " 2. mock.otherMethod();\n" +
+                        "  -> at org.mockitousage.debugging.InvocationsPrinterTest.triggerInteraction(InvocationsPrinterTest.java:34)\n" +
+                        "[Mockito] Unused stubbings of: mock\n" +
+                        " 1. mock.simpleMethod(\"a\");\n" +
+                        "  - stubbed -> at org.mockitousage.debugging.InvocationsPrinterTest.triggerStubbing(InvocationsPrinterTest.java:62)\n"));
     }
 
     private void triggerInteraction() {
