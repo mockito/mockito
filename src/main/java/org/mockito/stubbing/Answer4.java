cda47c1637358eab6186c0c5bcfe3b731057969b
Brice Dutheil <brice.dutheil@gmail.com>
2016-10-24 12:30:21 +0200

    Fixes #707, adds throwable to arity answers, also improved javadoc

--- src/main/java/org/mockito/stubbing/Answer4.java
+++ src/main/java/org/mockito/stubbing/Answer4.java
@@ -3,15 +3,42 @@ package org.mockito.stubbing;
 import org.mockito.Incubating;
 
 /**
- * Three parameter function which returns something
+ * Generic interface to be used for configuring mock's answer for a four argument invocation.
+ *
+ * Answer specifies an action that is executed and a return value that is returned when you interact with the mock.
+ * <p>
+ * Example of stubbing a mock with this custom answer:
+ *
+ * <pre class="code"><code class="java">
+ * when(mock.someMethod(anyInt(), anyString(), anyChar(), any())).thenAnswer(
+ *     new Answer&lt;StringBuilder, Integer, String, Character, Object&gt;() {
+ *         StringBuilder answer(Integer i, String s, Character c, Object o) {
+ *             return new StringBuilder().append(i).append(s).append(c).append(o.hashCode());
+ *         }
+ *     });
+ *
+ * //Following will print a string like "3xyz131635550"
+ * System.out.println(mock.someMethod(3, "xy", 'z', new Object()));
+ * </code></pre>
  *
  * @param <T> return type
- * @param <A> input parameter 1 type
- * @param <B> input parameter 2 type
- * @param <C> input parameter 3 type
- * @param <D> input parameter 4 type
+ * @param <A0> type of the first argument
+ * @param <A1> type of the second argument
+ * @param <A2> type of the third argument
+ * @param <A3> type of the fourth argument
+ * @see Answer
  */
 @Incubating
-public interface Answer4<T, A, B, C, D> {
-    T answer(A a, B b, C c, D d);
+public interface Answer4<T, A0, A1, A2, A3> {
+    /**
+     * @param argument0 the first argument.
+     * @param argument1 the second argument.
+     * @param argument2 the third argument.
+     * @param argument3 the fourth argument.
+     *
+     * @return the value to be returned.
+     *
+     * @throws Throwable the throwable to be thrown
+     */
+    T answer(A0 argument0, A1 argument1, A2 argument2, A3 argument3) throws Throwable;
 }
--- src/main/java/org/mockito/stubbing/VoidAnswer4.java
+++ src/main/java/org/mockito/stubbing/VoidAnswer4.java
@@ -3,14 +3,39 @@ package org.mockito.stubbing;
 import org.mockito.Incubating;
 
 /**
- * Two parameter void function
+ * Generic interface to be used for configuring mock's answer for a four argument invocation that returns nothing.
  *
- * @param <A> input parameter 1 type
- * @param <B> input parameter 2 type
- * @param <C> input parameter 3 type
- * @param <D> input parameter 4 type
+ * Answer specifies an action that is executed when you interact with the mock.
+ * <p>
+ * Example of stubbing a mock with this custom answer:
+ *
+ * <pre class="code"><code class="java">
+ * when(mock.someMethod(anyString(), anyInt(), anyString(), anyChar())).thenAnswer(
+ *     new Answer&lt;String, Integer, String, Character&gt;() {
+ *         void answer(String msg, Integer count, String another, Character c) {
+ *             throw new Exception(String.format(msg, another, c, count));
+ *         }
+ *     });
+ *
+ * //Following will raise an exception with the message "ka-boom <3"
+ * mock.someMethod("%s-boom %c%d", 3, "ka", '&lt;');
+ * </code></pre>
+ *
+ * @param <A0> type of the first argument
+ * @param <A1> type of the second argument
+ * @param <A2> type of the third argument
+ * @param <A3> type of the fourth argument
+ * @see Answer
  */
 @Incubating
-public interface VoidAnswer4<A, B, C, D> {
-    void answer(A a, B b, C c, D d);
+public interface VoidAnswer4<A0, A1, A2, A3> {
+    /**
+     * @param argument0 the first argument.
+     * @param argument1 the second argument.
+     * @param argument2 the third argument.
+     * @param argument3 the fourth argument.
+     *
+     * @throws Throwable the throwable to be thrown
+     */
+    void answer(A0 argument0, A1 argument1, A2 argument2, A3 argument3) throws Throwable;
 }
