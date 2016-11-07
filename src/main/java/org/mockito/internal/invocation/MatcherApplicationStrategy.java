31b7646c535fa125f4a1c4d9b12da976364ab770
Dmitriy Zaitsev <zaitsev.dl@gmail.com>
2016-11-08 00:11:19 +0200

    Add missing header copyrights to source files
    Fixes #727

--- src/main/java/org/mockito/internal/invocation/MatcherApplicationStrategy.java
+++ src/main/java/org/mockito/internal/invocation/MatcherApplicationStrategy.java
@@ -1,3 +1,7 @@
+/*
+ * Copyright (c) 2016 Mockito contributors
+ * This program is made available under the terms of the MIT License.
+ */
 package org.mockito.internal.invocation;
 
 import static org.mockito.internal.invocation.MatcherApplicationStrategy.MatcherApplicationType.ERROR_UNSUPPORTED_NUMBER_OF_MATCHERS;
