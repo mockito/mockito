bcedd5a7edfa6c86f9d84c0fa68d00eb5d167337
Szczepan Faber <szczepiq@gmail.com>
2016-11-19 13:10:33 -0800

    Refactoring JUnit rule implementation
    Needed for work on Mockito strictness, see #769. Decoupled JUnit API from the work Mockito does before and after every test.
    
    No public API exposed, listeners and friends need to be refactored and documented.
    
    Don't merge. This refactoring on its own does not add value.

---
+++ src/main/java/org/mockito/internal/junit/WarningTestListener.java
@@ -0,0 +1,36 @@
+package org.mockito.internal.junit;
+
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+import org.mockito.internal.util.MockitoLogger;
+
+import java.util.Collection;
+
+class WarningTestListener implements MockitoTestListener {
+
+    private final MockitoLogger logger;
+    private String testName;
+
+    WarningTestListener(MockitoLogger logger) {
+        this.logger = logger;
+    }
+
+    public void beforeTest(Object testClassInstance, String testMethodName) {
+        testName = testClassInstance.getClass().getSimpleName() + "." + testMethodName;
+        MockitoAnnotations.initMocks(testClassInstance);
+    }
+
+    public void afterTest(Collection<Object> mocks, Throwable problem) {
+        if (problem != null) {
+            //print stubbing mismatches only when there is a test failure
+            //to avoid false negatives. Give hint only when test fails.
+            new ArgMismatchFinder().getStubbingArgMismatches(mocks).format(testName, logger);
+        } else {
+            //Validate only when there is no other problem to avoid reporting multiple problems
+            Mockito.validateMockitoUsage();
+
+            //print unused stubbings only when test succeeds to avoid reporting multiple problems and confusing users
+            new UnusedStubbingsFinder().getUnusedStubbings(mocks).format(testName, logger);
+        }
+    }
+}
