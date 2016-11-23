da6310bf6721682aae2bbfcdc212336214a2e3b9
Szczepan Faber <szczepiq@gmail.com>
2016-11-23 08:37:58 -0800

    Improved the test clarity
    Started using a test utility rule so that we can wrap exception handling and clean up all rule testing code.

--- src/test/java/org/mockitousage/junitrule/StrictJUnitRuleTest.java
+++ src/test/java/org/mockitousage/junitrule/StrictJUnitRuleTest.java
@@ -1,174 +1,104 @@
 package org.mockitousage.junitrule;
 
 import org.assertj.core.api.Assertions;
-import org.junit.After;
-import org.junit.Ignore;
+import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runners.model.FrameworkMethod;
-import org.junit.runners.model.Statement;
 import org.mockito.Mock;
-import org.mockito.Mockito;
-import org.mockito.exceptions.base.MockitoAssertionError;
 import org.mockito.internal.junit.JUnitRule;
 import org.mockito.junit.MockitoJUnit;
 import org.mockitousage.IMethods;
+import org.mockitoutil.SafeJUnitRule;
 import org.mockitoutil.TestBase;
 
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.fail;
+import static org.junit.Assert.assertNull;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.*;
 
 public class StrictJUnitRuleTest extends TestBase {
 
-    private JUnitRule jUnitRule = ((JUnitRule) MockitoJUnit.rule()).strictStubs();
+    @Rule public SafeJUnitRule rule = new SafeJUnitRule(((JUnitRule) MockitoJUnit.rule()).strictStubs());
 
     private FrameworkMethod dummy = mock(FrameworkMethod.class);
 
-    @After public void after() {
-        //so that the validate framework usage exceptions do not collide with the tests here
-        resetState();
-    }
+    @Mock IMethods mock;
+    @Mock IMethods mock2;
 
     @Test public void ok_when_no_stubbings() throws Throwable {
-        run(new MockitoStatement() {
-            public void evaluate(IMethods mock, IMethods mock2) throws Throwable {
-                mock.simpleMethod();
-                Mockito.verify(mock).simpleMethod();
-            }
-        });
+        mock.simpleMethod();
+        verify(mock).simpleMethod();
     }
 
     @Test public void ok_when_all_stubbings_used() throws Throwable {
-        run(new MockitoStatement() {
-            public void evaluate(IMethods mock1, IMethods mock2) throws Throwable {
-                IMethods mock = mock(IMethods.class);
-                given(mock.simpleMethod(10)).willReturn("foo");
-                mock.simpleMethod(10);
-            }
-        });
+        given(mock.simpleMethod(10)).willReturn("foo");
+        mock.simpleMethod(10);
     }
 
     @Test public void ok_when_used_and_mismatched_argument() throws Throwable {
-        run(new MockitoStatement() {
-            public void evaluate(IMethods mock, IMethods mock2) throws Throwable {
-                given(mock.simpleMethod(10)).willReturn("foo");
-                mock.simpleMethod(10);
-                mock.simpleMethod(15);
-            }
-        });
+        given(mock.simpleMethod(10)).willReturn("foo");
+        mock.simpleMethod(10);
+        mock.simpleMethod(15);
     }
 
     @Test public void fails_when_unused_stubbings() throws Throwable {
-        try {
-            //when
-            run(new MockitoStatement() {
-                public void evaluate(IMethods mock, IMethods mock2) throws Throwable {
-                    given(mock.simpleMethod(10)).willReturn("foo");
-                    mock2.simpleMethod(15);
-                }
-            });
+        //when
+        given(mock.simpleMethod(10)).willReturn("foo");
+        mock2.simpleMethod(10);
 
-            //then
-            fail();
-        } catch (MockitoAssertionError e) {
-            Assertions.assertThat(e.getMessage()).startsWith("Unused stubbings");
-        }
+        //then
+        String message = rule.getReportedThrowable().getMessage();
+        Assertions.assertThat(message).startsWith("Unused stubbings");
     }
 
     @Test public void test_failure_trumps_unused_stubbings() throws Throwable {
-        try {
-            //when
-            run(new MockitoStatement() {
-                public void evaluate(IMethods mock, IMethods mock2) throws Throwable {
-                    given(mock.simpleMethod(10)).willReturn("foo");
-                    mock.otherMethod();
+        //expect
+        rule.expectThrowable(AssertionError.class, "x");
 
-                    throw new AssertionError("x");
-                }
-            });
+        //when
+        given(mock.simpleMethod(10)).willReturn("foo");
+        mock.otherMethod();
 
-            //then
-            fail();
-        } catch (AssertionError e) {
-            assertEquals("x", e.getMessage());
-        }
+        throw new AssertionError("x");
     }
 
     @Test public void fails_fast_when_stubbing_invoked_with_different_argument() throws Throwable {
-        try {
-            //when
-            run(new MockitoStatement() {
-                public void evaluate(IMethods mock, IMethods mock2) throws Throwable {
-                    //stubbing in the test code:
-                    given(mock.simpleMethod(10)).willReturn("foo");
+        //when stubbing in the test code:
+        given(mock.simpleMethod(10)).willReturn("foo");
 
-                    //invocation in the code under test uses different argument and should fail immediately
-                    //this helps with debugging and is essential for Mockito strictness
-                    mock.simpleMethod(15);
-                }
-            });
+        //and invocation in the code under test uses different argument and should fail immediately
+        //this helps with debugging and is essential for Mockito strictness
+        mock.simpleMethod(15);
 
-            //then
-            fail();
-        } catch (MockitoAssertionError e) {
-            assertThat(e.getMessage()).startsWith("Argument mismatch");
-        }
+        //then
+        Assertions.assertThat(rule.getReportedThrowable()).hasMessageStartingWith("Argument mismatch");
     }
 
     @Test public void verify_no_more_interactions_ignores_stubs() throws Throwable {
-        //when
-        run(new MockitoStatement() {
-            public void evaluate(IMethods mock, IMethods mock2) throws Throwable {
-                //in test:
-                given(mock.simpleMethod(10)).willReturn("foo");
+        //when stubbing in test:
+        given(mock.simpleMethod(10)).willReturn("foo");
 
-                //in code:
-                mock.simpleMethod(10); //implicitly verifies the stubbing
-                mock.otherMethod();
+        //and code under test does:
+        mock.simpleMethod(10); //implicitly verifies the stubbing
+        mock.otherMethod();
 
-                //in test:
-                verify(mock).otherMethod();
-                verifyNoMoreInteractions(mock);
-            }
-        });
+        //and in test we:
+        verify(mock).otherMethod();
+        verifyNoMoreInteractions(mock);
+
+        //then no exception is thrown
+        assertNull(rule.getReportedThrowable());
     }
 
     @Test public void unused_stubs_with_multiple_mocks() throws Throwable {
-        try {
-            //when
-            run(new MockitoStatement() {
-                public void evaluate(IMethods mock1, IMethods mock2) throws Throwable {
-                    given(mock1.simpleMethod(10)).willReturn("foo");
-                    given(mock2.simpleMethod(20)).willReturn("foo");
+        //test
+        given(mock.simpleMethod(10)).willReturn("foo");
+        given(mock2.simpleMethod(20)).willReturn("foo");
 
-                    mock1.otherMethod();
-                    mock2.booleanObjectReturningMethod();
-                }
-            });
+        //code
+        mock.otherMethod();
+        mock2.booleanObjectReturningMethod();
 
-            //then
-            fail();
-        } catch (MockitoAssertionError e) {
-            assertThat(e.getMessage()).startsWith("Unused stubbings");
-        }
-    }
-
-    private void run(MockitoStatement statement) throws Throwable {
-        jUnitRule.apply(statement, dummy, new DummyTestCase()).evaluate();
-    }
-
-    public static class DummyTestCase {
-        @Mock private IMethods mock;
-    }
-
-    abstract class MockitoStatement extends Statement {
-        abstract void evaluate(IMethods mock1, IMethods mock2) throws Throwable;
-
-        @Override
-        public void evaluate() throws Throwable {
-            evaluate(mock(IMethods.class), mock(IMethods.class));
-        }
+        //then
+        Assertions.assertThat(rule.getReportedThrowable()).hasMessageStartingWith("Unused stubbings");
     }
 }
---
+++ src/test/java/org/mockitoutil/SafeJUnitRule.java
@@ -0,0 +1,93 @@
+package org.mockitoutil;
+
+import org.assertj.core.api.Assertions;
+import org.junit.rules.MethodRule;
+import org.junit.runners.model.FrameworkMethod;
+import org.junit.runners.model.Statement;
+
+/**
+ * Junit rule for testing exception handling other JUnit rules, like Mockito JUnit rules.
+ * Makes it easy to assert on expected exceptions triggered by the rule under test.
+ */
+public class SafeJUnitRule implements MethodRule {
+
+    private final MethodRule testedRule;
+    private Throwable reportedThrowable;
+    private final static ThrowableAssert NO_OP = new ThrowableAssert() { public void doAssert(Throwable throwable) { } };
+    private ThrowableAssert throwableAssert = NO_OP;
+
+    /**
+     * Wraps rule under test with exception handling so that it is easy to assert on exceptions fired from the tested rule.
+     */
+    public SafeJUnitRule(MethodRule testedRule) {
+        this.testedRule = testedRule;
+    }
+
+    public Statement apply(final Statement base, FrameworkMethod method, Object target) {
+        return new Statement() {
+            public void evaluate() throws Throwable {
+                try {
+                    base.evaluate();
+                } catch (Throwable throwable) {
+                    reportedThrowable = throwable;
+                    throwableAssert.doAssert(throwable);
+                    return;
+                }
+                if (throwableAssert != NO_OP) {
+                    //looks like the user expects a throwable but it was not thrown!
+                    throw new ExpectedThrowableNotReported("Expected the tested rule to throw an exception but it did not.");
+                }
+            }
+        };
+    }
+
+    /**
+     * Expects that _after_ the test, the rule will fire specific exception with specific exception message
+     */
+    public void expectThrowable(final Class<? extends Throwable> expected, final String expectedMessage) {
+        this.expectThrowable(new ThrowableAssert() {
+            public void doAssert(Throwable throwable) {
+                Assertions.assertThat(throwable)
+                        .isInstanceOf(expected)
+                        .hasMessage(expectedMessage);
+            }
+        });
+    }
+
+    /**
+     * Expects that _after_ the test, the rule will fire an exception
+     */
+    public void expectThrowable(ThrowableAssert throwableAssert) {
+        this.throwableAssert = throwableAssert;
+    }
+
+    /**
+     * Offers a way to assert the throwable triggered by the tested rule
+     */
+    public interface ThrowableAssert {
+        void doAssert(Throwable throwable);
+    }
+
+    /**
+     * Rule under test
+     */
+    public MethodRule getTestedRule() {
+        return testedRule;
+    }
+
+    /**
+     * Exception reported by rule under test. Can be null, meaning the tested rule did not emit an exception.
+     */
+    public Throwable getReportedThrowable() {
+        return reportedThrowable;
+    }
+
+    /**
+     * Thrown when user expects the tested rule to throw an exception but no exception was thrown
+     */
+    class ExpectedThrowableNotReported extends Throwable {
+        public ExpectedThrowableNotReported(String message) {
+            super(message);
+        }
+    }
+}
