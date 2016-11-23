7bb583407fdc50a54fa152144a9a5b3ad06ad856
Szczepan Faber <szczepiq@gmail.com>
2016-11-23 09:50:46 -0800

    Added missing unit test coverage
    Added comprehensive tests for Mockito listeners

--- src/main/java/org/mockito/internal/progress/MockingProgress.java
+++ src/main/java/org/mockito/internal/progress/MockingProgress.java
@@ -47,4 +47,9 @@ public interface MockingProgress {
     void setVerificationStrategy(VerificationStrategy strategy);
 
     VerificationMode maybeVerifyLazily(VerificationMode mode);
+
+    /**
+     * Removes all listeners added via {@link #addListener(MockitoListener)}.
+     */
+    void clearListeners();
 }
--- src/test/java/org/mockito/internal/framework/DefaultMockitoFrameworkTest.java
+++ src/test/java/org/mockito/internal/framework/DefaultMockitoFrameworkTest.java
@@ -1,11 +1,28 @@
 package org.mockito.internal.framework;
 
+import org.junit.After;
 import org.junit.Test;
+import org.mockito.ArgumentMatchers;
+import org.mockito.MockSettings;
+import org.mockito.StateMaster;
+import org.mockito.listeners.MockCreationListener;
+import org.mockito.listeners.MockitoListener;
+import org.mockito.mock.MockCreationSettings;
 import org.mockitoutil.TestBase;
+
+import java.util.List;
+import java.util.Set;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
 
 public class DefaultMockitoFrameworkTest extends TestBase {
 
-    DefaultMockitoFramework framework = new DefaultMockitoFramework();
+    private DefaultMockitoFramework framework = new DefaultMockitoFramework();
+
+    @After public void clearListeners() {
+        new StateMaster().clearMockitoListeners();
+    }
 
     @Test(expected = IllegalArgumentException.class)
     public void prevents_adding_null_listener() {
@@ -15,5 +32,58 @@ public class DefaultMockitoFrameworkTest extends TestBase {
     @Test(expected = IllegalArgumentException.class)
     public void prevents_removing_null_listener() {
         framework.removeListener(null);
+    }
+
+    @Test
+    public void ok_to_remove_unknown_listener() {
+        //it is safe to remove listener that was not added before
+        framework.removeListener(new MockitoListener() {});
+    }
+
+    @Test
+    public void ok_to_remove_listener_multiple_times() {
+        MockitoListener listener = new MockitoListener() {};
+
+        //when
+        framework.addListener(listener);
+
+        //then it is ok to:
+        framework.removeListener(listener);
+        framework.removeListener(listener);
+    }
+
+    @Test
+    public void adds_creation_listener() {
+        //given creation listener is added
+        MockCreationListener listener = mock(MockCreationListener.class);
+        framework.addListener(listener);
+
+        //when
+        MockSettings settings = withSettings().name("my list");
+        List mock = mock(List.class, settings);
+        Set mock2 = mock(Set.class);
+
+        //then
+        verify(listener).onMockCreated(eq(mock), any(MockCreationSettings.class));
+        verify(listener).onMockCreated(eq(mock2), any(MockCreationSettings.class));
+        verifyNoMoreInteractions(listener);
+    }
+
+    @Test
+    public void removes_creation_listener() {
+        //given creation listener is added
+        MockCreationListener listener = mock(MockCreationListener.class);
+        framework.addListener(listener);
+
+        //and hooked up correctly
+        mock(List.class);
+        verify(listener).onMockCreated(ArgumentMatchers.any(), any(MockCreationSettings.class));
+
+        //when
+        framework.removeListener(listener);
+        mock(Set.class);
+
+        //then
+        verifyNoMoreInteractions(listener);
     }
 }
