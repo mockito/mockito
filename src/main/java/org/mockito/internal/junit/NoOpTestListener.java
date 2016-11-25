4ae25885a37c9406f146c20ed8091c861d041c1a
Szczepan Faber <szczepiq@gmail.com>
2016-11-25 07:06:00 -0800

    Split the interface hierarchy
    There is no good reason to keep them together. The code is simpler now.

--- src/main/java/org/mockito/internal/junit/NoOpTestListener.java
+++ src/main/java/org/mockito/internal/junit/NoOpTestListener.java
@@ -1,10 +1,6 @@
 package org.mockito.internal.junit;
 
-import org.mockito.mock.MockCreationSettings;
-
 class NoOpTestListener implements MockitoTestListener {
 
     public void testFinished(TestFinishedEvent event) {}
-
-    public void onMockCreated(Object mock, MockCreationSettings settings) {}
 }
