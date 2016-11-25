b02615b7c8eb3f5d433c46831e2b9580dca7534a
Szczepan Faber <szczepiq@gmail.com>
2016-11-25 06:57:00 -0800

    Tidy-up, simplified code

--- src/main/java/org/mockito/internal/junit/MockitoTestListener.java
+++ src/main/java/org/mockito/internal/junit/MockitoTestListener.java
@@ -3,8 +3,5 @@ package org.mockito.internal.junit;
 import org.mockito.listeners.MockCreationListener;
 
 interface MockitoTestListener extends MockCreationListener {
-
-    void beforeTest(Object testClassInstance, String testMethodName);
-
-    void afterTest(Throwable problem);
+    void testFinished(TestFinishedEvent event);
 }
--- src/main/java/org/mockito/internal/junit/NoOpTestListener.java
+++ src/main/java/org/mockito/internal/junit/NoOpTestListener.java
@@ -4,9 +4,7 @@ import org.mockito.mock.MockCreationSettings;
 
 class NoOpTestListener implements MockitoTestListener {
 
-    public void beforeTest(Object testClassInstance, String testMethodName) {}
-
-    public void afterTest(Throwable testFailure) {}
+    public void testFinished(TestFinishedEvent event) {}
 
     public void onMockCreated(Object mock, MockCreationSettings settings) {}
 }
