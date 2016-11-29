bde093f05ace8f5a78230e36a7685fc9476ea568
Szczepan Faber <szczepiq@gmail.com>
2016-11-29 07:36:36 -0800

    Removed controversial instanceof code
    2 team member raised concerns so I updated the code accordingly. Thanks for feedback!!!

--- src/main/java/org/mockito/internal/framework/DefaultMockitoFramework.java
+++ src/main/java/org/mockito/internal/framework/DefaultMockitoFramework.java
@@ -4,7 +4,6 @@
  */
 package org.mockito.internal.framework;
 
-import org.mockito.Mockito;
 import org.mockito.MockitoFramework;
 import org.mockito.internal.util.Checks;
 import org.mockito.listeners.MockitoListener;
@@ -23,25 +22,5 @@ public class DefaultMockitoFramework implements MockitoFramework {
         Checks.checkNotNull(listener, "listener");
         mockingProgress().removeListener(listener);
         return this;
-    }
-
-    /**
-     * Adds listener only if it is an instance of MockitoListener.
-     * Useful to avoid casting in internal code.
-     */
-    public static void tryAddListner(Object listener) {
-        if (listener instanceof MockitoListener) {
-            Mockito.framework().addListener((MockitoListener) listener);
-        }
-    }
-
-    /**
-     * Removes listener only if it is an instance of MockitoListener.
-     * Useful to avoid casting in internal code.
-     */
-    public static void tryRemoveListener(Object listener) {
-        if (listener instanceof MockitoListener) {
-            Mockito.framework().removeListener((MockitoListener) listener);
-        }
     }
 }
