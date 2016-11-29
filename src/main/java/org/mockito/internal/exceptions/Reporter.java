8112aac014672e5de9a2507a074678b422eca17a
Szczepan Faber <szczepiq@gmail.com>
2016-11-29 07:41:27 -0800

    Updated the exception message for clarity.

--- src/main/java/org/mockito/internal/exceptions/Reporter.java
+++ src/main/java/org/mockito/internal/exceptions/Reporter.java
@@ -870,8 +870,8 @@ public class Reporter {
                 "Typically, stubbing argument mismatch indicates user mistake when writing tests.",
                 "In order to streamline debugging tests Mockito fails early in this scenario.",
                 "However, there are legit scenarios when this exception generates false negative signal:",
-                "  - stubbing the same method multiple times using 'given' or 'when' syntax",
-                "    Please use willReturn/doReturn API for stubbing",
+                "  - stubbing the same method multiple times using 'given().will()' or 'when().then()' API",
+                "    Please usew 'will().given()' or 'doReturn().when()' API for stubbing",
                 "  - stubbed method is intentionally invoked with different arguments by code under test",
                 "    Please use 'default' or 'silent' JUnit Rule.",
                 "For more information see javadoc for PotentialStubbingProblem class."));
