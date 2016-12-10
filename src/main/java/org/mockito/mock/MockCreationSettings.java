66f8e0f9f5dea006aa3f9e8a09283f29309e596c
Szczepan Faber <szczepiq@gmail.com>
2016-12-10 06:39:34 -0800

    Removed unused import

--- src/main/java/org/mockito/mock/MockCreationSettings.java
+++ src/main/java/org/mockito/mock/MockCreationSettings.java
@@ -7,7 +7,6 @@ package org.mockito.mock;
 
 import org.mockito.Incubating;
 import org.mockito.listeners.InvocationListener;
-import org.mockito.internal.listeners.StubbingLookupListener;
 import org.mockito.stubbing.Answer;
 
 import java.util.List;
