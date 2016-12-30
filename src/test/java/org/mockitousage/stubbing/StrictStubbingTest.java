65da26d04ce6229ed3440d779bef014a26485ddc
Szczepan Faber <szczepiq@gmail.com>
2016-12-30 13:35:17 -0800

    Avoided redundant exceptions
    - split test code into separate test classes
    - added TODOs
    - added missing functionality in a form of an integ test

--- src/test/java/org/mockitousage/stubbing/StrictStubbingTest.java
+++ src/test/java/org/mockitousage/stubbing/StrictStubbingTest.java
@@ -1,9 +1,6 @@
 package org.mockitousage.stubbing;
 
-import org.junit.After;
 import org.junit.Test;
-import org.junit.runner.JUnitCore;
-import org.junit.runner.Result;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.MockitoMocking;
@@ -12,7 +9,6 @@ import org.mockito.exceptions.misusing.UnnecessaryStubbingException;
 import org.mockito.exceptions.verification.NoInteractionsWanted;
 import org.mockito.quality.Strictness;
 import org.mockitousage.IMethods;
-import org.mockitoutil.JUnitResultAssert;
 
 import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.verify;
@@ -102,27 +98,5 @@ public class StrictStubbingTest {
                 mocking.finishMocking();
             }
         }).throwsException(UnnecessaryStubbingException.class);
-    }
-
-    @Test public void finish_mocking_exception_does_not_hide_the_exception_from_test() {
-        JUnitCore junit = new JUnitCore();
-        Result result = junit.run(ArgumentMismatch.class);
-        JUnitResultAssert.assertThat(result)
-                .fails("stubbing_argument_mismatch", IllegalStateException.class)
-                .fails("stubbing_argument_mismatch", UnnecessaryStubbingException.class);
-    }
-
-    public static class ArgumentMismatch {
-        @Mock IMethods mock;
-        MockitoMocking mocking = Mockito.startMocking(this, Strictness.STRICT_STUBS);
-
-        @After public void after() {
-            mocking.finishMocking();
-        }
-
-        @Test public void stubbing_argument_mismatch() {
-            given(mock.simpleMethod("1")).willReturn("one");
-            throw new IllegalStateException();
-        }
     }
 }
