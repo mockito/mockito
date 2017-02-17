87f69b15e9fd4348c80e9afe1ca997a503d4cb93
Terebesi Robert <terebesi.robert@yahoo.com>
2017-02-17 21:10:23 +0200

    Fixes 358: Removed unused argument matchers

--- src/main/java/org/mockito/ArgumentMatchers.java
+++ src/main/java/org/mockito/ArgumentMatchers.java
@@ -8,7 +8,6 @@ import static org.mockito.internal.progress.ThreadSafeMockingProgress.mockingPro
 import static org.mockito.internal.util.Primitives.defaultValue;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Collection;
 import java.util.HashMap;
 import java.util.HashSet;
@@ -17,24 +16,14 @@ import java.util.Map;
 import java.util.Set;
 import java.util.regex.Pattern;
 
-import org.mockito.internal.matchers.And;
 import org.mockito.internal.matchers.Any;
-import org.mockito.internal.matchers.ArrayEquals;
-import org.mockito.internal.matchers.CompareEqual;
 import org.mockito.internal.matchers.Contains;
 import org.mockito.internal.matchers.EndsWith;
 import org.mockito.internal.matchers.Equals;
-import org.mockito.internal.matchers.EqualsWithDelta;
-import org.mockito.internal.matchers.Find;
-import org.mockito.internal.matchers.GreaterOrEqual;
-import org.mockito.internal.matchers.GreaterThan;
 import org.mockito.internal.matchers.InstanceOf;
-import org.mockito.internal.matchers.LessOrEqual;
-import org.mockito.internal.matchers.LessThan;
 import org.mockito.internal.matchers.Matches;
 import org.mockito.internal.matchers.NotNull;
 import org.mockito.internal.matchers.Null;
-import org.mockito.internal.matchers.Or;
 import org.mockito.internal.matchers.Same;
 import org.mockito.internal.matchers.StartsWith;
 import org.mockito.internal.matchers.apachecommons.ReflectionEquals;
@@ -595,119 +584,6 @@ public class ArgumentMatchers {
      */
     public static <T> Set<T> anySetOf(Class<T> clazz) {
         return anySet();
-    }
-
-    /**
-     * argument greater than or equal to the given value.
-     * 
-     * @param value
-     *            the given value.
-     * @return {@link ArgumentMatcher}.
-     */
-    public static <T extends Comparable<T>> ArgumentMatcher<T> geq(T value) {
-        return new GreaterOrEqual<T>(value);
-    }
-
-    /**
-     * comparable argument less than or equal than the given value details.
-     * 
-     * @param value
-     *            the given value.
-     * @return {@link ArgumentMatcher}.
-     */
-    public static <T extends Comparable<T>> ArgumentMatcher<T> leq(T value) {
-        return new LessOrEqual<T>(value);
-    }
-
-    /**
-     * comparable argument greater than the given value.
-     * 
-     * @param value
-     *            the given value.
-     * @return {@link ArgumentMatcher}.
-     */
-    public static <T extends Comparable<T>> ArgumentMatcher<T> gt(T value) {
-        return new GreaterThan<T>(value);    }
-
-    /**
-     * comparable argument less than the given value.
-     * 
-     * @param value
-     *            the given value.
-     * @return {@link ArgumentMatcher}.
-     */
-    public static <T extends Comparable<T>> ArgumentMatcher<T> lt(T value) {
-        return new LessThan<T>(value);
-    }
-
-    /**
-     * comparable argument equals to than the given value according to their
-     * compareTo method.
-     * 
-     * @param value
-     *            the given value.
-     * @return {@link ArgumentMatcher}.
-     */
-    public static <T extends Comparable<T>> ArgumentMatcher<T> cmpEq(T value) {
-        return new CompareEqual<T>(value);
-    }
-
-    /**
-     * String argument that contains a substring that matches the given regular
-     * expression.
-     * 
-     * @param regex
-     *            the regular expression.
-     * @return {@link ArgumentMatcher}.
-     */
-    public static ArgumentMatcher<String> find(String regex) {
-        return new Find(regex);
-    }
-
-    /**
-     * Object array argument that is equal to the given array, i.e. it has to
-     * have the same type, length, and each element has to be equal.
-     * 
-     * @param <T>
-     *            the type of the array, it is passed through to prevent casts.
-     * @param value
-     *            the given array.
-     * @return {@link ArgumentMatcher}.
-     */
-    public static <T> ArgumentMatcher aryEq(T[] value) {
-        return new ArrayEquals(value);
-    }
-
-    /**
-     * number argument that has an absolute difference to the given value that
-     * is less than the given delta details.
-     * 
-     * @param value
-     *            the given value.
-     * @param delta
-     *            the given delta.
-     * @return {@link ArgumentMatcher}.
-     */
-    public static <T extends Number> ArgumentMatcher eq(T value, T delta) {
-        return new EqualsWithDelta(value, delta);
-    }
-
-    /**
-     * Performs an and operation between matchers
-     * @param matchers the matchers that have to match the arguments
-     * @return {@link ArgumentMatcher}
-     */
-    public static ArgumentMatcher and(ArgumentMatcher ...matchers) {
-        return new And(Arrays.asList(matchers));
-    }
-
-    /**
-     * Performs an or operation between matchers
-     * @param matchers the matchers that have to match the arguments
-     * @return {@link ArgumentMatcher}
-     */
-    public static ArgumentMatcher or(ArgumentMatcher ...matchers) {
-        return new Or(Arrays.asList(matchers));
     }
 
     /**
