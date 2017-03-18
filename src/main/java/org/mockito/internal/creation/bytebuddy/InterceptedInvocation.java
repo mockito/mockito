9697ab424232e006ff4a3195cfe5a505042fcb0d
Tsuyoshi Murakami <tmurakami@users.noreply.github.com>
2017-03-19 06:47:55 +0900

    Fixes #974: Add Location parameter to constructor of InterceptedInvocation

--- src/main/java/org/mockito/internal/creation/bytebuddy/InterceptedInvocation.java
+++ src/main/java/org/mockito/internal/creation/bytebuddy/InterceptedInvocation.java
@@ -42,14 +42,15 @@ class InterceptedInvocation implements Invocation, VerificationAwareInvocation {
                                  MockitoMethod mockitoMethod,
                                  Object[] arguments,
                                  SuperMethod superMethod,
+                                 Location location,
                                  int sequenceNumber) {
         this.mock = mock;
         this.mockitoMethod = mockitoMethod;
         this.arguments = ArgumentsProcessor.expandVarArgs(mockitoMethod.isVarArgs(), arguments);
         this.rawArguments = arguments;
         this.superMethod = superMethod;
+        this.location = location;
         this.sequenceNumber = sequenceNumber;
-        location = new LocationImpl();
     }
 
     @Override
