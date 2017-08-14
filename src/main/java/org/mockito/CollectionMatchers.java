925fab37eee1fc443ea8b6243a352cc6982e1119
Jeroen Mols <jeroen.mols.be@gmail.com>
2017-08-14 17:01:33 +0200

    Added documentation for CollectionMatchers

--- src/main/java/org/mockito/CollectionMatchers.java
+++ src/main/java/org/mockito/CollectionMatchers.java
@@ -11,32 +11,79 @@ import java.util.List;
 
 import static org.mockito.ArgumentMatchers.argThat;
 
+/**
+ * See {@link Matchers} for general info about matchers.
+ * <p>
+ * CollectionMatchers provides matchers that simplify verifications on standard java collections.
+ * <p>
+ *
+ * Scroll down to see all methods - full list of matchers.
+ *
+ * @since 2.9.0
+ */
 public class CollectionMatchers {
 
+    /**
+     * list contains the given value
+     * list contains all given values
+     *
+     * @since 2.9.0
+     */
     public static <K> List listContains(K... object) {
         return argThat(new ListContains<K>(object));
     }
 
+    /**
+     * list contains the given value at the specified index
+     *
+     * @since 2.9.0
+     */
     public static <K> List listContains(K object, int index) {
         return argThat(new ListContainsAtIndex<K>(object, index));
     }
 
+    /**
+     * list contains null
+     *
+     * @since 2.9.0
+     */
     public static List listContainsNull() {
         return argThat(new ListContainsNull());
     }
 
+    /**
+     * list does not contain the given value
+     * list does not contain all given values
+     *
+     * @since 2.9.0
+     */
     public static <K> List listDoesNotContain(K... object) {
         return argThat(new ListDoesNotContain<K>(object));
     }
 
+    /**
+     * list does not contain the given value at the specified index
+     *
+     * @since 2.9.0
+     */
     public static <K> List listDoesNotContain(K object, int index) {
         return argThat(new ListDoesNotContainAtIndex<K>(object, index));
     }
 
+    /**
+     * list does not contain null
+     *
+     * @since 2.9.0
+     */
     public static List listDoesnotContainNull() {
         return argThat(new ListDoesNotContainNull());
     }
 
+    /**
+     * list has exactly the specified size
+     *
+     * @since 2.9.0
+     */
     public static List listOfSize(int size) {
         return argThat(new ListOfSize(size));
     }
