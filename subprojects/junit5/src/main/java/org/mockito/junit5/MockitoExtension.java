73f5a792c5495d0e71f9085c22a67012dcefd256
Christian Schwarz <chriss.dev@github>
2017-10-24 22:28:24 +0200

    fixed dependencies

---
+++ subprojects/junit5/src/main/java/org/mockito/junit5/MockitoExtension.java
@@ -0,0 +1,28 @@
+package org.mockito.junit5;
+
+
+import org.junit.jupiter.api.extension.AfterEachCallback;
+import org.junit.jupiter.api.extension.BeforeEachCallback;
+import org.junit.jupiter.api.extension.ExtensionContext;
+
+public class MockitoExtension implements BeforeEachCallback, AfterEachCallback{
+    /**
+     * Callback that is invoked <em>after</em> each test has been invoked.
+     *
+     * @param context the current extension context; never {@code null}
+     */
+    @Override
+    public void afterEach(ExtensionContext context) throws Exception {
+
+    }
+
+    /**
+     * Callback that is invoked <em>before</em> each test is invoked.
+     *
+     * @param context the current extension context; never {@code null}
+     */
+    @Override
+    public void beforeEach(ExtensionContext context) throws Exception {
+
+    }
+}
