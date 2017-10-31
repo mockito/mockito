2d0a0a79010b5bb118a07acaf28b2706349e487b
Tim van der Lippe <tvanderlippe@gmail.com>
2017-10-31 13:31:00 +0100

    Clarify documentation on annotation

--- src/main/java/org/mockito/CheckReturnValue.java
+++ src/main/java/org/mockito/CheckReturnValue.java
@@ -7,10 +7,15 @@ import java.lang.annotation.Target;
 
 
 /**
- * Findbugs handles any annotation with name "CheckReturnValue" in return value check.
+ * This annotation is not supposed to be used by Mockito end-users. Instead, we
+ * use it to annotate methods for Static Analysis tools, including FindBugs and ErrorProne.
+ * These tools can check whether the return value of our Mockito methods are actually
+ * used. As such, Mockito State Validation can be performed at compile-time rather than run-time.
+ * This annotation is public, because we have to use it in multiple packages.
  *
  * @see <a href="https://github.com/findbugsproject/findbugs/blob/264ae7baf890d2b347d91805c90057062b5dcb1e/findbugs/src/java/edu/umd/cs/findbugs/detect/BuildCheckReturnAnnotationDatabase.java#L120">Findbugs source code</a>
  * @see <a href="http://errorprone.info/bugpattern/CheckReturnValue">ErrorProne check</a>
+ * @since 2.11.4
  */
 @Target({
     ElementType.CONSTRUCTOR,
