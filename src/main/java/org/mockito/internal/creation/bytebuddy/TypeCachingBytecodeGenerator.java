42163027cee6314935a7720a36f22c70c4937f63
Rafael Winterhalter <rafael.wth@gmail.com>
2017-11-13 22:30:06 +0100

    Addresses #1067: offer singleton class loading lock to avoid loading races.

--- src/main/java/org/mockito/internal/creation/bytebuddy/TypeCachingBytecodeGenerator.java
+++ src/main/java/org/mockito/internal/creation/bytebuddy/TypeCachingBytecodeGenerator.java
@@ -15,11 +15,14 @@ class TypeCachingBytecodeGenerator extends ReferenceQueue<ClassLoader> implement
 
     private final Object BOOTSTRAP_LOCK = new Object();
 
+    private final boolean singletonLock;
+
     private final BytecodeGenerator bytecodeGenerator;
 
     private final TypeCache<MockitoMockKey> typeCache;
 
     public TypeCachingBytecodeGenerator(BytecodeGenerator bytecodeGenerator, boolean weak) {
+        singletonLock = Boolean.getBoolean("org.mockito.lock.singleton");
         this.bytecodeGenerator = bytecodeGenerator;
         typeCache = new TypeCache.WithInlineExpunction<MockitoMockKey>(weak ? TypeCache.Sort.WEAK : TypeCache.Sort.SOFT);
     }
@@ -36,7 +39,7 @@ class TypeCachingBytecodeGenerator extends ReferenceQueue<ClassLoader> implement
                         public Class<?> call() throws Exception {
                             return bytecodeGenerator.mockClass(params);
                         }
-                    }, classLoader == null ? BOOTSTRAP_LOCK : classLoader);
+                    }, singletonLock || classLoader == null ? BOOTSTRAP_LOCK : classLoader);
         } catch (IllegalArgumentException exception) {
             Throwable cause = exception.getCause();
             if (cause instanceof RuntimeException) {
