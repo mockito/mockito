2edf0ce97f25aa879a00b3fc360779a8cbbef3d0
Szczepan Faber <szczepiq@gmail.com>
2018-01-17 10:14:42 -0800

    Added a TODO

--- src/main/java/org/mockito/internal/stubbing/StubbingStrictness.java
+++ src/main/java/org/mockito/internal/stubbing/StubbingStrictness.java
@@ -10,6 +10,8 @@ import org.mockito.stubbing.Stubbing;
 import static org.mockito.Mockito.mockingDetails;
 
 public class StubbingStrictness {
+    //TODO 792 - this does not seem correct - we don't check the leniency of the stubbing itself, only mock-level settings
+    //we should also check if we can pass mock creation settings or mocking details directly to avoid 'mockingDetails()' method overhead
     public static boolean isLenientStubbing(Stubbing stubbing) {
         Strictness mockStrictness = mockingDetails(stubbing.getInvocation().getMock()).getMockCreationSettings().getStrictness();
         return mockStrictness == Strictness.LENIENT;
