8e237472e1b912b2c683281d088dc8b871c68d61
Szczepan Faber <szczepiq@gmail.com>
2018-01-20 18:46:10 -0800

    Added more coverage for edge cases

--- src/test/java/org/mockitousage/strictness/StrictnessWithRulesTest.java
+++ src/test/java/org/mockitousage/strictness/StrictnessWithRulesTest.java
@@ -17,9 +17,11 @@ import org.mockitousage.IMethods;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.mockito.Mockito.lenient;
+import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.withSettings;
 
-public class StrictnessPerStubbingWithRuleTest {
+public class StrictnessWithRulesTest {
 
     @Mock IMethods mock;
     @Rule public MockitoRule rule = MockitoJUnit.rule().strictness(Strictness.STRICT_STUBS);
@@ -49,5 +51,25 @@ public class StrictnessPerStubbingWithRuleTest {
     public void unnecessary_stubbing() {
         //this unnecessary stubbing is not flagged by the rule:
         lenient().when(mock.differentMethod("2")).thenReturn("2");
+    }
+
+    @Test
+    public void rule_changes_strictness() {
+        //when
+        rule.strictness(Strictness.LENIENT);
+
+        //then original mock is lenient:
+        when(mock.simpleMethod(1)).thenReturn("1");
+        mock.simpleMethod(2);
+
+        //but the new strict-stubs mock is strict, even though the rule is not:
+        final IMethods strictStubsMock = mock(IMethods.class, withSettings().strictness(Strictness.STRICT_STUBS));
+        when(strictStubsMock.simpleMethod(1)).thenReturn("1");
+        assertThatThrownBy(new ThrowableAssert.ThrowingCallable() {
+            @Override
+            public void call() throws Throwable {
+                strictStubsMock.simpleMethod(2);
+            }
+        });
     }
 }
