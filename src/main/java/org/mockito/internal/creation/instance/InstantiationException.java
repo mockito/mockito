8d61069930daea18d3077b258f0f13e21ccdf87f
Philip P. Moltmann <moltmann@google.com>
2018-02-05 15:45:38 -0800

    Fixes #1303 : Make Instantiator a public API
    The public API org.mockito.plugins.InstantiatorProvider#getInstantiator
    returns an Instantiator object. Hence the Instantiator class needs to be
    a public too, otherwise the API would be useless.

--- src/main/java/org/mockito/internal/creation/instance/InstantiationException.java
+++ src/main/java/org/mockito/internal/creation/instance/InstantiationException.java
@@ -6,6 +6,15 @@ package org.mockito.internal.creation.instance;
 
 import org.mockito.exceptions.base.MockitoException;
 
+/**
+ * Exception generated when {@link Instantiator#newInstance(Class)} failed.
+ *
+ * <p>This exception is considered public even though it lives in private package.
+ * In the next major version of Mockito, this class will be moved to public space.
+ * </p>
+ *
+ * @since 2.14.0
+ */
 public class InstantiationException extends MockitoException {
 
     public InstantiationException(String message, Throwable cause) {
