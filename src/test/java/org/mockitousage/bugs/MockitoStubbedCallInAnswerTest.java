58aaab1dedf6d8d6903349e23823e7337c347547
Szczepan Faber <szczepiq@gmail.com>
2018-02-11 07:37:29 -0800

    Refactored the test, made it more clear

--- src/test/java/org/mockitousage/bugs/MockitoStubbedCallInAnswerTest.java
+++ src/test/java/org/mockitousage/bugs/MockitoStubbedCallInAnswerTest.java
@@ -5,23 +5,26 @@
 package org.mockitousage.bugs;
 
 import org.junit.Test;
+import org.mockito.Mock;
 import org.mockito.invocation.InvocationOnMock;
 import org.mockito.stubbing.Answer;
+import org.mockitoutil.TestBase;
 
 import static org.junit.Assert.assertEquals;
-import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
 /**
  * @see <a href="https://github.com/mockito/mockito/issues/1279">Issue #1279</a>
  */
-public class MockitoStubbedCallInAnswerTest {
+public class MockitoStubbedCallInAnswerTest extends TestBase {
+
+    @Mock Foo foo;
+    @Mock Bar bar;
 
     @Test
-    public void directInvocationInAnswer() throws Exception {
-        final Foo foo = mock(Foo.class);
-        final Bar bar = mock(Bar.class);
-
+    public void stubbing_the_right_mock() throws Exception {
+        //stubbing on different mock should not be altered
+        when(bar.doInt()).thenReturn(0);
         when(foo.doInt()).thenAnswer(new Answer<Integer>() {
             @Override
             public Integer answer(InvocationOnMock invocation) throws Throwable {
@@ -31,35 +34,34 @@ public class MockitoStubbedCallInAnswerTest {
         assertEquals(0, foo.doInt());
         assertEquals(0, bar.doInt());
 
+        //when we override the stubbing
         when(foo.doInt()).thenReturn(1);
+
+        //we expect it to be reflected:
         assertEquals(1, foo.doInt());
+
+        //but the stubbing on a different mock should not be altered:
         assertEquals(0, bar.doInt());
     }
 
     @Test
-    public void useOtherStubbedMethodReturnTypeInLogic() throws Exception {
-        final Foo foo = mock(Foo.class);
-        final Bar bar = mock(Bar.class);
-
+    public void return_type_validation() throws Exception {
         when(foo.doString()).thenAnswer(new Answer<String>() {
-            @Override
             public String answer(InvocationOnMock invocation) throws Throwable {
+                //invoking a method on a different mock, with different return type
                 return String.valueOf(bar.doInt());
             }
         });
         assertEquals("0", foo.doString());
 
+        //we can override stubbing without misleading return type validation errors:
         when(foo.doString()).thenReturn("");
         assertEquals("", foo.doString());
     }
 
     @Test
-    public void doesNotProduceStackOverflowWhenStubbingTwice() throws Exception {
-        final Foo foo = mock(Foo.class);
-        final Bar bar = mock(Bar.class);
-
+    public void prevents_stack_overflow() throws Exception {
         when(foo.doInt()).thenAnswer(new Answer<Integer>() {
-            @Override
             public Integer answer(InvocationOnMock invocation) throws Throwable {
                 return bar.doInt();
             }
@@ -67,22 +69,19 @@ public class MockitoStubbedCallInAnswerTest {
         assertEquals(0, foo.doInt());
 
         when(foo.doInt()).thenAnswer(new Answer<Integer>() {
-            @Override
             public Integer answer(InvocationOnMock invocation) throws Throwable {
                 return bar.doInt() + 1;
             }
         });
+
+        //calling below used to cause SO error
         assertEquals(1, foo.doInt());
     }
 
     @Test
     public void overriding_stubbing() throws Exception {
-        final Foo foo = mock(Foo.class);
-        final Bar bar = mock(Bar.class);
-
         when(bar.doInt()).thenReturn(10);
         when(foo.doInt()).thenAnswer(new Answer<Integer>() {
-            @Override
             public Integer answer(InvocationOnMock invocation) throws Throwable {
                 return bar.doInt() + 1;
             }
