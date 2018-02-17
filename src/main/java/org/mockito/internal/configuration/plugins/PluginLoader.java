b1c419992e2c88dcf57dceaadc2bd648cb3af25a
Szczepan Faber <szczepiq@gmail.com>
2018-02-17 07:20:29 -0800

    Busted complexity into separate classes
    This way, it will be easier to write unit tests

--- src/main/java/org/mockito/internal/configuration/plugins/PluginLoader.java
+++ src/main/java/org/mockito/internal/configuration/plugins/PluginLoader.java
@@ -4,26 +4,19 @@
  */
 package org.mockito.internal.configuration.plugins;
 
-import org.mockito.internal.util.collections.Iterables;
 import org.mockito.plugins.PluginSwitch;
 
-import java.io.IOException;
 import java.lang.reflect.InvocationHandler;
 import java.lang.reflect.Method;
 import java.lang.reflect.Proxy;
-import java.net.URL;
-import java.util.Enumeration;
 
 class PluginLoader {
 
     private final DefaultMockitoPlugins plugins = new DefaultMockitoPlugins();
+    PluginInitializer initializer;
 
-    private final PluginSwitch pluginSwitch;
-
-    private String alias;
-
-    public PluginLoader(PluginSwitch pluginSwitch) {
-        this.pluginSwitch = pluginSwitch;
+    PluginLoader(PluginSwitch pluginSwitch) {
+        this.initializer = new PluginInitializer(pluginSwitch, null, plugins);
     }
 
     /**
@@ -34,9 +27,8 @@ class PluginLoader {
      * the alias can be used as a convenience name for a known plugin.
      */
     @Deprecated
-    PluginLoader withAlias(String name) {
-        alias = name;
-        return this;
+    PluginLoader(PluginSwitch pluginSwitch, String alias) {
+        this.initializer = new PluginInitializer(pluginSwitch, alias, plugins);
     }
 
     /**
@@ -57,11 +49,11 @@ class PluginLoader {
     @SuppressWarnings("unchecked")
     <PreferredType, AlternateType> Object loadPlugin(final Class<PreferredType> preferredPluginType, final Class<AlternateType> alternatePluginType) {
         try {
-            PreferredType preferredPlugin = loadImpl(preferredPluginType);
+            PreferredType preferredPlugin = initializer.loadImpl(preferredPluginType);
             if (preferredPlugin != null) {
                 return preferredPlugin;
             } else if (alternatePluginType != null) {
-                AlternateType alternatePlugin = loadImpl(alternatePluginType);
+                AlternateType alternatePlugin = initializer.loadImpl(alternatePluginType);
                 if (alternatePlugin != null) {
                     return alternatePlugin;
                 }
@@ -81,39 +73,6 @@ class PluginLoader {
                         }
                     }
                 });
-        }
-    }
-
-    /**
-     * Equivalent to {@link java.util.ServiceLoader#load} but without requiring
-     * Java 6 / Android 2.3 (Gingerbread).
-     */
-    private <T> T loadImpl(Class<T> service) {
-        ClassLoader loader = Thread.currentThread().getContextClassLoader();
-        if (loader == null) {
-            loader = ClassLoader.getSystemClassLoader();
-        }
-        Enumeration<URL> resources;
-        try {
-            resources = loader.getResources("mockito-extensions/" + service.getName());
-        } catch (IOException e) {
-            throw new IllegalStateException("Failed to load " + service, e);
-        }
-
-        try {
-            String classOrAlias = new PluginFinder(pluginSwitch).findPluginClass(Iterables.toIterable(resources));
-            if (classOrAlias != null) {
-                if (classOrAlias.equals(alias)) {
-                    classOrAlias = plugins.getDefaultPluginClass(alias);
-                }
-                Class<?> pluginClass = loader.loadClass(classOrAlias);
-                Object plugin = pluginClass.newInstance();
-                return service.cast(plugin);
-            }
-            return null;
-        } catch (Exception e) {
-            throw new IllegalStateException(
-                    "Failed to load " + service + " implementation declared in " + resources, e);
         }
     }
 }
