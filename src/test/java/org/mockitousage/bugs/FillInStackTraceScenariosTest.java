920d98660702c2b43db6fde521b1284f16414ba2
Szczepan Faber <szczepiq@gmail.com>
2018-03-20 05:54:24 -0700

    Renamed test class and add one more case
    This way we preserve the current functionality in unit tests

--- src/test/java/org/mockitousage/bugs/FillInStackTraceScenariosTest.java
+++ src/test/java/org/mockitousage/bugs/FillInStackTraceScenariosTest.java
@@ -12,25 +12,40 @@ import org.mockitoutil.TestBase;
 import static org.junit.Assert.fail;
 import static org.mockito.Mockito.when;
 
-public class NPEWhenCustomExceptionStackTraceReturnNullTest extends TestBase {
+public class FillInStackTraceScenariosTest extends TestBase {
 
-    @Mock
-    IMethods mock;
+    @Mock IMethods mock;
+
+    private class SomeException extends RuntimeException {}
 
     class NullStackTraceException extends RuntimeException {
-        @Override
         public Exception fillInStackTrace() {
             return null;
         }
     }
 
+    class NewStackTraceException extends RuntimeException {
+        public Exception fillInStackTrace() {
+            return new SomeException();
+        }
+    }
+
     //issue 866
     @Test
-    public void shouldNotThrowNPE() {
+    public void avoids_NPE() {
         when(mock.simpleMethod()).thenThrow(new NullStackTraceException());
         try {
             mock.simpleMethod();
             fail();
         } catch(NullStackTraceException e) {}
+    }
+
+    @Test
+    public void uses_return_value_from_fillInStackTrace() {
+        when(mock.simpleMethod()).thenThrow(new NewStackTraceException());
+        try {
+            mock.simpleMethod();
+            fail();
+        } catch(SomeException e) {}
     }
 }
