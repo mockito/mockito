7fad8aebfa05baa8dea715d36f594bc73cd52282
Szczepan Faber <szczepiq@gmail.com>
2018-04-06 22:50:39 -0700

    Renamed the system property and env variable
     - made it clear that it is an internal property and not a public API (we want to keep the public API front slim)
     - changed the name to indicate intent. It will remind us to remove it when Gradle works with Java11

--- src/main/java/org/mockito/internal/creation/bytebuddy/SubclassInjectionLoader.java
+++ src/main/java/org/mockito/internal/creation/bytebuddy/SubclassInjectionLoader.java
@@ -20,7 +20,7 @@ class SubclassInjectionLoader implements SubclassLoader {
     private final SubclassLoader loader;
 
     SubclassInjectionLoader() {
-        if (!Boolean.getBoolean("org.mockito.reflection.disabled") && ClassInjector.UsingReflection.isAvailable()) {
+        if (!Boolean.getBoolean("org.mockito.internal.simulateJava11") && ClassInjector.UsingReflection.isAvailable()) {
             this.loader = new WithReflection();
         } else if (ClassInjector.UsingLookup.isAvailable()) {
             this.loader = tryLookup();
