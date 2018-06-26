3f98ce5be9f61ba9f9552a7aa3f2a88f1780fa1b
Christian Schwarz <christian.schwarz@iav.de>
2018-06-26 13:33:04 +0200

    Added more tests to AtLeastAndAtMostTest

--- src/test/java/org/mockitousage/verification/within/AtLeastAndAtMostTest.java
+++ src/test/java/org/mockitousage/verification/within/AtLeastAndAtMostTest.java
@@ -5,18 +5,20 @@
 
 package org.mockitousage.verification.within;
 
-import static java.util.concurrent.TimeUnit.MILLISECONDS;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.within;
-
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.ExpectedException;
 import org.mockito.Mock;
 import org.mockito.exceptions.base.MockitoException;
+import org.mockito.exceptions.verification.TooLittleActualInvocations;
+import org.mockito.exceptions.verification.TooManyActualInvocations;
 import org.mockito.junit.MockitoJUnit;
 import org.mockito.junit.MockitoRule;
 import org.mockitousage.IMethods;
+
+import static java.util.concurrent.TimeUnit.MILLISECONDS;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.within;
 
 public class AtLeastAndAtMostTest {
 
@@ -29,7 +31,7 @@ public class AtLeastAndAtMostTest {
     private IMethods mock;
 
     @Test
-    public void atLeastAtMost_tooLowMinimum() throws Exception {
+    public void atLeastAtMost_tooLowMinimum() {
         exception.expect(MockitoException.class);
         exception.expectMessage("The minimum number of invocations must be greater that 0!");
 
@@ -37,7 +39,7 @@ public class AtLeastAndAtMostTest {
     }
 
     @Test
-    public void atLeastAtMost_negativeMinimum() throws Exception {
+    public void atLeastAtMost_negativeMinimum() {
         exception.expect(MockitoException.class);
         exception.expectMessage("The minimum number of invocations must be greater that 0!");
 
@@ -45,7 +47,7 @@ public class AtLeastAndAtMostTest {
     }
 
     @Test
-    public void atLeastAtMost_minimumEqualToMax() throws Exception {
+    public void atLeastAtMost_minimumEqualToMax() {
         exception.expect(MockitoException.class);
         exception.expectMessage("The minimum number of invocations must be greater than the maximum!");
 
@@ -53,11 +55,48 @@ public class AtLeastAndAtMostTest {
     }
 
     @Test
-    public void atLeastAtMost_tooLowMaximum() throws Exception {
+    public void atLeastAtMost_tooLowMaximum() {
         exception.expect(MockitoException.class);
         exception.expectMessage("The maximum number of invocations must be greater than 1!");
 
         verify(mock, within(100, MILLISECONDS).atLeast(1).andAtMost(0)).simpleMethod();
+    }
+
+    @Test
+    public void atLeastAtMost_minNumberOfInvocations() {
+        mock.simpleMethod();
+        verify(mock, within(100, MILLISECONDS).atLeast(1).andAtMost(2)).simpleMethod();
+    }
+
+    @Test
+    public void atLeastAtMost_maxNumberOfInvocations() {
+        mock.simpleMethod();
+        mock.simpleMethod();
+        verify(mock, within(100, MILLISECONDS).atLeast(1).andAtMost(2)).simpleMethod();
+    }
+
+    @Test
+    public void atLeastAtMost_tooManyInvocations() {
+        exception.expect(TooManyActualInvocations.class);
+        exception.expectMessage("mock.simpleMethod()");
+        exception.expectMessage("Wanted 2 times");
+        exception.expectMessage("But was 3 times");
+
+
+        mock.simpleMethod();
+        mock.simpleMethod();
+        mock.simpleMethod();
+        verify(mock, within(100, MILLISECONDS).atLeast(1).andAtMost(2)).simpleMethod();
+    }
+
+    @Test
+    public void atLeastAtMost_tooLittleInvocations() {
+        exception.expect(TooLittleActualInvocations.class);
+        exception.expectMessage("mock.simpleMethod()");
+        exception.expectMessage("Wanted *at least* 1 time");
+        exception.expectMessage("But was 0 times");
+
+        verify(mock, within(100, MILLISECONDS).atLeast(1).andAtMost(2)).simpleMethod();
     }
 
 }
