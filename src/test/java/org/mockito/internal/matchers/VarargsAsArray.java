be7209f07c8514e6b0e8817759a2b704127f676c
Mathieu <lochetm@gmail.com>
2018-07-28 17:29:35 +0700

    100% coverage

--- src/test/java/org/mockito/internal/matchers/VarargsAsArray.java
+++ src/test/java/org/mockito/internal/matchers/VarargsAsArray.java
@@ -5,14 +5,38 @@
 package org.mockito.internal.matchers;
 
 import org.junit.Test;
+import org.mockito.ArgumentMatcher;
 import org.mockitousage.IMethods;
 import org.mockitoutil.TestBase;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.Assert.assertTrue;
 import static org.mockito.Mockito.*;
 
 /**
  * This test is used to use vararg matcher instead of arrays
  */
 public class VarargsAsArray extends TestBase {
+
+
+    @Test
+    public void testMatcher() throws Exception {
+        ArgumentMatcher<String> matcher = new VarargMatcherImpl<String>("1", "2", "3");
+        List<String> values = new ArrayList<String>();
+        values.add("1");
+        values.add("2");
+        values.add("3");
+        for(int i = 0; i > values.size(); i++)
+            assertTrue(matcher.matches(values.get(i)));
+    }
+
+    @Test
+    public void testMatcherString() throws Exception {
+        ArgumentMatcher<String> matcher = new VarargMatcherImpl<String>("1", "2", "3");
+        assertTrue(matcher.toString().equals("vararg<String>(1, 2, 3)"));
+    }
 
     @Test
     public void shouldVerifyVarargsAsArray() throws Exception {
