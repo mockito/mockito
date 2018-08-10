87898cca7ef2292d76d07d4118bc2e8e69924182
Szczepan Faber <szczepiq@gmail.com>
2018-08-10 09:14:57 -0700

    Tidied the test up

--- src/test/java/org/mockitousage/verification/VerificationWithAfterAndCaptorTest.java
+++ src/test/java/org/mockitousage/verification/VerificationWithAfterAndCaptorTest.java
@@ -5,90 +5,44 @@
 
 package org.mockitousage.verification;
 
-import org.junit.Rule;
 import org.junit.Test;
 import org.mockito.ArgumentCaptor;
-import org.mockito.Captor;
-import org.mockito.Mock;
-import org.mockito.junit.MockitoRule;
+import org.mockito.verification.VerificationMode;
 import org.mockitousage.IMethods;
-import org.mockitoutil.Stopwatch;
 
-import static java.util.concurrent.TimeUnit.MILLISECONDS;
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.Assert.assertEquals;
 import static org.mockito.Mockito.after;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.timeout;
 import static org.mockito.Mockito.verify;
-import static org.mockito.junit.MockitoJUnit.rule;
-import static org.mockitoutil.Stopwatch.createNotStarted;
 
 public class VerificationWithAfterAndCaptorTest {
 
-    @Rule public MockitoRule mockito = rule();
-
-    @Mock private IMethods mock;
-
-    @Captor private ArgumentCaptor<Character> captor;
-
-    private Stopwatch watch = createNotStarted();
-
     /**
-     * Test for issue #345.
+     * Test for issue #345 and similar use cases
      */
     @Test
-    public void shouldReturnListOfArgumentsWithSameSizeAsGivenInAtMostVerification() {
-        // given
-        int n = 3;
+    public void should_capture_correct_number_of_values() {
+        verifyCaptor(after(200).atMost(3));
+        verifyCaptor(after(200).times(3));
+        verifyCaptor(after(200).atLeast(3));
 
-        // when
-        exerciseMockNTimes(n);
-
-        watch.start();
-
-        // then
-        verify(mock, after(200).atMost(n)).oneArg((char) captor.capture());
-
-        watch.assertElapsedTimeIsMoreThan(200, MILLISECONDS);
-        assertThat(captor.getAllValues()).containsExactly('0', '1', '2');
+        verifyCaptor(timeout(200).atLeast(3));
+        verifyCaptor(timeout(200).times(3));
     }
 
-    @Test
-//    @Ignore("TODO review after #936")
-    public void shouldReturnListOfArgumentsWithSameSizeAsGivenInTimesVerification() {
-        // given
-        int n = 3;
+    private void verifyCaptor(VerificationMode mode) {
+        IMethods mock = mock(IMethods.class);
+        ArgumentCaptor<Character> captor = ArgumentCaptor.forClass(Character.class);
 
         // when
-        exerciseMockNTimes(n);
-
-        //Then
-        verify(mock, after(200).times(n)).oneArg((char) captor.capture());
-        assertEquals(n, captor.getAllValues().size());
-        assertEquals('0', (char) captor.getAllValues().get(0));
-        assertEquals('1', (char) captor.getAllValues().get(1));
-        assertEquals('2', (char) captor.getAllValues().get(2));
-    }
-
-    @Test
-//    @Ignore("TODO review after #936")
-    public void shouldReturnListOfArgumentsWithSameSizeAsGivenInAtLeastVerification() {
-        // given
-        int n = 3;
-
-        // when
-        exerciseMockNTimes(n);
-
-        //Then
-        verify(mock, after(200).atLeast(n)).oneArg((char) captor.capture());
-        assertEquals(n, captor.getAllValues().size());
-        assertEquals('0', (char) captor.getAllValues().get(0));
-        assertEquals('1', (char) captor.getAllValues().get(1));
-        assertEquals('2', (char) captor.getAllValues().get(2));
-    }
-
-    private void exerciseMockNTimes(int n) {
-        for (int i = 0; i < n; i++) {
+        for (int i = 0; i < 3; i++) {
             mock.oneArg((char) ('0' + i));
         }
+
+        // then
+        verify(mock, mode).oneArg((char) captor.capture());
+
+        assertThat(captor.getAllValues()).containsExactly('0', '1', '2');
     }
 }
