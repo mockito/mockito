8d2b9b23221732a1910af7ba1dbf19a675e9ff78
Denis Zharkov <dzharkov@users.noreply.github.com>
2018-10-05 14:51:14 +0300

    Support mocking kotlin suspend functions compiled by Kotlin 1.3 (#1500) (#1501)
    * Fixes #1500: Support mocking kotlin suspend functions compiled by Kotlin 1.3
    
    Coroutines support is not experimental anymore and a type
    of continuation-parameter is renamed to kotlin.coroutines.Continuation
    
    * Move dependencies description to dependencies.gradle (#1500)

--- src/main/java/org/mockito/internal/creation/SuspendMethod.java
+++ src/main/java/org/mockito/internal/creation/SuspendMethod.java
@@ -11,12 +11,18 @@ import java.util.Arrays;
  * See <a href="https://github.com/Kotlin/kotlin-coroutines/blob/master/kotlin-coroutines-informal.md#continuation-passing-style">Design docs for details</a>.
  */
 public class SuspendMethod {
-    private static final String KOTLIN_CONTINUATION = "kotlin.coroutines.experimental.Continuation";
+    private static final String KOTLIN_EXPERIMENTAL_CONTINUATION = "kotlin.coroutines.experimental.Continuation";
+    private static final String KOTLIN_CONTINUATION = "kotlin.coroutines.Continuation";
 
     public static Class<?>[] trimSuspendParameterTypes(Class<?>[] parameterTypes) {
         int n = parameterTypes.length;
-        if (n > 0 && parameterTypes[n - 1].getName().equals(KOTLIN_CONTINUATION))
+        if (n > 0 && isContinuationType(parameterTypes[n - 1]))
             return Arrays.copyOf(parameterTypes, n - 1);
         return parameterTypes;
+    }
+
+    private static boolean isContinuationType(Class<?> parameterType) {
+        String name = parameterType.getName();
+        return name.equals(KOTLIN_CONTINUATION) || name.equals(KOTLIN_EXPERIMENTAL_CONTINUATION);
     }
 }
