8a614c20f10d440fbc9ccc4afd116d6e4f145800
Patouche <Patouche@users.noreply.github.com>
2019-01-06 21:42:29 +0100

    Fix/bug 1551 cce on smart not null answers (#1576)
    * Fix #1551 : Add support for Generic in ReturnsSmartNulls
    
    * Fix #1551 : Integrate support for generic using varargs
    
    * #1551 : Fix typo and some missing documentation

--- src/main/java/org/mockito/internal/stubbing/defaultanswers/ReturnsSmartNulls.java
+++ src/main/java/org/mockito/internal/stubbing/defaultanswers/ReturnsSmartNulls.java
@@ -8,12 +8,18 @@ import static org.mockito.internal.exceptions.Reporter.smartNullPointerException
 import static org.mockito.internal.util.ObjectMethodsGuru.isToStringMethod;
 
 import java.io.Serializable;
+import java.lang.reflect.GenericArrayType;
 import java.lang.reflect.Modifier;
+import java.lang.reflect.Type;
+import java.lang.reflect.TypeVariable;
 
 import org.mockito.Mockito;
 import org.mockito.internal.debugging.LocationImpl;
+import org.mockito.internal.util.MockUtil;
+import org.mockito.internal.util.reflection.GenericMetadataSupport;
 import org.mockito.invocation.InvocationOnMock;
 import org.mockito.invocation.Location;
+import org.mockito.mock.MockCreationSettings;
 import org.mockito.stubbing.Answer;
 
 /**
@@ -46,15 +52,108 @@ public class ReturnsSmartNulls implements Answer<Object>, Serializable {
             return defaultReturnValue;
         }
         Class<?> type = invocation.getMethod().getReturnType();
-        if (!type.isPrimitive() && !Modifier.isFinal(type.getModifiers())) {
+
+        final Type returnType = invocation.getMethod().getGenericReturnType();
+        if (returnType instanceof TypeVariable) {
+            type = findTypeFromGeneric(invocation, (TypeVariable) returnType);
+            if (type != null) {
+                defaultReturnValue = delegateChains(type);
+            }
+        }
+        if (defaultReturnValue != null) {
+            return defaultReturnValue;
+        }
+
+        if (type != null && !type.isPrimitive() && !Modifier.isFinal(type.getModifiers())) {
             final Location location = new LocationImpl();
             return Mockito.mock(type, new ThrowsSmartNullPointer(invocation, location));
         }
         return null;
     }
 
+    /**
+     * Try to resolve the result value using {@link ReturnsEmptyValues} and {@link ReturnsMoreEmptyValues}.
+     *
+     * This will try to use all parent class (superclass & interfaces) to retrieve the value..
+     *
+     * @param type the return type of the method
+     * @return a non-null instance if the type has been resolve. Null otherwise.
+     */
+    private Object delegateChains(final Class<?> type) {
+        final ReturnsEmptyValues returnsEmptyValues = new ReturnsEmptyValues();
+        Object result = returnsEmptyValues.returnValueFor(type);
+
+        if (result == null) {
+            Class<?> emptyValueForClass = type;
+            while (emptyValueForClass != null && result == null) {
+                final Class<?>[] classes = emptyValueForClass.getInterfaces();
+                for (Class<?> clazz : classes) {
+                    result = returnsEmptyValues.returnValueFor(clazz);
+                    if (result != null) {
+                        break;
+                    }
+                }
+                emptyValueForClass = emptyValueForClass.getSuperclass();
+            }
+        }
+
+        if (result == null) {
+            result = new ReturnsMoreEmptyValues().returnValueFor(type);
+        }
+
+        return result;
+    }
+
+    /**
+     * Retrieve the expected type when it came from a primitive. If the type cannot be retrieve, return null.
+     *
+     * @param invocation the current invocation
+     * @param returnType the expected return type
+     * @return the type or null if not found
+     */
+    private Class<?> findTypeFromGeneric(final InvocationOnMock invocation, final TypeVariable returnType) {
+        // Class level
+        final MockCreationSettings mockSettings = MockUtil.getMockHandler(invocation.getMock()).getMockSettings();
+        final GenericMetadataSupport returnTypeSupport = GenericMetadataSupport
+            .inferFrom(mockSettings.getTypeToMock())
+            .resolveGenericReturnType(invocation.getMethod());
+        final Class<?> rawType = returnTypeSupport.rawType();
+
+        // Method level
+        if (rawType == Object.class) {
+            return findTypeFromGenericInArguments(invocation, returnType);
+        }
+        return rawType;
+    }
+
+    /**
+     * Find a return type using generic arguments provided by the calling method.
+     *
+     * @param invocation the current invocation
+     * @param returnType the expected return type
+     * @return the return type or null if the return type cannot be found
+     */
+    private Class<?> findTypeFromGenericInArguments(final InvocationOnMock invocation, final TypeVariable returnType) {
+        final Type[] parameterTypes = invocation.getMethod().getGenericParameterTypes();
+        for (int i = 0; i < parameterTypes.length; i++) {
+            Type argType = parameterTypes[i];
+            if (returnType.equals(argType)) {
+                return invocation.getArgument(i).getClass();
+            }
+            if (argType instanceof GenericArrayType) {
+                argType = ((GenericArrayType) argType).getGenericComponentType();
+                if (returnType.equals(argType)) {
+                    return invocation.getArgument(i).getClass();
+                }
+            }
+        }
+        return null;
+    }
+
     private static class ThrowsSmartNullPointer implements Answer {
+
         private final InvocationOnMock unstubbedInvocation;
+
         private final Location location;
 
         public ThrowsSmartNullPointer(InvocationOnMock unstubbedInvocation, Location location) {
@@ -65,7 +164,7 @@ public class ReturnsSmartNulls implements Answer<Object>, Serializable {
         public Object answer(InvocationOnMock currentInvocation) throws Throwable {
             if (isToStringMethod(currentInvocation.getMethod())) {
                 return "SmartNull returned by this unstubbed method call on a mock:\n" +
-                        unstubbedInvocation.toString();
+                    unstubbedInvocation.toString();
             }
 
             throw smartNullPointerException(unstubbedInvocation.toString(), location);
