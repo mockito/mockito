8f99e4749f62a37d3edbb18c33c860e3cd157e76
Rafael Winterhalter <rafael.wth@gmail.com>
2019-01-16 22:27:58 +0100

    Add package info to satisfy javadoc plugin and exclude it to avoid adding a package to the module.

---
+++ src/main/java/org/mockito/internal/creation/bytebuddy/inject/package-info.java
@@ -0,0 +1,11 @@
+/*
+ * Copyright (c) 2007 Mockito contributors
+ * This program is made available under the terms of the MIT License.
+ */
+
+/**
+ * Internal classes, not to be used by clients. Intended for injection into the bootstrap class loader.
+ *
+ * Subject to change at any time without notice.
+ */
+package org.mockito.internal.creation.bytebuddy.inject;
