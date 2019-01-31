de957b10b6bb591914cc0e805bde3be086458247
Matthew Ouyang <mouyang@users.noreply.github.com>
2019-01-31 16:39:01 -0700

    Fixes #1578: handle multiple invocations on MockitoSession (#1596)
    - Problem - JUnitRule detects an unfinished mocking session when the apply method is invoked more than once.  This commonly happens with Runners that enable developers to write nested test classes.
    - Fix - If a MockitoSession has been established for JUnitRule, then use that and initialize any new mocks (typically from a nested test class).
    - Test - Instantiate one @Rule and assign it to a second one.  This will simulate the apply method being invoked more than once without introducing a new test dependency.

--- src/test/java/org/mockitousage/junitrule/MockitoJUnitRuleTest.java
+++ src/test/java/org/mockitousage/junitrule/MockitoJUnitRuleTest.java
@@ -19,6 +19,10 @@ public class MockitoJUnitRuleTest {
     @Rule
     public MockitoRule mockitoRule = MockitoJUnit.rule();
 
+    // Fixes #1578: Protect against multiple execution.
+    @Rule
+    public MockitoRule mockitoRule2 = mockitoRule;
+
     @Mock
     private Injected injected;
 
