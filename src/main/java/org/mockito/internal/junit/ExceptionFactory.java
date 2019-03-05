b756a37819ae8eebb7374f204c03b31aba8699d9
kriegfrj <fr.jkrieg@greekwelfaresa.org.au>
2019-03-05 15:54:31 +1030

    Refactor ExceptionFactory to make use of interface.
    Removes the need to check for JUnit each time createArgumentsAreDifferentException() is
    called and also makes it easy to 1. extend and 2. streamline further if source 1.8
    compatibility is introduced.

--- src/main/java/org/mockito/internal/junit/ExceptionFactory.java
+++ src/main/java/org/mockito/internal/junit/ExceptionFactory.java
@@ -4,49 +4,47 @@
  */
 package org.mockito.internal.junit;
 
-import junit.framework.ComparisonFailure;
 import org.mockito.exceptions.verification.ArgumentsAreDifferent;
 
 public class ExceptionFactory {
 
-    private final static boolean hasJUnit = canLoadJunitClass();
-
     private ExceptionFactory() {
     }
 
+    private static interface ExceptionFactoryImpl {
+        AssertionError create(String message, String wanted, String actual);
+    }
+    
+    private final static ExceptionFactoryImpl factory;
+    
+    static {
+        ExceptionFactoryImpl theFactory = null;
+        
+        try {
+            // The following is neater but requires -source >= 1.8
+            // theFactory = org.mockito.exceptions.verification.junit.ArgumentsAreDifferent::new;
+            theFactory = new ExceptionFactoryImpl() {
+                @Override
+                public AssertionError create(String message, String wanted, String actual) {
+                    return new org.mockito.exceptions.verification.junit.ArgumentsAreDifferent(message, wanted, actual);
+                }
+            };
+        } catch (Throwable onlyIfJUnit3IsNotAvailable) {
+        }
+        // The following is much neater but requires -source >= 1.8
+        // factory = (theFactory == null) ? ArgumentsAreDifferent::new : theFactory;
+        factory = (theFactory == null) ? new ExceptionFactoryImpl() {
+            @Override
+            public AssertionError create(String message, String wanted, String actual) {
+                return new ArgumentsAreDifferent(message, wanted, actual);
+            }
+        } : theFactory;
+    }
+    
     /**
      * If JUnit is used, an AssertionError is returned that extends from JUnit {@link ComparisonFailure} and hence provide a better IDE support as the comparison result is comparable
      */
     public static AssertionError createArgumentsAreDifferentException(String message, String wanted, String actual) {
-        if (hasJUnit) {
-            return createJUnitArgumentsAreDifferent(message, wanted, actual);
-        }
-        return new ArgumentsAreDifferent(message);
-    }
-
-    private static AssertionError createJUnitArgumentsAreDifferent(String message, String wanted, String actual) {
-        return JUnitArgsAreDifferent.create(message, wanted, actual);
-    }
-
-    private static boolean canLoadJunitClass() {
-        try {
-            JUnitArgsAreDifferent.create("message", "wanted", "actual");
-        } catch (Throwable onlyIfJUnitIsNotAvailable) {
-            return false;
-        }
-        return true;
-    }
-
-    /**
-     * Don't inline this class! It allows create the JUnit-ArgumentsAreDifferent exception without the need to use reflection.
-     * <p>
-     * If JUnit is not available a call to {@link #create(String, String, String)} will throw a {@link NoClassDefFoundError}.
-     * The {@link NoClassDefFoundError} will be thrown by the class loader cause the JUnit class {@link ComparisonFailure}
-     * can't be loaded which is a upper class of ArgumentsAreDifferent.
-     */
-    private static class JUnitArgsAreDifferent {
-        static AssertionError create(String message, String wanted, String actual) {
-            return new org.mockito.exceptions.verification.junit.ArgumentsAreDifferent(message, wanted, actual);
-        }
+        return factory.create(message, wanted, actual);
     }
 }
