9b46745009f598eb035cee663bdeba858fe4c3e3
kriegfrj <fr.jkrieg@greekwelfaresa.org.au>
2019-03-05 15:54:31 +1030

    Refactor ExceptionFactory to make use of interface.
    Removes the need to check for JUnit each time createArgumentsAreDifferentException() is
    called and also makes it easy to 1. extend and 2. streamline further if source 1.8
    compatibility is introduced.

--- src/test/java/org/mockito/internal/junit/ExceptionFactoryTest.java
+++ src/test/java/org/mockito/internal/junit/ExceptionFactoryTest.java
@@ -8,32 +8,32 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockitoutil.ClassLoaders.excludingClassLoader;
 
 import java.lang.reflect.Method;
+
 import org.junit.BeforeClass;
 import org.junit.Test;
 import org.mockito.exceptions.verification.ArgumentsAreDifferent;
 
 public class ExceptionFactoryTest {
 
-    private static ClassLoader classLoaderWithoutJUnit = excludingClassLoader().withCodeSourceUrlOf(ExceptionFactory.class).without("org.junit", "junit").build();
-
-    /** loaded by the current current class loader */
+    private static ClassLoader classLoaderWithoutJUnit  = excludingClassLoader().withCodeSourceUrlOf(ExceptionFactory.class).without("org.junit", "junit").build();
+    
+    /** loaded by the current classloader */
+    private static Class<?> comparisonFailure;
     private static Class<?> junitArgumentsAreDifferent;
 
-    /** loaded by the custom classloader {@value #classLoaderWithoutJUnit}, which excludes junit-classes */
+    /** loaded by the custom classloader {@value #classLoaderWithoutJUnit}, which excludes JUnit classes */
     private static Class<?> nonJunitArgumentsAreDifferent;
 
     @BeforeClass
     public static void init() throws ClassNotFoundException {
         nonJunitArgumentsAreDifferent = classLoaderWithoutJUnit.loadClass(ArgumentsAreDifferent.class.getName());
+        comparisonFailure = junit.framework.ComparisonFailure.class;
         junitArgumentsAreDifferent = org.mockito.exceptions.verification.junit.ArgumentsAreDifferent.class;
     }
 
     @Test
     public void createArgumentsAreDifferentException_withoutJUnit() throws Exception {
-        Class<?> exceptionFactory = classLoaderWithoutJUnit.loadClass(ExceptionFactory.class.getName());
-
-        Method m = exceptionFactory.getDeclaredMethod("createArgumentsAreDifferentException", String.class, String.class, String.class);
-        Object e = m.invoke(null, "message", "wanted", "actual");
+        AssertionError e = invokeFactoryThroughLoader(classLoaderWithoutJUnit);
 
         assertThat(e).isExactlyInstanceOf(nonJunitArgumentsAreDifferent);
     }
@@ -42,17 +42,24 @@ public class ExceptionFactoryTest {
     public void createArgumentsAreDifferentException_withJUnit() throws Exception {
         AssertionError e = ExceptionFactory.createArgumentsAreDifferentException("message", "wanted", "actual");
 
-        assertThat(e).isExactlyInstanceOf(junitArgumentsAreDifferent);
+        assertThat(e).isExactlyInstanceOf(junitArgumentsAreDifferent).isInstanceOf(comparisonFailure);
     }
 
     @Test
-    public void createArgumentsAreDifferentException_withJUnit2x() throws Exception {
+    public void createArgumentsAreDifferentException_withJUnit_2x() throws Exception {
         AssertionError e;
-
+        
         e = ExceptionFactory.createArgumentsAreDifferentException("message", "wanted", "actual");
         assertThat(e).isExactlyInstanceOf(junitArgumentsAreDifferent);
 
         e = ExceptionFactory.createArgumentsAreDifferentException("message", "wanted", "actual");
         assertThat(e).isExactlyInstanceOf(junitArgumentsAreDifferent);
+    }
+
+    private static AssertionError invokeFactoryThroughLoader(ClassLoader loader) throws Exception {
+        Class<?> exceptionFactory = loader.loadClass(ExceptionFactory.class.getName());
+
+        Method m = exceptionFactory.getDeclaredMethod("createArgumentsAreDifferentException", String.class, String.class, String.class);
+        return (AssertionError) m.invoke(null, "message", "wanted", "actual");
     }
 }
