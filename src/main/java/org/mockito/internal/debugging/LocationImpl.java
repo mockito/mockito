c16f43ac4f9ed980626586184f621b3a7b57eb2d
Tim van der Lippe <tvanderlippe@google.com>
2019-04-10 13:05:36 +0100

    Implement fast-path computation for stackTrace filtering
    This significantly speeds up average test durations. On one internal
    test, before this change it was timing out after 60 seconds. After this
    change, it reliably completes within 25 seconds.

--- src/main/java/org/mockito/internal/debugging/LocationImpl.java
+++ src/main/java/org/mockito/internal/debugging/LocationImpl.java
@@ -11,35 +11,26 @@ import org.mockito.invocation.Location;
 public class LocationImpl implements Location, Serializable {
 
     private static final long serialVersionUID = -9054861157390980624L;
-    //Limit the amount of objects being created, as this class is heavily instantiated:
-    private static final StackTraceFilter defaultStackTraceFilter = new StackTraceFilter();
+    // Limit the amount of objects being created, as this class is heavily instantiated:
+    private static final StackTraceFilter stackTraceFilter = new StackTraceFilter();
 
-    private final String stackTraceLine;
-    private final StackTraceFilter stackTraceFilter;
-    private final String sourceFile;
+    private String stackTraceLine;
+    private String sourceFile;
 
     public LocationImpl() {
-        this(defaultStackTraceFilter);
+        this(new Throwable());
+    }
+
+    public LocationImpl(Throwable stackTraceHolder) {
+        this(stackTraceFilter, stackTraceHolder);
     }
 
     public LocationImpl(StackTraceFilter stackTraceFilter) {
         this(stackTraceFilter, new Throwable());
     }
 
-    public LocationImpl(Throwable stackTraceHolder) {
-        this(defaultStackTraceFilter, stackTraceHolder);
-    }
-
     private LocationImpl(StackTraceFilter stackTraceFilter, Throwable stackTraceHolder) {
-        this.stackTraceFilter = stackTraceFilter;
-        this.stackTraceLine = computeStacktraceLine(this.stackTraceFilter, stackTraceHolder);
-        if (stackTraceHolder.getStackTrace() == null || stackTraceHolder.getStackTrace().length == 0) {
-            //there are corner cases where exception can have a null or empty stack trace
-            //for example, a custom exception can override getStackTrace() method
-            this.sourceFile = "<unknown source file>";
-        } else {
-            this.sourceFile = stackTraceFilter.findSourceFile(stackTraceHolder.getStackTrace(), "<unknown source file>");
-        }
+        computeStackTraceInformation(stackTraceFilter, stackTraceHolder);
     }
 
     @Override
@@ -49,16 +40,22 @@ public class LocationImpl implements Location, Serializable {
 
     /**
      * Eagerly compute the stacktrace line from the stackTraceHolder. Storing the Throwable is
-     * memory-intensive for tests that have large stacktraces and have a lot of invocations on mocks.
+     * memory-intensive for tests that have large stacktraces and have a lot of invocations on
+     * mocks.
      */
-    private static String computeStacktraceLine(
+    private void computeStackTraceInformation(
         StackTraceFilter stackTraceFilter, Throwable stackTraceHolder) {
-        StackTraceElement[] filtered =
-            stackTraceFilter.filter(stackTraceHolder.getStackTrace(), false);
-        if (filtered.length == 0) {
-            return "-> at <<unknown line>>";
+        StackTraceElement filtered = stackTraceFilter.filterFirst(stackTraceHolder);
+
+        // there are corner cases where exception can have a null or empty stack trace
+        // for example, a custom exception can override getStackTrace() method
+        if (filtered == null) {
+            this.stackTraceLine = "-> at <<unknown line>>";
+            this.sourceFile = "<unknown source file>";
+        } else {
+            this.stackTraceLine = "-> at " + filtered.toString();
+            this.sourceFile = filtered.getFileName();
         }
-        return "-> at " + filtered[0].toString();
     }
 
     @Override
