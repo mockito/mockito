9ee1e218b83277810eb0394e45aa585797c3cffb
Tom-Brown <tom.brown@domo.com>
2019-05-01 08:56:02 -0600

    Added separate test

--- src/test/java/org/mockitousage/stubbing/CloningParameterTest.java
+++ src/test/java/org/mockitousage/stubbing/CloningParameterTest.java
@@ -27,12 +27,12 @@ public class CloningParameterTest extends TestBase {
         businessLogic(emailSender);
 
         // then
-        verify(emailSender).sendEmail(eq(1), eq(new Person("Wes")), (Person)any(), (Person[])any());
+        verify(emailSender).sendEmail(1, new Person("Wes"));
     }
 
     private void businessLogic(EmailSender emailSender) {
         Person person = new Person("Wes");
-        emailSender.sendEmail(1, person, null, new Person[0]);
+        emailSender.sendEmail(1, person);
         person.emailSent();
     }
 
@@ -48,6 +48,34 @@ public class CloningParameterTest extends TestBase {
 
         // then
         assertNotNull(emails);
+    }
+
+    @Test
+    public void shouldCloneArrays() throws Exception {
+
+        EmailSender emailSender = mock(EmailSender.class, new ClonesArguments());
+
+        // 1. Pass an array into a mock that "ClonesArguments"
+        Person[] ccList = new Person[] { new Person("Wes") };
+        emailSender.sendGroupEmail(1, ccList);
+
+        // 2. Mutate the array
+        ccList[0] = new Person("Joe");
+
+        // 3. Verify that the mock made a copy of the array
+        verify(emailSender).sendGroupEmail(1, new Person[] { new Person("Wes") });
+    }
+
+    @Test
+    public void shouldNotThrowNPEWhenCloningNulls() throws Exception {
+
+        EmailSender emailSender = mock(EmailSender.class, new ClonesArguments());
+
+        // 1. Pass a null into a mock that "ClonesArguments"
+        emailSender.sendEmail(1, (Person)null);
+
+        // 2. Verify that the null argument was captured
+        verify(emailSender).sendEmail(eq(1), (Person)isNull());
     }
 
     public class Person {
@@ -102,7 +130,9 @@ public class CloningParameterTest extends TestBase {
 
     public interface EmailSender {
 
-        void sendEmail(int i, Person person, Person from, Person[] ccList);
+        void sendEmail(int i, Person person);
+
+        void sendGroupEmail(int i, Person[] persons);
 
         List<?> getAllEmails(Person person);
 
