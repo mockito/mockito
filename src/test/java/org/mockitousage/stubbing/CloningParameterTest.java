96557452ebf0ef8a5448eff55aa930b43df7c433
tombrown52 <tombrown52@gmail.com>
2019-05-01 09:27:10 -0600

    Fix ClonesArguments for null and arrays (#1698)

--- src/test/java/org/mockitousage/stubbing/CloningParameterTest.java
+++ src/test/java/org/mockitousage/stubbing/CloningParameterTest.java
@@ -12,6 +12,7 @@ import org.mockitoutil.TestBase;
 import java.util.List;
 
 import static org.junit.Assert.assertNotNull;
+import static org.mockito.ArgumentMatchers.*;
 import static org.mockito.Mockito.*;
 
 public class CloningParameterTest extends TestBase {
@@ -47,6 +48,34 @@ public class CloningParameterTest extends TestBase {
 
         // then
         assertNotNull(emails);
+    }
+
+    @Test
+    public void shouldCloneArrays() throws Exception {
+
+        EmailSender emailSender = mock(EmailSender.class, new ClonesArguments());
+
+        // 1. Pass an array into a mock that "ClonesArguments"
+        Person[] ccList = new Person[] { new Person("Wes") };
+        emailSender.sendGroupEmail(1, ccList);
+
+        // 2. Mutate the array
+        ccList[0] = new Person("Joe");
+
+        // 3. Verify that the mock made a copy of the array
+        verify(emailSender).sendGroupEmail(1, new Person[] { new Person("Wes") });
+    }
+
+    @Test
+    public void shouldNotThrowNPEWhenCloningNulls() throws Exception {
+
+        EmailSender emailSender = mock(EmailSender.class, new ClonesArguments());
+
+        // 1. Pass a null into a mock that "ClonesArguments"
+        emailSender.sendEmail(1, (Person)null);
+
+        // 2. Verify that the null argument was captured
+        verify(emailSender).sendEmail(eq(1), (Person)isNull());
     }
 
     public class Person {
@@ -102,6 +131,8 @@ public class CloningParameterTest extends TestBase {
     public interface EmailSender {
 
         void sendEmail(int i, Person person);
+
+        void sendGroupEmail(int i, Person[] persons);
 
         List<?> getAllEmails(Person person);
 
