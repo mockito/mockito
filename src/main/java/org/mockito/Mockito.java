9ff643de0a48a6d0f8a7954448647487ef479530
Jakub Vrána <jakubvrana@google.com>
2019-05-06 16:30:35 +0200

    Fix a typo

--- src/main/java/org/mockito/Mockito.java
+++ src/main/java/org/mockito/Mockito.java
@@ -694,7 +694,7 @@ import org.mockito.verification.VerificationWithTimeout;
  * Also it may reduce defect localization because if stubbed method was not called then no argument is captured.
  * <p>
  * In a way ArgumentCaptor is related to custom argument matchers (see javadoc for {@link ArgumentMatcher} class).
- * Both techniques can be used for making sure certain arguments where passed to mocks.
+ * Both techniques can be used for making sure certain arguments were passed to mocks.
  * However, ArgumentCaptor may be a better fit if:
  * <ul>
  * <li>custom argument matcher is not likely to be reused</li>
