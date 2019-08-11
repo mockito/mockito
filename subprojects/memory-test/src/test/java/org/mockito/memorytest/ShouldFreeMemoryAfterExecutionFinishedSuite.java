7767e37d9500909e40cf6caa7b0bbce27fc7c327
Andreas Willinger <awi95@gmx.at>
2019-08-11 19:07:12 +0200

    Add test/suite showcasing memory leak described in issue #1736

---
+++ subprojects/memory-test/src/test/java/org/mockito/memorytest/ShouldFreeMemoryAfterExecutionFinishedSuite.java
@@ -0,0 +1,10 @@
+package org.mockito.memorytest;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@SuiteClasses({ShouldFreeMemoryAfterExecutionFinishedTest.class, ShouldFreeMemoryAfterExecutionFinishedTest.class})
+@RunWith(Suite.class)
+public class ShouldFreeMemoryAfterExecutionFinishedSuite {
+}
---
+++ subprojects/memory-test/src/test/java/org/mockito/memorytest/ShouldFreeMemoryAfterExecutionFinishedTest.java
@@ -0,0 +1,33 @@
+package org.mockito.memorytest;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.junit.MockitoJUnitRunner;
+
+@RunWith(MockitoJUnitRunner.class)
+public class ShouldFreeMemoryAfterExecutionFinishedTest {
+    private static final int ELEMENT_COUNT = 4_000_000;
+
+    private MemoryHog memoryHog;
+
+    @Before
+    public void setUp() {
+        memoryHog = new MemoryHog();
+    }
+
+    @Test
+    public void test() {
+        memoryHog.run();
+    }
+
+    static class MemoryHog {
+        private Long[] values = new Long[ELEMENT_COUNT];
+
+        void run() {
+            for (int i = 0; i < ELEMENT_COUNT; i++) {
+                values[i] = (long) i;
+            }
+        }
+    }
+}
