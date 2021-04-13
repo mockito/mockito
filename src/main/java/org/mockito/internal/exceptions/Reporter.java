5d388e280c19cff8427697324e93fff63dff084a
Jon-Man <72472369+Jon-Man@users.noreply.github.com>
2021-04-12 21:10:57 -0400

    Update Reporter.java

--- src/main/java/org/mockito/internal/exceptions/Reporter.java
+++ src/main/java/org/mockito/internal/exceptions/Reporter.java
@@ -298,10 +298,12 @@ public class Reporter {
                         new LocationImpl(),
                         ""));
     }
-    #TODO: Update for loop to use braces
+
     private static Object locationsOf(Collection<LocalizedMatcher> matchers) {
         List<String> description = new ArrayList<String>();
-        for (LocalizedMatcher matcher : matchers) description.add(matcher.getLocation().toString());
+        for (LocalizedMatcher matcher : matchers) {
+            description.add(matcher.getLocation().toString());
+        }
         return join(description.toArray());
     }
 
