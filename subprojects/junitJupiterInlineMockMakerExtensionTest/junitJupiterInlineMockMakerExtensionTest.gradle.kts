066aef2b5329d79a9f4601dcd5ace791b9103f1f
Tim van der Lippe <tvanderlippe@google.com>
2021-08-27 16:12:38 +0100

    Upgrade Gradle to version 7.2
    This includes the removal of the compile configuration (applying the
    tips in
    https://docs.gradle.org/current/userguide/upgrading_version_6.html#sec:configuration_removal
    ) and reworks the copy-task for inline-mock to avoid an implicit
    dependency (per
    https://discuss.gradle.org/t/gradle-7-0-seems-to-take-an-overzealous-approach-to-inter-task-dependencies/39656/2
    )

--- subprojects/junitJupiterInlineMockMakerExtensionTest/junitJupiterInlineMockMakerExtensionTest.gradle.kts
+++ subprojects/junitJupiterInlineMockMakerExtensionTest/junitJupiterInlineMockMakerExtensionTest.gradle.kts
@@ -7,11 +7,11 @@ plugins {
 description = "End-to-end tests for automatic registration of MockitoExtension with the inline mock maker."
 
 dependencies {
-    testCompile(project(":junit-jupiter"))
-    testCompile(library("assertj"))
-    testCompile(library("junitPlatformLauncher"))
-    testCompile(library("junitJupiterApi"))
-    testRuntime(library("junitJupiterEngine"))
+    testImplementation(project(":junit-jupiter"))
+    testImplementation(library("assertj"))
+    testImplementation(library("junitPlatformLauncher"))
+    testImplementation(library("junitJupiterApi"))
+    testRuntimeOnly(library("junitJupiterEngine"))
 }
 
 tasks.named<Test>("test") {
