2c30062eb70669094787a6cb831e12b8fb6d80f4
temp-droid <gatien.bovyn@hotmail.com>
2021-12-16 19:56:52 +0100

    Replace ExpectedException in ChecksTest

--- src/test/java/org/mockito/internal/util/ChecksTest.java
+++ src/test/java/org/mockito/internal/util/ChecksTest.java
@@ -4,36 +4,40 @@
  */
 package org.mockito.internal.util;
 
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.Assert.assertEquals;
 
-import org.junit.Rule;
 import org.junit.Test;
-import org.junit.rules.ExpectedException;
 
 public class ChecksTest {
-    @Rule public ExpectedException expectedException = ExpectedException.none();
 
     @Test
-    public void checkNotNull_not_null() throws Exception {
+    public void checkNotNull_not_null() {
         assertEquals("abc", Checks.checkNotNull("abc", "someValue"));
     }
 
     @Test
-    public void checkNotNull_not_null_additional_message() throws Exception {
+    public void checkNotNull_not_null_additional_message() {
         assertEquals("abc", Checks.checkNotNull("abc", "someValue", "Oh no!"));
     }
 
     @Test
-    public void checkNotNull_null() throws Exception {
-        expectedException.expect(IllegalArgumentException.class);
-        expectedException.expectMessage("someValue should not be null");
-        Checks.checkNotNull(null, "someValue");
+    public void checkNotNull_null() {
+        assertThatThrownBy(
+                        () -> {
+                            Checks.checkNotNull(null, "someValue");
+                        })
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("someValue should not be null");
     }
 
     @Test
-    public void checkNotNull_null_additonal_message() throws Exception {
-        expectedException.expect(IllegalArgumentException.class);
-        expectedException.expectMessage("someValue should not be null. Oh no!");
-        Checks.checkNotNull(null, "someValue", "Oh no!");
+    public void checkNotNull_null_additonal_message() {
+        assertThatThrownBy(
+                        () -> {
+                            Checks.checkNotNull(null, "someValue", "Oh no!");
+                        })
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("someValue should not be null. Oh no!");
     }
 }
