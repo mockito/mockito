36fba923510323d23c5532f33019c1d05e8f13a3
temp-droid <gatien.bovyn@hotmail.com>
2021-12-16 19:55:53 +0100

    Replace ExpectedException in DescriptionTest

--- src/test/java/org/mockito/DescriptionTest.java
+++ src/test/java/org/mockito/DescriptionTest.java
@@ -4,42 +4,50 @@
  */
 package org.mockito;
 
-import static org.mockito.Mockito.*;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.mockito.Mockito.description;
+import static org.mockito.Mockito.spy;
+import static org.mockito.Mockito.verify;
 
-import org.junit.Rule;
 import org.junit.Test;
-import org.junit.rules.ExpectedException;
 import org.mockito.exceptions.base.MockitoAssertionError;
 
 /**
  * Tests for https://github.com/mockito/mockito/issues/1712
  */
 public class DescriptionTest {
-    @Rule public ExpectedException expectedException = ExpectedException.none();
 
     @Test
     public void verify_method_not_called_should_include_description_in_report() {
         final String description = "Failed to call doSomethingElse";
-        expectedException.expect(MockitoAssertionError.class);
-        expectedException.expectMessage(description);
 
         Dependency dependency = spy(Dependency.class);
         SystemUnderTest systemUnderTest = new SystemUnderTest();
         systemUnderTest.doNothing(dependency);
-        verify(dependency, description(description)).doSomethingElse(false);
+
+        assertThatThrownBy(
+                        () -> {
+                            verify(dependency, description(description)).doSomethingElse(false);
+                        })
+                .isInstanceOf(MockitoAssertionError.class)
+                .hasMessageContaining(description);
     }
 
     @Test
     public void
             verify_method_called_with_unexpected_argument_should_include_description_in_report() {
         final String description = "Failed to call doSomethingElse with expected argument";
-        expectedException.expect(MockitoAssertionError.class);
-        expectedException.expectMessage(description);
 
         Dependency dependency = spy(Dependency.class);
         SystemUnderTest systemUnderTest = new SystemUnderTest();
         systemUnderTest.doSomething(dependency);
-        verify(dependency, description(description)).doSomethingElse(false);
+
+        assertThatThrownBy(
+                        () -> {
+                            verify(dependency, description(description)).doSomethingElse(false);
+                        })
+                .isInstanceOf(MockitoAssertionError.class)
+                .hasMessageContaining(description);
     }
 
     static class SystemUnderTest {
