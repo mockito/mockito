20108c55eb5a018e4cef65b219dcd778afd84757
temp-droid <gatien.bovyn@hotmail.com>
2021-12-20 22:35:15 +0100

    Replace @Test(expected=...) in ClassCastExOnVerifyZeroInteractionsTest

--- src/test/java/org/mockitousage/bugs/ClassCastExOnVerifyZeroInteractionsTest.java
+++ src/test/java/org/mockitousage/bugs/ClassCastExOnVerifyZeroInteractionsTest.java
@@ -4,13 +4,13 @@
  */
 package org.mockitousage.bugs;
 
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verifyNoInteractions;
 
 import org.junit.Test;
 import org.mockito.exceptions.misusing.WrongTypeOfReturnValue;
 import org.mockito.exceptions.verification.NoInteractionsWanted;
-import org.mockito.invocation.InvocationOnMock;
 import org.mockito.stubbing.Answer;
 
 public class ClassCastExOnVerifyZeroInteractionsTest {
@@ -18,31 +18,35 @@ public class ClassCastExOnVerifyZeroInteractionsTest {
         boolean m1();
     }
 
-    @Test(expected = NoInteractionsWanted.class)
+    @Test
     public void should_not_throw_ClassCastException_when_mock_verification_fails() {
-        TestMock test =
-                mock(
-                        TestMock.class,
-                        new Answer<Object>() {
-                            public Object answer(InvocationOnMock invocation) throws Throwable {
-                                return false;
-                            }
-                        });
+        TestMock test = mock(TestMock.class, (Answer<Object>) invocation -> false);
         test.m1();
-        verifyNoInteractions(test);
+
+        assertThatThrownBy(
+                        () -> {
+                            verifyNoInteractions(test);
+                        })
+                .isInstanceOf(NoInteractionsWanted.class)
+                .hasMessageContainingAll(
+                        "No interactions wanted here:",
+                        "But found these interactions on mock 'testMock':",
+                        "Actually, above is the only interaction with this mock.");
     }
 
-    @Test(expected = WrongTypeOfReturnValue.class)
-    public void should_report_bogus_default_answer() throws Exception {
-        TestMock test =
-                mock(
-                        TestMock.class,
-                        new Answer<Object>() {
-                            public Object answer(InvocationOnMock invocation) throws Throwable {
-                                return false;
-                            }
-                        });
+    @Test
+    public void should_report_bogus_default_answer() {
+        TestMock test = mock(TestMock.class, (Answer<Object>) invocation -> false);
 
-        String ignored = test.toString();
+        assertThatThrownBy(
+                        () -> {
+                            String ignored = test.toString();
+                        })
+                .isInstanceOf(WrongTypeOfReturnValue.class)
+                .hasMessageContainingAll(
+                        "Default answer returned a result with the wrong type:",
+                        "Boolean cannot be returned by toString()",
+                        "toString() should return String",
+                        "The default answer of testMock that was configured on the mock is probably incorrectly implemented.");
     }
 }
