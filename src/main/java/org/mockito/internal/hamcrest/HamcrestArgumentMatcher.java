71173877e27ffb661da986190d0df2136a2c1249
Big Andy <8012398+big-andy-coates@users.noreply.github.com>
2022-12-23 22:41:58 +0700

    add `argThat(matcher, type)`
    ... required to support matchers that match a vararg parameter, rather than values in a varargs parameter.

--- src/main/java/org/mockito/internal/hamcrest/HamcrestArgumentMatcher.java
+++ src/main/java/org/mockito/internal/hamcrest/HamcrestArgumentMatcher.java
@@ -8,12 +8,24 @@ import org.hamcrest.Matcher;
 import org.hamcrest.StringDescription;
 import org.mockito.ArgumentMatcher;
 
+import static java.util.Objects.requireNonNull;
+
 public class HamcrestArgumentMatcher<T> implements ArgumentMatcher<T> {
 
     private final Matcher<T> matcher;
+    private final Class<?> type;
 
     public HamcrestArgumentMatcher(Matcher<T> matcher) {
-        this.matcher = matcher;
+        this(Void.class, matcher);
+    }
+
+    public HamcrestArgumentMatcher(Matcher<T> matcher, Class<T> type) {
+        this(type, matcher);
+    }
+
+    private HamcrestArgumentMatcher(Class<?> type, Matcher<T> matcher) {
+        this.type = requireNonNull(type, "type");
+        this.matcher = requireNonNull(matcher, "matcher");
     }
 
     @Override
@@ -25,5 +37,10 @@ public class HamcrestArgumentMatcher<T> implements ArgumentMatcher<T> {
     public String toString() {
         // TODO SF add unit tests and integ test coverage for toString()
         return StringDescription.toString(matcher);
+    }
+
+    @Override
+    public Class<?> type() {
+        return type;
     }
 }
