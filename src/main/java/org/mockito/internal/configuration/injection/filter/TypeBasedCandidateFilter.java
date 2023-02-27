5617b2e1ba8b1b7c1973c1e38d3ebd1d6a877485
Jörg von Frantzius <joerg.frantzius@aperto.com>
2023-02-27 17:01:14 +0100

    #2921 evaluate type arguments and raw types

--- src/main/java/org/mockito/internal/configuration/injection/filter/TypeBasedCandidateFilter.java
+++ src/main/java/org/mockito/internal/configuration/injection/filter/TypeBasedCandidateFilter.java
@@ -5,10 +5,15 @@
 package org.mockito.internal.configuration.injection.filter;
 
 import java.lang.reflect.Field;
+import java.lang.reflect.GenericArrayType;
+import java.lang.reflect.ParameterizedType;
 import java.lang.reflect.Type;
+import java.lang.reflect.WildcardType;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collection;
 import java.util.List;
+import java.util.Objects;
 
 import org.mockito.internal.util.MockUtil;
 
@@ -20,6 +25,49 @@ public class TypeBasedCandidateFilter implements MockCandidateFilter {
         this.next = next;
     }
 
+    protected boolean isCompatibleTypes(Type typeToMock, Type mockType) {
+        if (typeToMock instanceof ParameterizedType && mockType instanceof ParameterizedType) {
+            // ParameterizedType.equals() is documented as: 
+            // "Instances of classes that implement this interface must implement
+            // an equals() method that equates any two instances that share the
+            // same generic type declaration and have equal type parameters."
+            // Unfortunately, e.g. Wildcard parameter "?" doesn't equal java.lang.String
+            if (typeToMock.equals(mockType)) {
+                return true;
+            } else {
+                ParameterizedType genericTypeToMock = (ParameterizedType) typeToMock;
+                ParameterizedType genericMockType = (ParameterizedType) mockType;
+                Type[] actualTypeArguments = genericTypeToMock.getActualTypeArguments();
+                Type[] actualTypeArguments2 = genericMockType.getActualTypeArguments();
+                // getRawType() says "the Type object representing the class or interface that declares this type", 
+                // no clue why that's a Type rather than a Class as return type anyway
+                Class rawType = (Class) genericTypeToMock.getRawType();
+                Class rawType2 = (Class) genericMockType.getRawType();
+                if (Objects.equals(genericTypeToMock.getOwnerType(), genericMockType.getOwnerType()) &&
+                    // e.g. Tree and TreeSet
+                    rawType.isAssignableFrom(rawType2) &&
+                    actualTypeArguments.length == actualTypeArguments2.length ) {
+                    // descend into recursion on type arguments
+                    for (int i = 0; i < actualTypeArguments.length; i++) {
+                        return isCompatibleTypes(actualTypeArguments[i], actualTypeArguments2[i]);
+                    }
+                } else {
+                    // owner type, raw type or type arguments length don't match
+                    return false;
+                }
+            }
+        } else if (typeToMock instanceof WildcardType) {
+            WildcardType wildcardTypeToMock = (WildcardType) typeToMock;
+            Type[] upperBounds = wildcardTypeToMock.getUpperBounds();
+            return Arrays.stream(upperBounds).anyMatch(t -> isCompatibleTypes(t, mockType));
+        } else if (typeToMock instanceof GenericArrayType && mockType instanceof GenericArrayType) {
+            return isCompatibleTypes(((GenericArrayType)typeToMock).getGenericComponentType(), ((GenericArrayType)mockType).getGenericComponentType());
+        } else if (typeToMock instanceof Class && mockType instanceof Class) {
+            return ((Class)typeToMock).isAssignableFrom((Class) mockType);
+        }
+        return false;
+    }
+
     @Override
     public OngoingInjector filterCandidate(
             final Collection<Object> mocks,
@@ -29,22 +77,16 @@ public class TypeBasedCandidateFilter implements MockCandidateFilter {
         List<Object> mockTypeMatches = new ArrayList<>();
         for (Object mock : mocks) {
             if (candidateFieldToBeInjected.getType().isAssignableFrom(mock.getClass())) {
-                Type genericTypeToMock = MockUtil.getMockSettings(mock).getGenericTypeToMock();
+                Type genericMockType = MockUtil.getMockSettings(mock).getGenericTypeToMock();
                 Type genericType = candidateFieldToBeInjected.getGenericType();
+                boolean bothHaveGenericTypeInfo = genericType!=null && genericMockType!=null;
                 // be more specific if generic type information is available
-                if (genericTypeToMock != null || genericType != null) {
-                    // would rather like to use Type.getTypeName(), but that doesn't exist in
-                    // Android SDK 26 which Mockito aims to maintain compatibility with.
-                    // Type.getTypeName() is documented to simply call Type.toString(), so use that
-                    // instead
-                    if (genericTypeToMock != null
-                            && genericType != null
-                            && genericTypeToMock.toString().equals(genericType.toString())) {
-                        mockTypeMatches.add(mock);
-                    } // else: filter out mock, as generic types don't match
-                } else {
+                if (!bothHaveGenericTypeInfo || isCompatibleTypes(genericType, genericMockType)) {
                     mockTypeMatches.add(mock);
-                }
+                } else {
+                    // else: filter out mock, as generic types don't match
+                    System.out.println("types don't macht " + candidateFieldToBeInjected + " " + genericType + " " + genericMockType);
+                } 
             }
         }
 
