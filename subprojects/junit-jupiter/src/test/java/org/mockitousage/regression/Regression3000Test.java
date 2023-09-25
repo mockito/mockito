6d33b25a4bca5f0b3f857299ecfd1d39e08d7b6a
Harald Fassler <harald.fassler+9974@gmail.com>
2023-09-25 07:37:16 +0200

    add license headers

--- subprojects/junit-jupiter/src/test/java/org/mockitousage/regression/Regression3000Test.java
+++ subprojects/junit-jupiter/src/test/java/org/mockitousage/regression/Regression3000Test.java
@@ -1,3 +1,7 @@
+/*
+ * Copyright (c) 2016 Mockito contributors
+ * This program is made available under the terms of the MIT License.
+ */
 package org.mockitousage.regression;
 
 import org.junit.jupiter.api.Test;
