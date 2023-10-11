edc581d016855b4514d0208f3e9911b0c6d4a95b
ascopes <73482956+ascopes@users.noreply.github.com>
2023-10-11 21:20:32 +0100

    Feat: add generic-inferred methods for constructing ArgumentCaptors
    Added a new method .captor(U...) on the ArgumentCaptor class that
    has the same interface as Mockito.mock(U...) with the goal of removing
    the need for unchecked-warning suppression when users make use of
    generic types in argument captors.

--- src/test/java/org/mockito/ArgumentCaptorTest.java
+++ src/test/java/org/mockito/ArgumentCaptorTest.java
@@ -5,11 +5,14 @@
 package org.mockito;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.mockito.Mockito.validateMockitoUsage;
 
 import org.junit.After;
 import org.junit.Test;
 import org.mockito.exceptions.misusing.InvalidUseOfMatchersException;
+
+import java.util.Map;
 
 public class ArgumentCaptorTest {
 
@@ -29,5 +32,39 @@ public class ArgumentCaptorTest {
 
         ArgumentCaptor<Object> captor = ArgumentCaptor.forClass(Object.class);
         assertThat(captor.capture()).isNull();
+    }
+
+    @Test
+    public void getCaptorType_returns_captor_type() throws Exception {
+        class Foo {
+        }
+
+        class Bar {
+        }
+
+        ArgumentCaptor<Foo> fooCaptor = ArgumentCaptor.forClass(Foo.class);
+        ArgumentCaptor<Bar> barCaptor = ArgumentCaptor.forClass(Bar.class);
+
+        assertThat(fooCaptor.getCaptorType()).isEqualTo(Foo.class);
+        assertThat(barCaptor.getCaptorType()).isEqualTo(Bar.class);
+    }
+
+    @Test
+    public void captor_calls_forClass_with_the_inferred_argument() throws Exception {
+        ArgumentCaptor<Map<String, Object>> captor = ArgumentCaptor.captor();
+        assertThat(captor.getCaptorType()).isEqualTo(Map.class);
+    }
+
+    @Test
+    public void captor_called_with_explicit_varargs_is_invalid() throws Exception {
+        // Test passing a single argument.
+        assertThatThrownBy(() -> ArgumentCaptor.captor(1234L))
+                .isInstanceOf(IllegalArgumentException.class);
+        // Test passing multiple arguments.
+        assertThatThrownBy(() -> ArgumentCaptor.captor("this shouldn't", "be here"))
+                .isInstanceOf(IllegalArgumentException.class);
+        // Test passing a totally null varargs array.
+        assertThatThrownBy(() -> ArgumentCaptor.<String>captor((String[]) null))
+                .isInstanceOf(IllegalArgumentException.class);
     }
 }
