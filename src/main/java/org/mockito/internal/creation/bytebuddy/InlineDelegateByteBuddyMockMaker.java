b6554b29ed6c204a0dd4b8a670877fe0ba2e808b
Mikaël Francoeur <mikael.francoeur@hotmail.com>
2023-11-28 01:57:26 -0500

    Fix interference between spies when spying on records (#3173)
    Fixes #3160
    
    Co-authored-by: Mikaël Francoeur <mikael.francoeur@ticketmaster.com>

--- src/main/java/org/mockito/internal/creation/bytebuddy/InlineDelegateByteBuddyMockMaker.java
+++ src/main/java/org/mockito/internal/creation/bytebuddy/InlineDelegateByteBuddyMockMaker.java
@@ -218,7 +218,7 @@ class InlineDelegateByteBuddyMockMaker
     private final DetachedThreadLocal<Map<Class<?>, BiConsumer<Object, MockedConstruction.Context>>>
             mockedConstruction = new DetachedThreadLocal<>(DetachedThreadLocal.Cleaner.MANUAL);
 
-    private final ThreadLocal<Boolean> mockitoConstruction = ThreadLocal.withInitial(() -> false);
+    private final ThreadLocal<Class<?>> currentMocking = ThreadLocal.withInitial(() -> null);
 
     private final ThreadLocal<Object> currentSpied = new ThreadLocal<>();
 
@@ -272,7 +272,9 @@ class InlineDelegateByteBuddyMockMaker
                 type -> {
                     if (isSuspended.get()) {
                         return false;
-                    } else if (mockitoConstruction.get() || currentConstruction.get() != null) {
+                    } else if ((currentMocking.get() != null
+                                    && type.isAssignableFrom(currentMocking.get()))
+                            || currentConstruction.get() != null) {
                         return true;
                     }
                     Map<Class<?>, ?> interceptors = mockedConstruction.get();
@@ -290,7 +292,7 @@ class InlineDelegateByteBuddyMockMaker
                 };
         ConstructionCallback onConstruction =
                 (type, object, arguments, parameterTypeNames) -> {
-                    if (mockitoConstruction.get()) {
+                    if (currentMocking.get() != null) {
                         Object spy = currentSpied.get();
                         if (spy == null) {
                             return null;
@@ -647,11 +649,11 @@ class InlineDelegateByteBuddyMockMaker
                     accessor.newInstance(
                             selected,
                             callback -> {
-                                mockitoConstruction.set(true);
+                                currentMocking.set(cls);
                                 try {
                                     return callback.newInstance();
                                 } finally {
-                                    mockitoConstruction.set(false);
+                                    currentMocking.remove();
                                 }
                             },
                             arguments);
---
+++ subprojects/java21/src/test/java/org/mockito/java21/RecordTest.java
@@ -0,0 +1,26 @@
+/*
+ * Copyright (c) 2023 Mockito contributors
+ * This program is made available under the terms of the MIT License.
+ */
+package org.mockito.java21;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.spy;
+
+import java.util.List;
+
+import org.junit.Test;
+
+public class RecordTest {
+
+    @Test
+    public void given_list_is_already_spied__when_spying_record__then_record_fields_are_correctly_populated() {
+        var ignored = spy(List.of());
+
+        record MyRecord(String name) {
+        }
+        MyRecord spiedRecord = spy(new MyRecord("something"));
+
+        assertThat(spiedRecord.name()).isEqualTo("something");
+    }
+}
