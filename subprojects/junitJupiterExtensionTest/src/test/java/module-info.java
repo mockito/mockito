add76cdd1abd98ea0d3ecaa1f0e8664e54d7dace
Caleb Cushing <xenoterracide@gmail.com>
2024-07-03 14:23:41 -0400

    chore: spotlessApply

--- subprojects/junitJupiterExtensionTest/src/test/java/module-info.java
+++ subprojects/junitJupiterExtensionTest/src/test/java/module-info.java
@@ -3,6 +3,8 @@ module mockito.junitJupiterExtensionTest.test {
     requires org.mockito;
     requires org.mockito.junit.jupiter;
     requires org.assertj.core;
+
     opens org.mockitousage;
+
     uses org.mockito.junit.jupiter.MockitoExtension;
 }
