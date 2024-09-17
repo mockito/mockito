e4fb71d5fd9d8d9ae8a58da6d7a3f0cf26e6c606
Brice Dutheil <brice.dutheil@gmail.com>
2024-09-17 16:11:37 +0200

    Fixes #3443: Split subproject by deliverable and integration tests

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -3,50 +3,43 @@ plugins {
     id("org.gradle.toolchains.foojay-resolver-convention") version "0.8.0"
 }
 
+// BOM project
+include("mockito-bom")
+// extensions
 include(
-    "subclass",
-    "inlineTest",
-    "proxy",
-    "extTest",
-    "groovyTest",
-    "groovyInlineTest",
-    "kotlinTest",
-    "kotlinReleaseCoroutinesTest",
-    "android",
-    "junit-jupiter",
-    "junitJupiterExtensionTest",
-    "junitJupiterInlineMockMakerExtensionTest",
-    "module-test",
-    "memory-test",
-    "junitJupiterParallelTest",
-    "osgi-test",
-    "bom",
-    "errorprone",
-    "programmatic-test",
-    "java21-test"
+    "mockito-extensions:android",
+    "mockito-extensions:errorprone",
+    "mockito-extensions:junit-jupiter",
+    "mockito-extensions:proxy",
+    "mockito-extensions:subclass",
+)
+// integration tests
+include(
+    "mockito-integration-tests:inline-mocks-tests",
+    "mockito-integration-tests:extensions-tests",
+    "mockito-integration-tests:groovy-tests",
+    "mockito-integration-tests:groovy-inline-tests",
+    "mockito-integration-tests:kotlin-tests",
+    "mockito-integration-tests:kotlin-release-coroutines-tests",
+    "mockito-integration-tests:junit-jupiter-extension-tests",
+    "mockito-integration-tests:junit-jupiter-inline-mock-maker-extension-tests",
+    "mockito-integration-tests:module-tests",
+    "mockito-integration-tests:memory-tests",
+    "mockito-integration-tests:junit-jupiter-parallel-tests",
+    "mockito-integration-tests:osgi-tests",
+    "mockito-integration-tests:programmatic-tests",
+    "mockito-integration-tests:java-21-tests",
 )
 
 // https://developer.android.com/studio/command-line/variables#envar
 // https://developer.android.com/studio/build#properties-files
 if (System.getenv("ANDROID_HOME") != null || File("local.properties").exists()) {
-    include("androidTest")
+    include("mockito-integration-tests:android-tests")
 } else {
     logger.info("Not including android test project due to missing SDK configuration")
 }
 
 rootProject.name = "mockito"
-
-rootProject.children.forEach { project ->
-    val projectDirName = "subprojects/${project.name}"
-    project.projectDir = File(settingsDir, projectDirName)
-    project.buildFileName = "${project.name}.gradle"
-    require(project.projectDir.isDirectory) {
-        "Project directory ${project.projectDir} for project ${project.name} does not exist."
-    }
-    require(project.buildFile.isFile) {
-        "Build file ${project.buildFile} for project ${project.name} does not exist."
-    }
-}
 
 //Posting Build scans to https://scans.gradle.com
 gradleEnterprise {
