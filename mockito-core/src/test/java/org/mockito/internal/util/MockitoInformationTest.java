334c9d3d0db00942ec49c2bf3ef2f54a6c265fb5
Brice Dutheil <brice.dutheil@gmail.com>
2024-12-07 17:34:32 +0100

    Adds mockito version to error reports

--- mockito-core/src/test/java/org/mockito/internal/util/MockitoInformationTest.java
+++ mockito-core/src/test/java/org/mockito/internal/util/MockitoInformationTest.java
@@ -4,27 +4,34 @@
  */
 package org.mockito.internal.util;
 
-import org.assertj.core.api.SoftAssertions;
 import org.junit.Test;
 import org.mockito.MockMakers;
 import org.mockito.internal.configuration.plugins.Plugins;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.SoftAssertions.assertSoftly;
 
 public class MockitoInformationTest {
 
     @Test
     public void should_describe_mock_makers() {
-        assertSoftly(softly -> {
-            softly.assertThat(MockitoInformation.describe(null))
-                  .containsPattern(
-                      "mock-maker\\s+:\\s+" + Plugins.getMockMaker().getName());
-            softly.assertThat(MockitoInformation.describe(MockMakers.SUBCLASS))
-                  .containsPattern("mock-maker\\s+:\\s+mock-maker-subclass");
-            softly.assertThat(MockitoInformation.describe(MockMakers.PROXY))
-                  .containsPattern("mock-maker\\s+:\\s+mock-maker-proxy");
-            softly.assertThat(MockitoInformation.describe(MockMakers.INLINE))
-                  .containsPattern("mock-maker\\s+:\\s+mock-maker-inline");
-        });
+        assertSoftly(
+                softly -> {
+                    softly.assertThat(MockitoInformation.describe(null))
+                            .containsPattern(
+                                    "mock-maker\\s+:\\s+" + Plugins.getMockMaker().getName());
+                    softly.assertThat(MockitoInformation.describe(MockMakers.SUBCLASS))
+                            .containsPattern("mock-maker\\s+:\\s+mock-maker-subclass");
+                    softly.assertThat(MockitoInformation.describe(MockMakers.PROXY))
+                            .containsPattern("mock-maker\\s+:\\s+mock-maker-proxy");
+                    softly.assertThat(MockitoInformation.describe(MockMakers.INLINE))
+                            .containsPattern("mock-maker\\s+:\\s+mock-maker-inline");
+                });
+    }
+
+    @Test
+    public void should_mention_mockito_version() {
+        assertThat(MockitoInformation.describe(null))
+                .containsPattern("mockito-version\\s+:\\s+" + "\\w+");
     }
 }
