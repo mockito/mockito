4f3a2e8d0b49f276835d7ed405de6c7c53677b49
Tim van der Lippe <TimvdLippe@users.noreply.github.com>
2025-07-02 08:26:12 +0000

    Run `:mockito-errorprone` tests on minimum version 17
    The minimum version was upgraded in
    https://github.com/google/error-prone/issues/3803

--- buildSrc/src/main/kotlin/mockito.test-launcher-conventions.gradle.kts
+++ buildSrc/src/main/kotlin/mockito.test-launcher-conventions.gradle.kts
@@ -10,6 +10,9 @@ tasks.withType<Test> {
             .map {
                 if (it == "auto") {
                     JavaLanguageVersion.of(JavaVersion.current().majorVersion)
+                // ErrorProne minimum supported version is 17: https://github.com/google/error-prone/issues/3803
+                } else if (project.name == "mockito-errorprone" && it.toInt() < 17) {
+                    JavaLanguageVersion.of(17)
                 } else {
                     JavaLanguageVersion.of(it)
                 }
