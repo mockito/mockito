ee72b7800d3153eb43f7027b5191608c20b24d26
Mirko Alicastro <mirkoalicastro@gmail.com>
2026-03-14 10:26:28 +0100

    Add unit tests to the SpyStatic API

--- mockito-integration-tests/inline-mocks-tests/src/test/java/org/mockitoinline/SpyStaticTest.java
+++ mockito-integration-tests/inline-mocks-tests/src/test/java/org/mockitoinline/SpyStaticTest.java
@@ -5,6 +5,8 @@
 package org.mockitoinline;
 
 import static junit.framework.TestCase.assertEquals;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.withSettings;
 
 import java.util.concurrent.atomic.AtomicReference;
@@ -12,6 +14,9 @@ import java.util.concurrent.atomic.AtomicReference;
 import org.junit.Test;
 import org.mockito.MockedStatic;
 import org.mockito.Mockito;
+import org.mockito.exceptions.base.MockitoException;
+import org.mockito.exceptions.verification.NoInteractionsWanted;
+import org.mockito.exceptions.verification.WantedButNotInvoked;
 
 public final class SpyStaticTest {
 
@@ -109,6 +114,117 @@ public final class SpyStaticTest {
             spy.when(Dummy::foo).thenReturn("settings");
             assertEquals("settings", Dummy.foo());
         }
+    }
+
+    @Test
+    public void testStaticSpyReifiedOverload() {
+        try (MockedStatic<Dummy> spy = Mockito.spyStatic()) {
+            assertEquals("foo", Dummy.foo());
+            spy.when(Dummy::foo).thenReturn("reified");
+            assertEquals("reified", Dummy.foo());
+        }
+    }
+
+    @Test
+    public void testStaticSpyReifiedNamedOverload() {
+        try (MockedStatic<Dummy> spy = Mockito.spyStatic("reifiedSpy")) {
+            assertEquals("foo", Dummy.foo());
+            spy.when(Dummy::foo).thenReturn("reifiedNamed");
+            assertEquals("reifiedNamed", Dummy.foo());
+        }
+    }
+
+    @Test
+    public void testStaticSpyReifiedWithMockSettings() {
+        try (MockedStatic<Dummy> spy = Mockito.spyStatic(withSettings().name("reifiedSettings"))) {
+            assertEquals("foo", Dummy.foo());
+            spy.when(Dummy::foo).thenReturn("reifiedSettings");
+            assertEquals("reifiedSettings", Dummy.foo());
+        }
+    }
+
+    @Test
+    public void testStaticSpyVerifyWithTimes() {
+        try (MockedStatic<Dummy> spy = Mockito.spyStatic(Dummy.class)) {
+            Dummy.foo();
+            Dummy.foo();
+            Dummy.foo();
+            spy.verify(Dummy::foo, times(3));
+        }
+    }
+
+    @Test
+    public void testStaticSpyClearInvocations() {
+        try (MockedStatic<Dummy> spy = Mockito.spyStatic(Dummy.class)) {
+            spy.when(Dummy::foo).thenReturn("bar");
+            assertEquals("bar", Dummy.foo());
+            spy.clearInvocations();
+            spy.verifyNoInteractions();
+            // Stubbing should still be active after clearing invocations
+            assertEquals("bar", Dummy.foo());
+        }
+    }
+
+    @Test
+    public void testStaticSpyVerifyNoMoreInteractions() {
+        try (MockedStatic<Dummy> spy = Mockito.spyStatic(Dummy.class)) {
+            assertEquals("foo", Dummy.foo());
+            spy.verify(Dummy::foo);
+            spy.verifyNoMoreInteractions();
+        }
+    }
+
+    @Test
+    public void testStaticSpyVerifyNoMoreInteractionsFailed() {
+        try (MockedStatic<Dummy> spy = Mockito.spyStatic(Dummy.class)) {
+            assertEquals("foo", Dummy.foo());
+            assertThatThrownBy(
+                            () -> {
+                                spy.verifyNoMoreInteractions();
+                            })
+                    .isInstanceOf(NoInteractionsWanted.class);
+        }
+    }
+
+    @Test
+    public void testStaticSpyVerifyNoInteractions() {
+        try (MockedStatic<Dummy> spy = Mockito.spyStatic(Dummy.class)) {
+            spy.verifyNoInteractions();
+        }
+    }
+
+    @Test
+    public void testStaticSpyVerifyNoInteractionsFailed() {
+        try (MockedStatic<Dummy> spy = Mockito.spyStatic(Dummy.class)) {
+            Dummy.foo();
+            assertThatThrownBy(
+                            () -> {
+                                spy.verifyNoInteractions();
+                            })
+                    .isInstanceOf(NoInteractionsWanted.class);
+        }
+    }
+
+    @Test
+    public void testStaticSpyVerifyFailed() {
+        try (MockedStatic<Dummy> spy = Mockito.spyStatic(Dummy.class)) {
+            assertThatThrownBy(
+                            () -> {
+                                spy.verify(Dummy::foo);
+                            })
+                    .isInstanceOf(WantedButNotInvoked.class);
+        }
+    }
+
+    @Test
+    public void testStaticSpyMustBeExclusiveInScopeWithinThread() {
+        assertThatThrownBy(
+                        () -> {
+                            try (MockedStatic<Dummy> spy1 = Mockito.spyStatic(Dummy.class);
+                                    MockedStatic<Dummy> spy2 = Mockito.spyStatic(Dummy.class)) {}
+                        })
+                .isInstanceOf(MockitoException.class)
+                .hasMessageContaining("static mocking is already registered in the current thread");
     }
 
     static class Dummy {
